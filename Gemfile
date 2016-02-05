0964f35c57cde712b2580e4e05ed32fc39697cb3
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-02-05 14:09:21 +0000

    3321: Add localisation and index template
    Authors: @whpearson @robinwhittleton @richardtowers @vixus0 @gdsamit
    @chrisholmes @srinivasmurty @leoshaw @willp-bl

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.1'
+gem 'rails-i18n', '~> 4.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- spec/features/user_visits_start_page_spec.rb
+++ spec/features/user_visits_start_page_spec.rb
@@ -3,6 +3,6 @@ require 'feature_helper'
 RSpec.describe 'When the user visits the start page' do
   it 'will display the start page' do
     visit '/start'
-    expect(page).to have_content 'Hello world'
+    expect(page).to have_content 'Sign in with GOV.UK Verify'
   end
 end
