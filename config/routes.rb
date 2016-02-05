1b3877bcbc6f2df9fba2b2f78d5f51029b4cfda1
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-02-05 14:53:22 +0000

    3321: Add Google translated Welsh to start page
    Authors: @whpearson @robinwhittleton @richardtowers @vixus0 @gdsamit
    @chrisholmes @srinivasmurty @leoshaw

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'start' => 'start#index'
+  get 'dechrau' => 'start#index', defaults: { locale: 'cy' }
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
--- spec/features/user_visits_start_page_spec.rb
+++ spec/features/user_visits_start_page_spec.rb
@@ -4,5 +4,11 @@ RSpec.describe 'When the user visits the start page' do
   it 'will display the start page' do
     visit '/start'
     expect(page).to have_content 'Sign in with GOV.UK Verify'
+    expect(page).to have_css 'html[lang=en]'
+  end
+  it 'will display the start page in Welsh' do
+    visit '/dechrau'
+    expect(page).to have_content 'Mewngofnodi gyda GOV.UK Verify'
+    expect(page).to have_css 'html[lang=cy]'
   end
 end
