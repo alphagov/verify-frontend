53092c915a6cc5e7debdb07261ab6f406187203d
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-02-16 17:27:34 +0000

    3374: load RP locale data from location using envvar
    Authors: @chrisholmes @richardtowers @veda @vixus0 @oswaldquek

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module VerifyFrontend
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
+    config.i18n.load_path +=  Dir[File.join(ENV.fetch('RP_DISPLAY_LOCALES'), '*.{rb,yml}').to_s]
 
     RouteTranslator.config do |config|
       config.hide_locale = true
