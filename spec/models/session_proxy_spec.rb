9173f39f0f13d2017e54345738808c256ed0b24e
Anshul Sirur <vixus0@gmail.com>
2016-02-25 12:31:04 +0000

    3320: refactor session proxy tests
    These weren't very descriptive, so we've removed the old context and
    given the tests better names.
    
     Authors: @vixus0 @richardtowers

--- spec/models/session_proxy_spec.rb
+++ spec/models/session_proxy_spec.rb
@@ -6,41 +6,40 @@ describe SessionProxy do
   let(:api_client) { double(:api_client) }
   let(:path) { "/session" }
 
-  context 'if request was successful' do
-    it 'should be ok and return cookies' do
-      x_forwarded_for = double(:x_forwarded_for)
-      authn_request_body = {
-          SessionProxy::PARAM_SAML_REQUEST => 'my-saml-request',
-          SessionProxy::PARAM_RELAY_STATE => 'my-relay-state',
-          SessionProxy::PARAM_ORIGINATING_IP => x_forwarded_for
-      }
-      cookie_hash = {
-          CookieNames::SESSION_ID_COOKIE_NAME => "my-session-id-cookie",
-          CookieNames::SECURE_COOKIE_NAME => "my-secure-cookie",
-          CookieNames::SESSION_STARTED_TIME_COOKIE_NAME => 'my-session-start-time'
-      }
-      expect(api_client).to receive(:post).with(path, authn_request_body).and_return(cookie_hash)
-      cookies = SessionProxy.new(api_client).create_session('my-saml-request', 'my-relay-state', x_forwarded_for)
-      expect(cookies).to eq cookie_hash
-    end
-    it 'should return a list of IDP ids' do
-      cookie_hash = {
-          CookieNames::SESSION_ID_COOKIE_NAME => "my-session-id-cookie",
-          CookieNames::SECURE_COOKIE_NAME => "my-secure-cookie",
-          CookieNames::SESSION_STARTED_TIME_COOKIE_NAME => 'my-session-start-time',
-          'SOME_OTHER_COOKIE' => 'something else'
-      }
+  it 'should return cookies when a session is created' do
+    x_forwarded_for = double(:x_forwarded_for)
+    authn_request_body = {
+        SessionProxy::PARAM_SAML_REQUEST => 'my-saml-request',
+        SessionProxy::PARAM_RELAY_STATE => 'my-relay-state',
+        SessionProxy::PARAM_ORIGINATING_IP => x_forwarded_for
+    }
+    cookie_hash = {
+        CookieNames::SESSION_ID_COOKIE_NAME => "my-session-id-cookie",
+        CookieNames::SECURE_COOKIE_NAME => "my-secure-cookie",
+        CookieNames::SESSION_STARTED_TIME_COOKIE_NAME => 'my-session-start-time'
+    }
+    expect(api_client).to receive(:post).with(path, authn_request_body).and_return(cookie_hash)
+    cookies = SessionProxy.new(api_client).create_session('my-saml-request', 'my-relay-state', x_forwarded_for)
+    expect(cookies).to eq cookie_hash
+  end
 
-      expected_cookie_hash = {
-          CookieNames::SESSION_ID_COOKIE_NAME => "my-session-id-cookie",
-          CookieNames::SECURE_COOKIE_NAME => "my-secure-cookie",
-          CookieNames::SESSION_STARTED_TIME_COOKIE_NAME => 'my-session-start-time',
-      }
-      idp_list = double(:idp_list)
+  it 'should return a list of IDP ids for the session' do
+    cookie_hash = {
+        CookieNames::SESSION_ID_COOKIE_NAME => "my-session-id-cookie",
+        CookieNames::SECURE_COOKIE_NAME => "my-secure-cookie",
+        CookieNames::SESSION_STARTED_TIME_COOKIE_NAME => 'my-session-start-time',
+        'SOME_OTHER_COOKIE' => 'something else'
+    }
 
-      expect(api_client).to receive(:get).with('/session/idps', {cookies: expected_cookie_hash}).and_return(idp_list)
-      result = SessionProxy.new(api_client).idps_for_session(cookie_hash)
-      expect(result).to eq idp_list
-    end
+    expected_cookie_hash = {
+        CookieNames::SESSION_ID_COOKIE_NAME => "my-session-id-cookie",
+        CookieNames::SECURE_COOKIE_NAME => "my-secure-cookie",
+        CookieNames::SESSION_STARTED_TIME_COOKIE_NAME => 'my-session-start-time',
+    }
+    idp_list = double(:idp_list)
+
+    expect(api_client).to receive(:get).with('/session/idps', {cookies: expected_cookie_hash}).and_return(idp_list)
+    result = SessionProxy.new(api_client).idps_for_session(cookie_hash)
+    expect(result).to eq idp_list
   end
 end
