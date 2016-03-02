ebe84dd3e3140ddb893d5b4516c0bc8edda0987c
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-03-02 17:25:55 +0000

    3347: remove control server and x01 port
    We won't be using the control server or the x01 port so these have been removed

--- config/puma.rb
+++ config/puma.rb
@@ -7,8 +7,5 @@ state_path 'tmp/puma.state'
 stdout_redirect 'log/puma.stdout', 'log/puma.stderr', true
 
 bind 'unix://tmp/puma.sock'
-bind 'tcp://127.0.0.1:50301'
 
 workers 2
-
-activate_control_app 'unix://tmp/pumactl.sock', { no_token: true }
