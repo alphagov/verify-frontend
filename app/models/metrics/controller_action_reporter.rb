e60831cc312572548c41c04f79591c8101f33239
Anshul Sirur <vixus0@gmail.com>
2016-03-09 11:25:36 +0000

    3433: Send total duration and view rendering times in ms
    Authors: @veda @lloydnye @vixus0

--- app/models/metrics/controller_action_reporter.rb
+++ app/models/metrics/controller_action_reporter.rb
@@ -1,6 +1,8 @@
 module Metrics
   class ControllerActionReporter
-    KEY_DELIMETER = '.'
+    KEY_DELIMITER = '.'
+    TOTAL_DURATION_IN_MS = 'total_duration_ms'
+    VIEW_RUNTIME_IN_MS = 'view_runtime_ms'
 
     def initialize(graphite)
       @graphite = graphite
@@ -9,13 +11,19 @@ module Metrics
     def report(_name, start, finish, _id, payload)
       # args name and id get passed from ActiveSupport, however we will not need them to report.
       source = [payload[:controller], payload[:action]]
-      @graphite.send(metric_key(source, 'total_duration'), finish - start)
+      # Time.to_i converts to seconds and we want milliseconds
+      @graphite.send(metric_key(source, TOTAL_DURATION_IN_MS), duration_in_ms(finish, start))
+      @graphite.send(metric_key(source, VIEW_RUNTIME_IN_MS), payload[:view_runtime])
     end
 
   private
 
+    def duration_in_ms(finish, start)
+      (finish.to_i - start.to_i) * 1000
+    end
+
     def metric_key(source, metric)
-      [source, metric].join(KEY_DELIMETER)
+      [source, metric].join(KEY_DELIMITER)
     end
   end
 end
--- spec/models/metrics/controller_action_reporter_spec.rb
+++ spec/models/metrics/controller_action_reporter_spec.rb
@@ -6,22 +6,22 @@ module Metrics
     let(:graphite) { double(:graphite) }
     let(:reporter) { ControllerActionReporter.new(graphite) }
 
-    it 'should send a duration of 60 when start and finish 60s apart' do
+    it 'should send total duration of event' do
       payload = {controller: 'SomeController', action: 'someAction'}
       duration = 60
       start = Time.now
       finish = start + duration
-      expect(graphite).to receive(:send).with("SomeController.someAction.total_duration", duration)
+      expect(graphite).to receive(:send).with("SomeController.someAction.total_duration_ms", duration * 1000)
+      allow(graphite).to receive(:send)
       reporter.report('event_name', start, finish, 'notification_id', payload)
     end
 
-    it 'should send a key constructed from the controller and action name' do
-      payload = {controller: 'AnotherController', action: 'anotherAction'}
-      duration = 60
-      start = Time.now
-      finish = start + duration
-      expect(graphite).to receive(:send).with("AnotherController.anotherAction.total_duration", duration)
-      reporter.report('event_name', start, finish, 'notification_id', payload)
+    it 'should send time spent rendering view' do
+      view_runtime_in_ms = 50.12
+      payload = {controller: 'AnotherController', action: 'anotherAction', view_runtime: view_runtime_in_ms}
+      allow(graphite).to receive(:send)
+      expect(graphite).to receive(:send).with("AnotherController.anotherAction.view_runtime_ms", view_runtime_in_ms)
+      reporter.report('event_name', Time.now, Time.now, 'notification_id', payload)
     end
   end
 end
