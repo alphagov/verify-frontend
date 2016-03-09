93383766669096ef7bcc27aa6348450371976b9f
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-03-09 11:24:32 +0000

    bau: clear tmp directory on packaging
    Also, no need to create on postinst
    
    Authors: @chrisholmes @conorglynn

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,4 @@
 #/usr/bin/env sh
 RAILS_ENV=production bundle exec rake assets:precompile
+RAILS_ENV=production bundle exec rake tmp:clear
 pkgr package . --version=${BUILD_NUMBER} --iteration=1 --name=front
--- packaging/postinst.sh
+++ packaging/postinst.sh
@@ -1,4 +1,3 @@
 ln -fs /opt/front/upstart/front.conf /etc/init/front.conf
 mkdir -p /ida
 [ ! -L /ida/front ] && ln -s /opt/front /ida/front
-front run rake tmp:create
