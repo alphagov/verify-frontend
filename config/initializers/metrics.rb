dc6de77107329234d05d9cc8b65031b831fd207d
Veda Kanala <vedavalli.kanala@digital.cabinet-office.gov.uk>
2016-03-11 16:56:10 +0000

    3433 Set namespace / prefix so that all the metrics sent have it prefixed in the key.
    Authors: @veda @lloydnye

--- config/initializers/metrics.rb
+++ config/initializers/metrics.rb
@@ -2,7 +2,7 @@ require 'statsd-ruby'
 
 if CONFIG.metrics_enabled
   event_source = ActiveSupport::Notifications
-  statsd_client = Statsd.new(CONFIG.statsd_host, CONFIG.statsd_port)
+  statsd_client = Statsd.new(CONFIG.statsd_host, CONFIG.statsd_port).tap{|sd| sd.namespace = CONFIG.statsd_prefix}
   controller_action_reporter = Metrics::ControllerActionReporter.new(statsd_client)
   event_subscriber = Metrics::EventSubscriber.new(event_source)
   event_subscriber.subscribe(/process_action.action_controller/, controller_action_reporter)
