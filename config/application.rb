6a4b880b246da0275178082bf29cba3741508211
oswaldquek <oswald.quek@digital.cabinet-office.gov.uk>
2016-03-11 15:31:29 +0000

    3416: Configure LogstashLogger in initializers/

--- config/application.rb
+++ config/application.rb
@@ -33,13 +33,5 @@ module VerifyFrontend
       config.hide_locale = true
       config.available_locales = [:en,:cy]
     end
-
-    # we may be able to move this into config/environments/production.rb eventually, to remove an unnecessary dependency on LogStashLogger in development/test
-    LogStashLogger.configure do |config|
-      config.customize_event do |event|
-        event["SessionId"] = RequestStore.store[:session_id] || "no-current-session"
-        event["level"] = event.remove "severity"
-      end
-    end
   end
 end
