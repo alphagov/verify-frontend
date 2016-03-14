94c51c99a511d956d998cb513b205e1bfddf404a
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-03-14 11:57:07 +0000

    bau: add service-name as a field in logstash output
    Authors: @chrisholmes @robinwhittelton

--- config/initializers/logstash_logger.rb
+++ config/initializers/logstash_logger.rb
@@ -2,5 +2,6 @@ LogStashLogger.configure do |config|
   config.customize_event do |event|
     event["SessionId"] = RequestStore.store[:session_id] || "no-current-session"
     event["level"] = event.remove "severity"
+    event["service-name"] = "front"
   end
 end
