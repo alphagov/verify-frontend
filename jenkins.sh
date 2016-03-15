439772193bab24562ee3e2e27a2103bb9d48362f
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-03-15 10:35:49 +0000

    3456: copy /new-assets to /assets to satisfy pkgr
    pkgr uses a heroku buildpack to compile our app. The buildpack expects assets to
    be kept in /assets. As they are not there the compilation fails as it tries to
    compile them without the correct envvars.
    
    Authors: @chrisholmes @lloydnye

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,5 @@
 #/usr/bin/env sh
 RAILS_ENV=production bundle exec rake assets:precompile
 RAILS_ENV=production bundle exec rake tmp:clear
+cp -r public/new-assets public/assets
 pkgr package . --version=${BUILD_NUMBER} --iteration=1 --name=front
