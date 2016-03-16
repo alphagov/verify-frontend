ad6b239a0255efba2966c9603cf36a03e8fc08f3
Conor Glynn <ronocg@users.noreply.github.com>
2016-03-16 11:22:13 +0000

    3431 - frontend packaging
    tidy up this postinst with exit codes

--- packaging/postinst.sh
+++ packaging/postinst.sh
@@ -1,3 +1,13 @@
-ln -fs /opt/front/upstart/front.conf /etc/init/front.conf
-mkdir -p /ida
-[ ! -L /ida/front ] && ln -s /opt/front /ida/front
+#!/bin/bash
+set -e
+set -u
+
+if [ -e "/opt/front/upstart/front.conf" ];
+then
+     ln -fs /opt/front/upstart/front.conf /etc/init/front.conf
+     mkdir -p /ida
+     [ ! -L /ida/front ] && ln -s /opt/front /ida/front
+     exit 0;
+else
+exit 1;
+fi
