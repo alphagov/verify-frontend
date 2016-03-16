3ba8dad5ee6a2f92ae37660695f583f7d50949d2
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-03-16 15:18:37 +0000

    3470: Add piwik image to handle no-js analytics
    We're using plus signs instead of %20s, because rails seems to want to and piwik
    seems not to mind. Other than that the implementation is the same as the old
    frontend.
    
    Authors: @richardtowers @vixus0 @chrisholmes @lloydnye @thomasalanlee

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,17 @@ module ApplicationHelper
     content_for?(:piwik_custom_path)
   end
 
+  def piwik_noscript_query_string
+    hash = {
+        idsite: piwik.site_id,
+        rec: 1,
+        rand: Random.rand(2**32 - 1),
+        action_name: content_for(:page_title),
+    }
+    hash[:url] = piwik_custom_url if piwik_custom_url?
+    hash.to_query
+  end
+
   def uri_from_base(path)
     URI.join(request.base_url, path)
   end
--- spec/features/users_browser_sends_client_side_analytics_spec.rb
+++ spec/features/users_browser_sends_client_side_analytics_spec.rb
@@ -11,30 +11,65 @@ Pact.service_consumer "browser" do
 end
 
 RSpec.describe 'When the user visits the start page', pact: true do
-  it 'send a page view to analytics', js: true do
-    piwik.given("whatever")
-      .upon_receiving("a tracking request")
-      .with(method: :get, path: '/piwik.php', query: pact_query(
-        'action_name' => 'Start - GOV.UK Verify - GOV.UK',
-        'idsite' => '5'))
-      .will_respond_with(status: 200)
+  context 'when JS is enabled', js: true do
+    it 'sends a page view to analytics' do
+      piwik.given("whatever")
+        .upon_receiving("a tracking request")
+        .with(method: :get, path: '/piwik.php', query: pact_query(
+          'action_name' => 'Start - GOV.UK Verify - GOV.UK',
+          'idsite' => '5'))
+        .will_respond_with(status: 200)
 
-    set_session_cookies!
-    visit '/start'
-    expect(page).to have_content 'Sign in with GOV.UK Verify'
+      set_session_cookies!
+      visit '/start'
+      expect(page).to have_content 'Sign in with GOV.UK Verify'
+    end
+
+    it 'sends a page view with a custom url for error pages' do
+      stub_transactions_list
+      piwik.given("whatever")
+        .upon_receiving("an error tracking request")
+        .with(method: :get, path: '/piwik.php', query: pact_query(
+          'action_name' => 'Cookies Missing - GOV.UK Verify - GOV.UK',
+          'url' => Pact.term(generate: '/cookies-not-found', matcher: /cookies-not-found/)))
+        .will_respond_with(status: 200)
+
+      visit '/start'
+      expect(page).to have_content "If you can't access GOV.UK Verify from a service, enable your cookies."
+    end
   end
 
-  it 'sends a page view with a custom url for error pages', js: true do
-    stub_transactions_list
-    piwik.given("whatever")
-      .upon_receiving("an error tracking request")
-      .with(method: :get, path: '/piwik.php', query: pact_query(
-        'action_name' => 'Cookies Missing - GOV.UK Verify - GOV.UK',
-        'url' => Pact.term(generate: '/cookies-not-found', matcher: /cookies-not-found/)))
-      .will_respond_with(status: 200)
+  context 'when JS is disabled' do
+    it 'sends a page view to analytics' do
+      set_session_cookies!
+      visit '/start'
+      expect(page).to have_content 'Sign in with GOV.UK Verify'
+      noscript_image = page.find(:id, 'piwik-noscript-tracker')
+      expect(noscript_image).to_not be_nil
+      image_src = noscript_image['src']
+      expect(image_src).to match(/piwik.php\?/)
+      expect(image_src).to match(/idsite=5/)
+      expect(image_src).to match(/rec=1/)
+      expect(image_src).to match(/rand=\d+/)
+      expect(image_src).to match(/action_name=Start\+-\+GOV\.UK\+Verify\+-\+GOV\.UK/)
+      expect(image_src).to_not include('url')
+    end
 
-    visit '/start'
-    expect(page).to have_content "If you can't access GOV.UK Verify from a service, enable your cookies."
+    it 'sends a page view with a custom url for error pages' do
+      stub_transactions_list
+
+      visit '/start'
+      expect(page).to have_content "If you can't access GOV.UK Verify from a service, enable your cookies."
+      noscript_image = page.find(:id, 'piwik-noscript-tracker')
+      expect(noscript_image).to_not be_nil
+      image_src = noscript_image['src']
+      expect(image_src).to match(/piwik.php\?/)
+      expect(image_src).to match(/idsite=5/)
+      expect(image_src).to match(/rec=1/)
+      expect(image_src).to match(/rand=\d+/)
+      expect(image_src).to match(/action_name=Cookies\+Missing\+-\+GOV\.UK\+Verify\+-\+GOV\.UK/)
+      expect(image_src).to match(/url=[^&]+cookies-not-found/)
+    end
   end
 end
 
