7a8ad1a64e7aa3be515692f9ded4aae2d59d15ed
Leo Shaw <leo@leoshaw.co.uk>
2016-03-17 16:42:24 +0000

    3340: Add Welsh content to about-certified-companies
    Authors: @veda @oswaldquek @leoshaw

--- spec/features/user_visits_about_certified_companies_page_spec.rb
+++ spec/features/user_visits_about_certified_companies_page_spec.rb
@@ -5,21 +5,34 @@ RSpec.describe 'When the user visits the about certified companies page' do
   let(:simple_id) { 'stub-idp-one'}
   let(:idp_entity_id) { 'http://idcorp.com' }
 
-  it 'includes the appropriate feedback source' do
+  before(:each) do
     stub_request(:get, api_uri('session/idps')).to_return(body: [{'simpleId' => simple_id, 'entityId' => idp_entity_id}].to_json)
-
     set_session_cookies!
+  end
+
+  it 'includes the appropriate feedback source' do
     visit '/about-certified-companies'
 
     expect_feedback_source_to_be(page, 'ABOUT_CERTIFIED_COMPANIES_PAGE')
   end
 
-  it 'displays IdPs that are available for registration' do
-    stub_request(:get, api_uri('session/idps')).to_return(body: [{'simpleId' => simple_id, 'entityId' => idp_entity_id}].to_json)
+  it 'displays content in Welsh' do
+    visit '/am-gwmniau-ardystiedig'
 
-    set_session_cookies!
+    expect(page).to have_content 'Sut y gall cwmnïau wirio hunaniaeth'
+    expect(page).to have_content 'Gall y cwmnïau hyn yn defnyddio eu data eu hunain'
+  end
+
+  it 'displays IdPs that are enabled' do
     visit '/about-certified-companies'
 
     expect(page).to have_css("img[src*='#{simple_id}']")
+  end
+
+  it 'will show "How companies can verify identities" section' do
+    visit '/about-certified-companies'
+
+    expect(page).to have_content 'How companies can verify identities'
+    expect(page).to have_content 'These companies can use their own data'
   end
 end
