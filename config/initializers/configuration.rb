2c476fb3032eb24dac951b2696378f2c2b262f2c
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-03-18 10:52:58 +0000

    3433: don't require statsd config if metrics disabled
    Authors: @christopherholmes @robinwhittleton

--- config/initializers/configuration.rb
+++ config/initializers/configuration.rb
@@ -8,12 +8,14 @@ CONFIG = Configuration.load! do
   option_string 'zdd_file', 'ZDD_LATCH'
   option_string 'polling_wait_time', 'POLLING_WAIT_TIME'
   option_string 'api_cert_path', 'API_CERT_PATH', allow_missing: true
-  option_string 'statsd_host', 'STATSD_HOST'
-  option_string 'statsd_port', 'STATSD_PORT'
-  option_string 'statsd_prefix', 'STATSD_PREFIX'
-  option_int 'statsd_slice', 'STATSD_SLICE_IN_SECONDS'
-  option_int 'statsd_interval', 'STATSD_CLIENT_INTERVAL_IN_SECONDS'
   option_bool 'metrics_enabled', 'METRICS_ENABLED'
+  if metrics_enabled
+    option_string 'statsd_host', 'STATSD_HOST'
+    option_string 'statsd_port', 'STATSD_PORT'
+    option_string 'statsd_prefix', 'STATSD_PREFIX'
+    option_int 'statsd_slice', 'STATSD_SLICE_IN_SECONDS'
+    option_int 'statsd_interval', 'STATSD_CLIENT_INTERVAL_IN_SECONDS'
+  end
   option_string 'piwik_host', 'PIWIK_HOST', allow_missing: true
   option_int 'piwik_port', 'PIWIK_PORT', default: 443
   option_int 'piwik_site_id', 'PIWIK_SITE_ID', default: 1
