e28facfa2117f94bf8a6bec2725221e634119cb7
Veda Kanala <vedavalli.kanala@digital.cabinet-office.gov.uk>
2016-03-18 16:38:20 +0000

    3340 Refactored about choosing a company controller.
    Got rid of controller for about choosing a company page as we can render the same with
    about controller by excluding start layout for the action.
    
    Authors: @veda @leoshaw

--- app/controllers/about_choosing_a_company_controller.rb
+++
@@ -1,4 +0,0 @@
-class AboutChoosingACompanyController < ApplicationController
-  def index
-  end
-end
--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,10 +1,13 @@
 class AboutController < ApplicationController
-  layout 'start'
+  layout 'start', except: [:choosing_a_company]
 
   def index
   end
 
   def certified_companies
     @identity_providers = identity_provider_lister.list(cookies)
+  end
+
+  def choosing_a_company
   end
 end
