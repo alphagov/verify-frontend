9e265ea636432a3d1f8aea4af445f5a433b280f3
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-03-21 11:15:02 +0000

    bau: running tests explicity in jenkins.sh
    These probably should be switched with a rake task, but that's will be
    a separate commit.

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -eu
 bundle
+bundle exec govuk-lint-ruby app lib spec
+bundle exec govuk-lint-sass app/assets/stylesheets
+bundle exec rspec
+bundle exec rake spec:javascripts
 HEADLESS=true DISPLAY=:0 ./pre-commit.sh
 RAILS_ENV=production bundle exec rake assets:precompile
 RAILS_ENV=production bundle exec rake tmp:clear
