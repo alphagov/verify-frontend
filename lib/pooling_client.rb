9bd24c285779993d462ef257db72b583155df513
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-03-30 17:00:50 +0100

    3503: send headers and cookies with request statement
    Persistence appears to become broken when headers and cookies are not set on the
    same line as the request. Therefore, we have moved to the same line.
    
    There's an interesting quirk with cookies following this change. The client
    library was ignoring the key fields in the cookie hash. To overcome this quickly
    we have a workaround to transform the value fields into a cookie object that
    contains a key and value.
    
    Authors: @chrisholmes @davbo

--- lib/pooling_client.rb
+++ lib/pooling_client.rb
@@ -6,38 +6,44 @@ class PoolingClient
   def initialize(host, headers = {})
     @client_pool = ConnectionPool.new(size: 36, timeout: 5) do
       HTTP
-        .persistent(host)
         .headers(headers)
+        .persistent(host)
     end
   end
 
   def get(path, options = {})
+    cookies = transform_cookies(options.fetch(:cookies, {}))
     client_pool.with do |client|
       client
-        .cookies(options.fetch(:cookies, {}))
-        .headers(options.fetch(:headers, {}))
-        .get(path, params: options[:params])
+        .get(path, options.merge(cookies: cookies))
         .flush
     end
   end
 
   def post(path, body, options = {})
+    cookies = transform_cookies(options.fetch(:cookies, {}))
     client_pool.with do |client|
       client
-        .cookies(options.fetch(:cookies, {}))
-        .headers(options.fetch(:headers, {}))
-        .post(path, json: body)
+        .post(path, json: body, cookies: cookies)
         .flush
     end
   end
 
   def put(path, body, options = {})
+    cookies = transform_cookies(options.fetch(:cookies, {}))
     client_pool.with do |client|
       client
-        .cookies(options.fetch(:cookies, {}))
-        .headers(options.fetch(:headers, {}))
-        .put(path, json: body)
+        .put(path, options.merge(json: body, cookies: cookies))
         .flush
     end
+  end
+
+private
+
+  def transform_cookies(cookies)
+    cookies.inject({}) { |jar, (name, value)|
+      jar[name] = HTTP::Cookie.new(name, value)
+      jar
+    }
   end
 end
