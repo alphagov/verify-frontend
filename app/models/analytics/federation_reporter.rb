dd0942b078a18c23b3eaac361f82f2d78fa43dae
Veda Kanala <vedavalli.kanala@digital.cabinet-office.gov.uk>
2016-04-01 11:07:40 +0100

    3495 Refactored federation reporter to get rid of duplication
    Authors: @veda @lloydnye

--- app/models/analytics/federation_reporter.rb
+++ app/models/analytics/federation_reporter.rb
@@ -6,21 +6,22 @@ module Analytics
     end
 
     def report_sign_in(transaction_simple_id, request)
-      transaction_analytics_description = @federation_translator.translate(
-        "rps.#{transaction_simple_id}.analyticsDescription")
-      ANALYTICS_REPORTER.report_custom_variable(
-        request,
-        "The No option was selected on the introduction page #{transaction_analytics_description}",
-        Analytics::CustomVariable.build(:rp, transaction_analytics_description))
+      report_action(transaction_simple_id, request, 'The No option was selected on the introduction page')
     end
 
     def report_registration(transaction_simple_id, request)
+      report_action(transaction_simple_id, request, 'The Yes option was selected on the start page')
+    end
+
+  private
+
+    def report_action(transaction_simple_id, request, action)
       transaction_analytics_description =
         FEDERATION_TRANSLATOR.translate("rps.#{transaction_simple_id}.analyticsDescription")
 
       ANALYTICS_REPORTER.report_custom_variable(
         request,
-        "The Yes option was selected on the start page #{transaction_analytics_description}",
+        "#{action} #{transaction_analytics_description}",
         Analytics::CustomVariable.build(:rp, transaction_analytics_description))
     end
   end
