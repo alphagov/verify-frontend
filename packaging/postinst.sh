8969e78f7beeb78744a07abe86a8ff892d6e2fec
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-04-04 10:10:01 +0100

    3431: set file ownership to root
    all files aside from log and tmp directories should now be owned by root
    
    Authors: @jinnko @chrisholmes

--- packaging/postinst.sh
+++ packaging/postinst.sh
@@ -8,3 +8,6 @@ ln -fs /opt/front/upstart/front.conf /etc/init/front.conf
 
 # We want to ensure upstart realizes it's config may have changed.
 /sbin/initctl reload-configuration
+
+chown -R deployer:deployer /opt/front/log
+chown -R deployer:deployer /opt/front/tmp
