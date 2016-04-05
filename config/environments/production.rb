adf581e94dff9ceb39971319a67933ae6593deaf
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-04-05 17:18:07 +0100

    3517: enable force_ssl for production mode
    Authors: @chrisholmes @davbo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Log level is configurable depending on environment.
   config.log_level = ENV.fetch('LOG_LEVEL', :info).to_sym
