8863b92fdf8845de8efef8308cc6fb621aa33280
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-04-07 12:38:07 +0100

    3517: add more sensible defaults for config
    Adds defaults for configuration that is shared across environments.
    
    This should allow us to trim down the config files

--- config/initializers/configuration.rb
+++ config/initializers/configuration.rb
@@ -2,19 +2,17 @@ require 'configuration'
 CONFIG = Configuration.load! do
   option_string 'rp_display_locales', 'RP_DISPLAY_LOCALES'
   option_string 'idp_display_locales', 'IDP_DISPLAY_LOCALES'
-  option_string 'session_cookie_duration', 'SESSION_COOKIE_DURATION_IN_HOURS'
+  option_string 'session_cookie_duration', 'SESSION_COOKIE_DURATION_IN_HOURS', default: 2
   option_string 'api_host', 'API_HOST'
   option_string 'logo_directory', 'LOGO_DIRECTORY'
   option_string 'white_logo_directory', 'WHITE_LOGO_DIRECTORY'
   option_string 'zdd_file', 'ZDD_LATCH'
-  option_string 'polling_wait_time', 'POLLING_WAIT_TIME'
-  option_bool 'metrics_enabled', 'METRICS_ENABLED'
+  option_string 'polling_wait_time', 'POLLING_WAIT_TIME', default: 6
+  option_bool 'metrics_enabled', 'METRICS_ENABLED', default: true
   if metrics_enabled
-    option_string 'statsd_host', 'STATSD_HOST'
-    option_string 'statsd_port', 'STATSD_PORT'
+    option_string 'statsd_host', 'STATSD_HOST', default: '127.0.0.1'
+    option_string 'statsd_port', 'STATSD_PORT', default: 8125
     option_string 'statsd_prefix', 'STATSD_PREFIX'
-    option_int 'statsd_slice', 'STATSD_SLICE_IN_SECONDS'
-    option_int 'statsd_interval', 'STATSD_CLIENT_INTERVAL_IN_SECONDS'
   end
   option_string 'internal_piwik_host', 'INTERNAL_PIWIK_HOST', allow_missing: true
   option_string 'public_piwik_host', 'PUBLIC_PIWIK_HOST', allow_missing: true
