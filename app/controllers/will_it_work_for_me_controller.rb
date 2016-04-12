a455308d49193f46bc12e760ec085fa577e8d0ca
Veda Kanala <vedavalli.kanala@digital.cabinet-office.gov.uk>
2016-04-12 16:26:07 +0100

    3497: Passing selected evidence over to the next page.
    -Retain selected evidence and pass it over to choose a certified company page as query params.
    
    Authors: @veda @leoshaw @yolinas @vixus0

--- app/controllers/will_it_work_for_me_controller.rb
+++ app/controllers/will_it_work_for_me_controller.rb
@@ -6,13 +6,16 @@ class WillItWorkForMeController < ApplicationController
   def will_it_work_for_me
     @form = WillItWorkForMeForm.new(params[:will_it_work_for_me_form])
     if @form.resident_last_12_months?
-      redirect_to choose_a_certified_company_path
+      path_to_redirect = choose_a_certified_company_path
     elsif @form.address_but_not_resident?
-      redirect_to may_not_work_if_you_live_overseas_path
+      path_to_redirect = may_not_work_if_you_live_overseas_path
     elsif @form.no_uk_address?
-      redirect_to will_not_work_without_uk_address_path
+      path_to_redirect = will_not_work_without_uk_address_path
     else
-      redirect_to why_might_this_not_work_for_me_path
+      path_to_redirect = why_might_this_not_work_for_me_path
     end
+    uri = URI(path_to_redirect)
+    uri.query = request.query_string
+    redirect_to uri.to_s
   end
 end
--- spec/features/user_visits_will_it_work_for_me_page_spec.rb
+++ spec/features/user_visits_will_it_work_for_me_page_spec.rb
@@ -10,15 +10,17 @@ RSpec.describe 'When the user visits the will it work for me page' do
     expect_feedback_source_to_be(page, 'WILL_IT_WORK_FOR_ME_PAGE')
   end
 
-  it 'redirects to the choose-a-company page when user is over 20 and is a uk resident' do
+  #JS has to be on, so it uses the real browser and query params can be inspected
+  it 'redirects to the choose-a-company page when user is over 20 and is a uk resident', js: true do
     stub_federation
-    visit '/will-it-work-for-me'
+    visit '/will-it-work-for-me?selected-evidence=driving_licence&selected-evidence=landline'
 
     choose 'will_it_work_for_me_form_above_age_threshold_true'
     choose 'will_it_work_for_me_form_resident_last_12_months_true'
     click_button 'Continue'
 
     expect(page).to have_current_path(choose_a_certified_company_path, only_path: true)
+    expect(query_params['selected-evidence'].to_set).to eql %w(driving_licence landline).to_set
   end
 
   it 'redirects to the why-might-this-not-work-for-me page when user is over 20 and has moved to the uk in the last 12 months' do
