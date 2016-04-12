b76b90c90f03515ebb8aed350e7e3960bfeddb3b
Lloyd Nye <lloyd.nye@digital.cabinet-office.gov.uk>
2016-04-12 17:10:51 +0100

    3491: small refactor of structure of evidence checker
    Authors: @chrisholmes @lloydnye

--- app/models/idp_eligibility/checker.rb
+++ app/models/idp_eligibility/checker.rb
@@ -5,27 +5,35 @@ module IdpEligibility
     end
 
     def any?(evidence, enabled_idps)
+      evidence_set = evidence.to_set
       matching_idps = @rules_repository.rules.select do |_, evidence_collection|
-        evidence_collection.any? { |evidence_items| evidence_items.to_set.subset?(evidence.to_set) }
+        evidence_collection
+          .map(&:to_set)
+          .any? { |evidence_rule| evidence_rule.subset?(evidence_set) }
       end
-      (matching_idps.keys & enabled_idps).length > 0
+      recommended_idps = matching_idps.keys
+      (recommended_idps & enabled_idps).length > 0
     end
 
     def any_for_documents?(evidence, enabled_idps)
-      idps = idps_at_document_stage(evidence) & enabled_idps
-      idps.length > 0
+      recommended_idps = idps_at_document_stage(evidence)
+      (recommended_idps & enabled_idps).length > 0
     end
 
   private
 
-    def docs_only
+    def docs_only_mask
       [:passport, :driving_licence, :non_uk_id_document].to_set
     end
 
     def idps_at_document_stage(evidence)
-      user_docs = evidence.to_set
-      matching_idps = @rules_repository.rules.select do |_, evidence_collection|
-        evidence_collection.any? { |evidence_items| (evidence_items.to_set & docs_only).subset?(user_docs) }
+      evidence_set = evidence.to_set
+      matching_idps = @rules_repository.rules.select do |_, evidence_rule_collection|
+        evidence_rule_collection
+          .lazy
+          .map(&:to_set)
+          .map { |evidence_rule| evidence_rule & docs_only_mask }
+          .any? { |evidence_rule| evidence_rule.subset?(evidence_set) }
       end
       matching_idps.keys
     end
