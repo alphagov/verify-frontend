56850bddc872c2944769e9cc82980e8ab0bbeb30
Veda Kanala <vedavalli.kanala@digital.cabinet-office.gov.uk>
2016-04-14 16:07:01 +0100

    3497 Added missing scenarios to js specs
    - Added will it work for me js test scenarios
    - Removed redundant ones
    - Reworded tests such that they reflect what they are testing.
    
    Authors: @veda @yolinas

--- spec/javascripts/will_it_work_for_me_spec.js
+++ spec/javascripts/will_it_work_for_me_spec.js
@@ -69,38 +69,29 @@ describe("Will it work for me form", function () {
         this.submitForm = function () {
             willItWorkForMeForm.triggerHandler('submit');
         };
-        this.checkOverAgeThreshold = function () {
+        this.selectAboveAgeThreshold = function () {
             this.checkRadio(willItWorkForMeForm.find('input[name="will_it_work_for_me_form[above_age_threshold]"][value=true]'));
         };
-        this.checkResident12Months = function () {
+        this.selectResident12Months = function () {
             this.checkRadio(willItWorkForMeForm.find('input[name="will_it_work_for_me_form[resident_last_12_months]"][value=true]'));
         };
-        this.checkNotResident12Months = function () {
+        this.selectNotResident12Months = function () {
             this.checkRadio(willItWorkForMeForm.find('input[name="will_it_work_for_me_form[resident_last_12_months]"][value=false]'));
         };
-        this.checkNoUKAddress = function () {
+        this.selectNoUKAddress = function () {
             this.checkRadio(willItWorkForMeForm.find('input[name="will_it_work_for_me_form[not_resident_reason]"][value=NoAddress]'));
         };
-        this.checkMovedToUKInLast12Months = function () {
-            this.checkRadio(willItWorkForMeForm.find('#will_it_work_for_me_form_not_resident_reason_movedrecently'));
-        };
+
         this.checkRadio = function (el) {
             el.prop('checked', true).trigger('click');
         };
     });
-
-    function answerQuestion(document, answer) {
-        wilItWorkForMeForm.find('input[name="will_it_work_for_me_form[' + document + ']"][value=' + answer + ']')
-            .prop('checked', true)
-            .trigger('click');
-    }
 
     function submitForm() {
         wilItWorkForMeForm.triggerHandler('submit')
     }
 
     function expectErrorMessage() {
-        expect(willItWorkForMeForm.children('.form-group:first').is('.error')).toBe(true);
         expect(willItWorkForMeForm.find('#validation-error-message-js').text()).toBe("Please answer all the questions");
     }
 
@@ -117,68 +108,63 @@ describe("Will it work for me form", function () {
         expectNoError();
     });
 
-    it("should not initially show the which of these applies to you section.", function () {
+    it("should not initially show 'not-resident-reason' section.", function () {
         expect(willItWorkForMeForm.find('#not_resident_reason').attr("class")).toContain("js-hidden");
     });
 
-    it("should show the which of these applies section when user indicates no residency for 12 months", function () {
-        this.checkNotResident12Months();
+    it("should show 'not-resident-reason' section when user indicates no residency for 12 months", function () {
+        this.selectNotResident12Months();
 
         expect(willItWorkForMeForm.find('#not_resident_reason').attr("class")).not.toContain("js-hidden");
     });
 
-    it("should not show the which of these applies section when user indicates residency for 12 months", function () {
-        this.checkResident12Months();
+    it("should not show 'not-resident-reason' section when user indicates residency for 12 months", function () {
+        this.selectResident12Months();
 
         expect(willItWorkForMeForm.find('#not_resident_reason').attr("class")).toContain("js-hidden");
     });
 
-    it("should have errors on submit if user does not answer over the age threshold", function () {
+    it("should have errors on submit if user does not answer any questions", function () {
         this.submitForm();
+        expect(willItWorkForMeForm.children('#above-age-threshold').attr("class")).toContain('error');
+        expect(willItWorkForMeForm.children('#resident_last_12_months').attr("class")).toContain('error');
+        expectErrorMessage();
+    });
 
+    it("should have errors on submit if user does not answer over the age threshold", function () {
+        this.selectNotResident12Months();
+        this.submitForm();
         expect(willItWorkForMeForm.children('#above-age-threshold').attr("class")).toContain('error');
         expectErrorMessage();
     });
 
     it("should have errors on submit if the residency question isn't answered", function () {
+        this.selectAboveAgeThreshold();
         this.submitForm();
-
         expect(willItWorkForMeForm.children('#resident_last_12_months').attr("class")).toContain('error');
         expectErrorMessage();
     });
 
     it("should have errors on submit if user indicates not resident for 12 months and does not supply reason", function () {
-        this.checkNotResident12Months();
-
+        this.selectNotResident12Months();
         this.submitForm();
-
         expect(willItWorkForMeForm.children('#not_resident_reason').attr("class")).toContain('error');
         expectErrorMessage();
     });
 
     it("should have no errors on submit if user has answered over age threshold and lived in UK for 12 months", function () {
-        this.checkOverAgeThreshold();
-        this.checkResident12Months();
+        this.selectAboveAgeThreshold();
+        this.selectResident12Months();
 
         this.submitForm();
 
         expectNoError();
     });
 
-    it("should have no errors on submit if user has indicated over age threshold and lived in UK for 12 months and no uk address", function () {
-        this.checkOverAgeThreshold();
-        this.checkNotResident12Months();
-        this.checkNoUKAddress();
-
-        this.submitForm();
-
-        expectNoError();
-    });
-
-    it("should have no errors on submit if user has indicated over age threshold and lived in UK for 12 months and uk address", function () {
-        this.checkOverAgeThreshold();
-        this.checkNotResident12Months();
-        this.checkMovedToUKInLast12Months();
+    it("should have no errors on submit if user has answered above age threshold and lived in UK for 12 months and has chosen the reason", function () {
+        this.selectAboveAgeThreshold();
+        this.selectNotResident12Months();
+        this.selectNoUKAddress();
 
         this.submitForm();
 
