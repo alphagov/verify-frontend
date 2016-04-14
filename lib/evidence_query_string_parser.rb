7bd52966829abda83e18a865493b034e1fcebe7a
Anshul Sirur <vixus0@gmail.com>
2016-04-14 17:59:49 +0100

    3491: Remove lib/evidence_query_string_parser
    Due to a rebase on master, the old version of this file was added.
    
    Authors: @vixus0 @lloydnye

--- lib/evidence_query_string_parser.rb
+++
@@ -1,9 +0,0 @@
-require 'query_string_parser'
-
-class EvidenceQueryStringParser
-  def self.parse(query_string)
-    query_params = QueryStringParser.parse(query_string)
-    selected_evidence = query_params.fetch('selected-evidence', [])
-    ALL_ATTRIBUTES.select { |evidence| selected_evidence.include? evidence.to_s }
-  end
-end
