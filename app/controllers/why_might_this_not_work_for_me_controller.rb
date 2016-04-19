cf57ac922c0cffbac28cb62d2454e15897d7aa0b
oswaldquek <oswald.quek@digital.cabinet-office.gov.uk>
2016-04-19 16:37:10 +0100

    3532: code quality improvements
    @oswaldquek @richardtowers

--- app/controllers/why_might_this_not_work_for_me_controller.rb
+++ app/controllers/why_might_this_not_work_for_me_controller.rb
@@ -2,7 +2,7 @@ class WhyMightThisNotWorkForMeController < ApplicationController
   def index
     uri = URI(choose_a_certified_company_path)
     uri.query = request.query_string
-    @try_to_verify = uri
+    @try_to_verify = uri.to_s
     transaction_details = TRANSACTION_INFO_GETTER.get_info(cookies)
     @other_ways_description = transaction_details.other_ways_description
     @other_ways_text = transaction_details.other_ways_text
