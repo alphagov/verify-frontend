e253ab7f4a20dca6a5bb9d78b77365ee93e74b06
oswaldquek <oswald.quek@digital.cabinet-office.gov.uk>
2016-04-19 17:18:16 +0100

    3532: Show "Other ways" text and description
    Solo: @oswaldquek

--- app/controllers/will_not_work_without_uk_address_controller.rb
+++ app/controllers/will_not_work_without_uk_address_controller.rb
@@ -1,2 +1,7 @@
 class WillNotWorkWithoutUkAddressController < ApplicationController
+  def index
+    transaction_details = TRANSACTION_INFO_GETTER.get_info(cookies)
+    @other_ways_description = transaction_details.other_ways_description
+    @other_ways_text = transaction_details.other_ways_text
+  end
 end
---
+++ spec/features/user_visits_will_not_work_without_uk_address_page_spec.rb
@@ -0,0 +1,16 @@
+require 'feature_helper'
+
+RSpec.describe 'When the user visits the will-not-work-without-uk-address page' do
+  before(:each) do
+    set_session_cookies!
+    stub_federation
+  end
+
+  it 'includes other ways text' do
+    visit '/will-not-work-without-uk-address'
+
+    expect(page).to have_content("If you can't verify your identity using GOV.UK Verify, you can register for an identity profile here")
+    expect(page).to have_content('register for an identity profile')
+    expect(page).to have_link 'here', href: 'http://www.example.com'
+  end
+end
