8cbbbe6005dcc0eb26b0dc50444061f0980cc381
Leo Shaw <leo@leoshaw.co.uk>
2016-04-25 09:54:17 +0100

    3530: Add dead-end page for no documents
    This adds the dead-end page that the user will see if no IdPs can verify
    at the 'select documents' stage.  It's not currently possible to hit
    this page because at least one IdP can verify with no documents, but
    it's good to have this page as a fall-back in case that changes.

--- app/controllers/unlikely_to_verify_controller.rb
+++ app/controllers/unlikely_to_verify_controller.rb
@@ -1,4 +1,7 @@
 class UnlikelyToVerifyController < ApplicationController
   def index
+    transaction_details = TRANSACTION_INFO_GETTER.get_info(cookies)
+    @other_ways_description = transaction_details.other_ways_description
+    @other_ways_text = transaction_details.other_ways_text
   end
 end
