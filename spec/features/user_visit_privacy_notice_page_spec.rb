c2f1a492750bb9bd303f191c1571d8b1b7c9c43b
oswaldquek <oswald.quek@digital.cabinet-office.gov.uk>
2016-05-03 17:17:59 +0100

    3529: Port privacy notice page
    English and Welsh have static pages
    
    solo: @oswaldquek

---
+++ spec/features/user_visit_privacy_notice_page_spec.rb
@@ -0,0 +1,23 @@
+require 'feature_helper'
+
+RSpec.describe 'When the user visits the privacy notice page' do
+  it 'displays the page in English' do
+    visit '/privacy-notice'
+    expect(page).to have_title 'Privacy notice - GOV.UK Verify - GOV.UK'
+    expect(page).to have_link 'Summary', href: '#summary'
+    expect(page).to have_content 'GOV.UK Verify is provided by the Government Digital Service (GDS). We use your personal data to help you to transact securely with government services.'
+    expect(page).to have_link 'Privacy policies of certified companies and government departments', href: '#privacy-policies-of-certified-companies-and-government-departments'
+    expect(page).to have_content 'The certified companies have their own privacy policies that apply to their handling of your personal data as part of the identity verification process. Likewise, the government services have their own privacy policies that apply to their handling of your personal data when you use their services.'
+  end
+
+  it 'displays the page in Welsh' do
+    visit '/privacy-notice-cy'
+    expect(page).to have_content("Mae GOV.UK Verify wrthi’n cael ei gyfieithu i’r Gymraeg.")
+    #TODO update test when Welsh translations are in
+  end
+
+  it 'includes the appropriate feedback source' do
+    visit '/privacy-notice'
+    expect_feedback_source_to_be(page, 'PRIVACY_NOTICE_PAGE')
+  end
+end
