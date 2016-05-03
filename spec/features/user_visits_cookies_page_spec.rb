3e0057f123f97345e14147ddae7501c6dcc2dfab
Anshul Sirur <vixus0@gmail.com>
2016-05-03 17:29:49 +0100

    3524: Add verify-front-journey-hint to cookies info page
    Authors: @vixus0 @thomaslanlee

--- spec/features/user_visits_cookies_page_spec.rb
+++ spec/features/user_visits_cookies_page_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe 'When the user visits the cookies page' do
     expect(page).to have_content('_pk_ses')
     expect(page).to have_content('_pk_ref')
     expect(page).to have_content('verify-journey-hint')
+    expect(page).to have_content('verify-front-journey-hint')
     expect(page).to have_content('PIWIK_VISITOR_ID')
     expect(page).to have_content('ab_test')
   end
