d5c907c56111478f7c67b6a370c89e4946887992
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-05-04 14:13:16 +0100

    3609: rename RulesLoader to ProfilesLoader
    Authors: @chrisholmes @davbo

--- config/initializers/idp_eligibility.rb
+++ config/initializers/idp_eligibility.rb
@@ -1,7 +1,7 @@
-rules_repository = IdpEligibility::RulesLoader.new(CONFIG.rules_directory).load
+profiles_repository = IdpEligibility::ProfilesLoader.new(CONFIG.rules_directory).load
 
-DOCUMENTS_ELIGIBILITY_CHECKER = IdpEligibility::Checker.new(rules_repository.document_rules)
+DOCUMENTS_ELIGIBILITY_CHECKER = IdpEligibility::Checker.new(profiles_repository.document_profiles)
 
-IDP_ELIGIBILITY_CHECKER = IdpEligibility::Checker.new(rules_repository.all_rules)
+IDP_ELIGIBILITY_CHECKER = IdpEligibility::Checker.new(profiles_repository.all_profiles)
 
-IDP_RECOMMENDATION_GROUPER = IdpEligibility::RecommendationGrouper.new(rules_repository.recommended_rules, rules_repository.non_recommended_rules)
+IDP_RECOMMENDATION_GROUPER = IdpEligibility::RecommendationGrouper.new(profiles_repository.recommended_profiles, profiles_repository.non_recommended_profiles)
---
+++ spec/models/idp_eligibility/profiles_loader_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+require 'idp_eligibility/rules_repository'
+require 'idp_eligibility/profiles_loader'
+require 'idp_eligibility/profile'
+
+module IdpEligibility
+  describe ProfilesLoader do
+    def fixtures(data = '')
+      File.join('spec', 'fixtures', data)
+    end
+
+    describe '#load' do
+      it 'should load recommended profiles from YAML files' do
+        evidence = [Profile.new(%i(passport driving_licence))]
+        profiles_repository = RulesRepository.new(
+          'example-idp' => evidence,
+          'example-idp-two' => evidence,
+          'example-idp-stub' => evidence
+        )
+        expect(ProfilesLoader.new(fixtures('good_profiles')).load.recommended_profiles).to eq(profiles_repository)
+      end
+
+      it 'should load non recommended profiles from YAML files' do
+        evidence = [Profile.new(%i(passport mobile_phone))]
+        profiles_repository = RulesRepository.new(
+          'example-idp' => evidence,
+          'example-idp-stub' => evidence,
+          'example-idp-two' => [],
+        )
+        expect(ProfilesLoader.new(fixtures('good_profiles')).load.non_recommended_profiles).to eq(profiles_repository)
+      end
+
+      it 'should load all profiles from YAML files' do
+        evidence = [Profile.new(%i{passport driving_licence}), Profile.new(%i(passport mobile_phone))]
+        profiles_repository = RulesRepository.new(
+          'example-idp' => evidence,
+          'example-idp-stub' => evidence,
+          'example-idp-two' => [Profile.new(%i{passport driving_licence})]
+        )
+        expect(ProfilesLoader.new(fixtures('good_profiles')).load.all_profiles).to eq(profiles_repository)
+      end
+
+      it 'should supply a seperate repository of document profiles' do
+        evidence = [Profile.new(%i{passport driving_licence}), Profile.new(%i(passport))]
+        profiles_repository = RulesRepository.new(
+          'example-idp' => evidence,
+          'example-idp-stub' => evidence,
+          'example-idp-two' => [Profile.new(%i{passport driving_licence})]
+        )
+        expect(ProfilesLoader.new(fixtures('good_profiles')).load.document_profiles).to eq(profiles_repository)
+      end
+
+      it 'should raise an error when expected keys are missing from yaml' do
+        expect {
+          ProfilesLoader.new(fixtures('bad_profiles')).load
+        }.to raise_error KeyError
+      end
+
+      it 'should return an empty object when no yaml files found' do
+        expect(ProfilesLoader.new(fixtures).load.all_profiles).to eq(RulesRepository.new({}))
+      end
+
+      it 'should return the hints configuration' do
+        expected_hints = ['example-idp', 'example-idp-stub']
+        expect(ProfilesLoader.new(fixtures('good_profiles')).load.idps_with_hints).to eql(expected_hints)
+      end
+    end
+  end
+end
