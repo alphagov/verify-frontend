56d4b8b0eeeb373f5b0c6c8ff9ec1afb46a0fead
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-05-04 13:55:59 +0100

    3578: Refator feature tests
    There were a couple of tests performing the same actions with differing
    assertions. We've consolidated these into one test and moved tests into
    contexts.
    
    Authors: @richardTowers @lloydnye

--- spec/features/user_visits_may_not_work_if_you_live_overseas_spec.rb
+++ spec/features/user_visits_may_not_work_if_you_live_overseas_spec.rb
@@ -5,39 +5,24 @@ RSpec.describe 'When the user visits the may-not-work-if-you-live-overseas page'
     set_session_cookies!
   end
 
-  context 'with javascript enabled', js: true do
-    it 'contains the choose certified company link with selected evidence params' do
+  context 'session contains transaction id' do
+    it 'includes the appropriate feedback source amd other ways text' do
       page.set_rack_session('transaction_simple_id' => 'test-rp')
       visit '/may-not-work-if-you-live-overseas'
 
-      expect(page).to have_link "I’d like to try to verify my identity online", href: "/choose-a-certified-company"
+      expect_feedback_source_to_be(page, 'MAY_NOT_WORK_IF_YOU_LIVE_OVERSEAS_PAGE')
+      expect(page).to have_content('If you can’t verify your identity using GOV.UK Verify, you can register for an identity profile here')
+      expect(page).to have_content('register for an identity profile')
+      expect(page).to have_link 'here', href: 'http://www.example.com'
     end
-  end
-
-  it 'includes the appropriate feedback source' do
-    page.set_rack_session('transaction_simple_id' => 'test-rp')
-    visit '/may-not-work-if-you-live-overseas'
-
-    expect_feedback_source_to_be(page, 'MAY_NOT_WORK_IF_YOU_LIVE_OVERSEAS_PAGE')
-  end
-
-  it 'includes other ways text' do
-    page.set_rack_session('transaction_simple_id' => 'test-rp')
-    visit '/may-not-work-if-you-live-overseas'
-
-    expect(page).to have_content('If you can’t verify your identity using GOV.UK Verify, you can register for an identity profile here')
-    expect(page).to have_content('register for an identity profile')
-    expect(page).to have_link 'here', href: 'http://www.example.com'
   end
 
   context 'session does not contain transaction id' do
-    before(:each) do
+    it 'includes the appropriate feedback source amd other ways text' do
       stub_federation
-    end
-
-    it 'includes other ways text' do
       visit '/may-not-work-if-you-live-overseas'
 
+      expect_feedback_source_to_be(page, 'MAY_NOT_WORK_IF_YOU_LIVE_OVERSEAS_PAGE')
       expect(page).to have_content('If you can’t verify your identity using GOV.UK Verify, you can register for an identity profile here')
       expect(page).to have_content('register for an identity profile')
       expect(page).to have_link 'here', href: 'http://www.example.com'
