5c42130c056e650bebac477f962dc75f44c41efa
Anshul Sirur <vixus0@gmail.com>
2016-05-05 17:08:36 +0100

    3524: Presentation updates for non-repudiation page
    Change button text to "Sign in with" instead of "Select" and add the
    Verify logo. Added a test to check that the cookie is updated if the
    user goes back and selects another IDP for sign in.
    
    Authors: @vixus0 @thomasalanlee

--- spec/features/user_visits_confirm_your_identity_page_spec.rb
+++ spec/features/user_visits_confirm_your_identity_page_spec.rb
@@ -1,27 +1,33 @@
 require 'feature_helper'
 
 RSpec.describe 'When the user visits the confirm-your-identity page' do
-  let(:location) { '/test-idp-request-endpoint' }
+  let(:idp_location) { '/test-idp-request-endpoint' }
   let(:encrypted_entity_id) { 'an-encrypted-entity-id' }
   let(:originating_ip) { '<PRINCIPAL IP ADDRESS COULD NOT BE DETERMINED>' }
-  let(:response) {
+
+  def response(location)
     {
         'location' => location,
         'samlRequest' => 'a-saml-request',
         'relayState' => 'a-relay-state',
         'registration' => false
     }
-  }
+  end
+
+  def stub_api_and_analytics(idp_location)
+    stub_federation
+    stub_request(:put, api_uri('session/select-idp'))
+        .to_return(body: { 'encryptedEntityId' => encrypted_entity_id }.to_json)
+    stub_request(:get, api_uri('session/idp-authn-request'))
+        .with(query: { 'originatingIp' => originating_ip }).to_return(body: response(idp_location).to_json)
+    stub_request(:get, INTERNAL_PIWIK.url).with(query: hash_including({}))
+  end
+
   describe 'and the journey hint cookie is set' do
     before(:each) do
-      stub_federation
+      stub_api_and_analytics(idp_location)
       set_session_cookies!
       set_journey_hint_cookie('http://idcorp.com')
-      stub_request(:put, api_uri('session/select-idp'))
-          .to_return(body: { 'encryptedEntityId' => encrypted_entity_id }.to_json)
-      stub_request(:get, api_uri('session/idp-authn-request'))
-          .with(query: { 'originatingIp' => originating_ip }).to_return(body: response.to_json)
-      stub_request(:get, INTERNAL_PIWIK.url).with(query: hash_including({}))
     end
 
     it 'displays the page in Welsh' do
@@ -30,7 +36,7 @@ RSpec.describe 'When the user visits the confirm-your-identity page' do
       expect(page).to have_css 'html[lang=cy]'
     end
 
-    it 'displays the page in English', js: true do
+    it 'displays the page in English' do
       visit '/confirm-your-identity'
       expect(page).to have_title 'Confirm your identity - GOV.UK Verify - GOV.UK'
       expect(page).to have_css 'html[lang=en]'
@@ -53,25 +59,43 @@ RSpec.describe 'When the user visits the confirm-your-identity page' do
 
     it 'should display only the idp that the user last verified with' do
       visit '/confirm-your-identity'
-      expect(page).to have_button 'Select IDCorp'
+      expect(page).to have_button 'Sign in with IDCorp'
       expect(page).to have_css('.company', count: 1)
     end
 
     describe 'and js is disabled' do
       it 'should display the interstitial page' do
         visit '/confirm-your-identity'
-        click_button 'Select IDCorp'
+        click_button 'Sign in with IDCorp'
         expect(page).to have_current_path('/redirect-to-idp')
         click_button 'Continue'
-        expect(page).to have_current_path('/test-idp-request-endpoint')
+        expect(page).to have_current_path(idp_location)
       end
     end
 
     describe 'and js is enabled', js: true do
       it 'should redirect to the IDP sign in page' do
         visit '/confirm-your-identity'
-        click_button 'Select IDCorp'
-        expect(page).to have_current_path('/test-idp-request-endpoint')
+        click_button 'Sign in with IDCorp'
+        expect(page).to have_current_path(idp_location)
+      end
+
+      it 'should allow the user to select a new IDP and update the cookie' do
+        # User has previously chosen IDCorp
+        visit '/confirm-your-identity'
+        expect(page).to have_button('Sign in with IDCorp')
+
+        # User returns to sign in page and selects a new IDP
+        click_link 'sign in with a different certified company'
+        new_idp_location = '/another-idp-endpoint'
+        stub_api_and_analytics(new_idp_location)
+        click_button 'Select Bob'
+        expect(page).to have_current_path(new_idp_location)
+
+        # The new IDP is displayed for non-repudiation
+        visit '/confirm-your-identity'
+        click_button 'Sign in with Bob'
+        expect(page).to have_current_path(new_idp_location)
       end
     end
   end
