61d411c9b58d7b96f9e6c3ad894888232fe8ca53
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-05-13 12:00:27 +0100

    bau: Add "funky" pass and fail messages to pre-commit
    This doesn't otherwise change the behaviour of pre-commit - it still
    runs all the tests even if some fail.
    
    This doesn't add checking of Jenkins or anything clever, it's literally
    just the big text.
    
    Solo: @richardtowers

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
+. scripts/deploy.sh
+
 bundle exec govuk-lint-ruby app config lib spec
+success=$?
 bundle exec govuk-lint-sass app/assets/stylesheets
+success=$((success || $?))
 bundle exec rspec --exclude-pattern "spec/features/*_spec.rb"
+success=$((success || $?))
 bundle exec rspec --pattern "spec/features/*_spec.rb"
+success=$((success || $?))
 bundle exec rake spec:javascripts
+success=$((success || $?))
+
+if [ $success -eq 0 ]
+then
+  funky_pass_banner
+else
+  funky_fail_banner
+fi
+
