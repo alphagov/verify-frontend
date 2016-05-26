b08a8f8ee63e8046569ea6dabc62d22a84cf6a14
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-05-26 17:34:34 +0100

    3549: Red-ify sass lint output
    So that it's more noticable when it's failing.
    
    Authors: @lloydnye @richardtowers

--- pre-commit.sh
+++ pre-commit.sh
@@ -4,8 +4,14 @@
 
 bundle exec govuk-lint-ruby app config lib spec
 success=$?
+
+# govuk-lint-sass is very quiet. Setting the output colour
+# to red makes it more obvious when it fails.
+tput setaf 1
 bundle exec govuk-lint-sass app/assets/stylesheets
 success=$((success || $?))
+tput sgr0
+
 bundle exec rspec --exclude-pattern "spec/features/*_spec.rb"
 success=$((success || $?))
 bundle exec rspec --pattern "spec/features/*_spec.rb"
