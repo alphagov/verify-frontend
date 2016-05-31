dabf3319267b47e69abd205acc8acfaa76cf7291
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-05-31 11:32:10 +0100

    3549: reset session on redirect to service
    Authors: @richardtowers @leoshaw @veda @yolinas

--- app/controllers/redirect_to_service_controller.rb
+++ app/controllers/redirect_to_service_controller.rb
@@ -1,17 +1,27 @@
 class RedirectToServiceController < ApplicationController
   def signing_in
-    @title = 'hub.redirect_to_service.signing_in.title'
-    @response_for_rp = SESSION_PROXY.response_for_rp(cookies)
-    @rp_name = current_transaction.rp_name
-    @transition_message = t('hub.redirect_to_service.signing_in.transition_heading', rp_name: @rp_name)
-    render 'redirect_to_service'
+    redirect_to_service('hub.redirect_to_service.signing_in.title', 'hub.redirect_to_service.signing_in.transition_heading')
   end
 
   def start_again
-    @title = 'hub.redirect_to_service.start_again.title'
+    redirect_to_service('hub.redirect_to_service.start_again.title', 'hub.redirect_to_service.start_again.transition_heading')
+  end
+
+private
+
+  def redirect_to_service(title_key, transition_heading_key)
+    @title = title_key
     @response_for_rp = SESSION_PROXY.response_for_rp(cookies)
     @rp_name = current_transaction.rp_name
-    @transition_message = t('hub.redirect_to_service.start_again.transition_heading', rp_name: @rp_name)
+    @transition_message = t(transition_heading_key, rp_name: @rp_name)
+    reset_session_cookies
     render 'redirect_to_service'
+  end
+
+  def reset_session_cookies
+    reset_session
+    no_current_session_value = 'no-current-session'.freeze
+    cookies[CookieNames::SECURE_COOKIE_NAME] = no_current_session_value
+    cookies[CookieNames::SESSION_ID_COOKIE_NAME] = no_current_session_value
   end
 end
--- spec/features/user_visits_redirect_to_service_page_spec.rb
+++ spec/features/user_visits_redirect_to_service_page_spec.rb
@@ -5,64 +5,68 @@ def hidden_field_value(id)
   page.find("##{id}", visible: false).value
 end
 
+def verify_redirect_to_service(title)
+  expect(@api_request).to have_been_made.once
+  expect(page).to have_title(title)
+  expect(hidden_field_value('SAMLResponse')).to eql('a saml message')
+  expect(hidden_field_value('RelayState')).to eql('a relay state')
+end
+
+def check_session_reset
+  expect(page.get_rack_session.keys).to eql(['session_id'])
+  expect(cookie_value(CookieNames::SESSION_ID_COOKIE_NAME)).to eql 'no-current-session'
+  expect(cookie_value(CookieNames::SECURE_COOKIE_NAME)).to eql 'no-current-session'
+end
+
 RSpec.describe 'When the user visits the redirect to service page' do
   before(:each) do
     set_session_cookies!
     page.set_rack_session(transaction_simple_id: 'test-rp')
+    @api_request = stub_response_for_rp
   end
 
   context 'without javascript' do
     it 'should redirect to service when path is signing-in' do
-      api_request = stub_response_for_rp
-
       visit redirect_to_service_signing_in_path
-      expect(page).to have_title('Please wait. Signing you in')
+      verify_redirect_to_service(I18n.t('hub.redirect_to_service.signing_in.title'))
 
-      expect(hidden_field_value('SAMLResponse')).to eql('a saml message')
-      expect(hidden_field_value('RelayState')).to eql('a relay state')
-
-      click_button 'Continue'
+      click_button I18n.t('navigation.continue')
       expect(page).to have_current_path('/test-rp')
-      expect(api_request).to have_been_made.once
     end
 
     it 'should redirect to service when path is start-again' do
-      api_request = stub_response_for_rp
-
       visit redirect_to_service_start_again_path
-      expect(page).to have_title('Please wait. Starting again')
+      verify_redirect_to_service(I18n.t('hub.redirect_to_service.start_again.title'))
 
-      expect(hidden_field_value('SAMLResponse')).to eql('a saml message')
-      expect(hidden_field_value('RelayState')).to eql('a relay state')
-
-      click_button 'Continue'
+      click_button I18n.t('navigation.continue')
       expect(page).to have_current_path('/test-rp')
-      expect(api_request).to have_been_made.once
     end
 
     it 'should redirect to service when path is error' do
-      api_request = stub_response_for_rp
-
       visit redirect_to_service_error_path
-      expect(page).to have_title('Please wait. Starting again')
+      verify_redirect_to_service(I18n.t('hub.redirect_to_service.start_again.title'))
 
-      expect(hidden_field_value('SAMLResponse')).to eql('a saml message')
-      expect(hidden_field_value('RelayState')).to eql('a relay state')
-
-      click_button 'Continue'
+      click_button I18n.t('navigation.continue')
       expect(page).to have_current_path('/test-rp')
-      expect(api_request).to have_been_made.once
+    end
+
+    it 'should clear session when visiting start again' do
+      visit redirect_to_service_start_again_path
+      check_session_reset
+    end
+
+    it 'should clear session when signing in' do
+      visit redirect_to_service_signing_in_path
+      check_session_reset
     end
   end
 
   context 'with javascript' do
     it 'should redirect to service when path is signing-in', js: true do
-      api_request = stub_response_for_rp
-
       visit redirect_to_service_signing_in_path
 
       expect(page).to have_current_path('/test-rp')
-      expect(api_request).to have_been_made.once
+      expect(@api_request).to have_been_made.once
     end
   end
 end
