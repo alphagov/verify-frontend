alphagov/verify-frontend

commit 39081e210f28ac364b290b315da640e6a074fe83

3549: Add /redirect-to-service/signing-in
Added session proxy code to call API.
No JS only.
Doesn't handle error cases yet.

Authors: @lloydnye @veda @richardtowers

config/routes.rb
@@ -9,12 +9,17 @@ Rails.application.routes.draw do
   post 'SAML2/SSO/Response/POST' => 'authn_response#idp_response'
   get 'redirect-to-idp' => 'redirect_to_idp#index', as: :redirect_to_idp
   get 'redirect-to-idp-cy' => 'redirect_to_idp#index'
+  get 'response-processing' => 'response_processing#index', as: :response_processing
+  get 'response-processing-cy' => 'response_processing#index'
+  get 'redirect-to-service/signing-in' => 'redirect_to_service#signing_in', as: :redirect_signing_in
+  get 'redirect-to-service/signing-in-cy' => 'redirect_to_service#signing_in'
 
   match "/404", to: "errors#page_not_found", via: :all
 
   if %w(test development).include? Rails.env
     mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
     get 'test-saml' => 'test_saml#index'
+    post 'test-rp', to: proc { |_| [200, {}, ['OK']] }
     post 'test-idp-request-endpoint' => 'test_saml#idp_request'
     post 'another-idp-endpoint' => 'test_saml#idp_request'
     get 'test-journey-hint' => 'test_journey_hint_cookie#index', as: :test_journey_hint
@@ -65,12 +70,10 @@ Rails.application.routes.draw do
 
   if Rails.env == 'development'
     get 'feedback', to: redirect("#{API_HOST}/feedback")
-    get 'redirect-to-service/signing-in', to: redirect("#{API_HOST}/redirect-to-service/signing-in"), as: :redirect_signing_in
     get 'redirect-to-service/error', to: redirect("#{API_HOST}/redirect-to-service/error"), as: :redirect_to_service_error
     get 'further-information', to: redirect("#{API_HOST}/further-information"), as: :further_information
   else
     get 'feedback', to: 'feedback#index', as: :feedback
-    get 'redirect-to-service/signing-in', to: proc { |_| [200, {}, ['OK']] }, as: :redirect_signing_in
     get 'redirect-to-service/error', to: proc { |_| [200, {}, ['OK']] }, as: :redirect_to_service_error
     get 'further-information', to: proc { |_| [200, {}, ['OK']] }, as: :further_information
   end

spec/features/user_visits_response_processing_page_spec.rb
@@ -34,6 +34,7 @@ RSpec.describe 'When the user visits the response processing page' do
   end
 
   it 'redirects to signing in page when response is SEND_NO_MATCH_RESPONSE_TO_TRANSACTION' do
+    stub_response_for_rp
     stub_matching_outcome(MatchingOutcomeResponse::SEND_NO_MATCH_RESPONSE_TO_TRANSACTION)
     stubbed_report_request = stub_matching_report('No Match')
     visit '/response-processing'
@@ -42,6 +43,7 @@ RSpec.describe 'When the user visits the response processing page' do
   end
 
   it 'redirects to signing in page when response is SEND_SUCCESSFUL_MATCH_RESPONSE_TO_TRANSACTION' do
+    stub_response_for_rp
     stub_matching_outcome(MatchingOutcomeResponse::SEND_SUCCESSFUL_MATCH_RESPONSE_TO_TRANSACTION)
     stubbed_report_request = stub_matching_report('Match')
     visit '/response-processing'
@@ -50,6 +52,7 @@ RSpec.describe 'When the user visits the response processing page' do
   end
 
   it 'redirects to signing in page when response is SEND_USER_ACCOUNT_CREATED_RESPONSE_TO_TRANSACTION' do
+    stub_response_for_rp
     stubbed_report_request = stub_unknown_user_report
     stub_matching_outcome(MatchingOutcomeResponse::SEND_USER_ACCOUNT_CREATED_RESPONSE_TO_TRANSACTION)
     visit '/response-processing'

app/controllers/redirect_to_service_controller.rb
@@ -1,0 +1,5 @@
+class RedirectToServiceController < ApplicationController
+  def signing_in
+    @response_for_rp = SESSION_PROXY.response_for_rp(cookies)
+  end
+end

app/models/response_for_rp.rb
@@ -1,0 +1,10 @@
+class ResponseForRp < Api::Response
+  attr_reader :location, :saml_message, :relay_state
+  validates_presence_of :location, :saml_message
+
+  def initialize(hash)
+    @location = hash['postEndpoint']
+    @saml_message = hash['samlMessage']
+    @relay_state = hash['relayState']
+  end
+end

app/models/session_proxy.rb
@@ -6,6 +6,7 @@ class SessionProxy
   IDP_AUTHN_RESPONSE_PATH = "#{PATH}/idp-authn-response".freeze
   SESSION_STATE_PATH = "#{PATH}/state".freeze
   MATCHING_OUTCOME_PATH = "#{PATH}/matching-outcome".freeze
+  RESPONSE_FOR_RP_PATH = "#{PATH}/response-for-rp".freeze
   PARAM_SAML_REQUEST = 'samlRequest'.freeze
   PARAM_SAML_RESPONSE = 'samlResponse'.freeze
   PARAM_RELAY_STATE = 'relayState'.freeze
@@ -67,6 +68,11 @@ class SessionProxy
   def matching_outcome(cookies)
     response = @api_client.get(MATCHING_OUTCOME_PATH, cookies: select_cookies(cookies, CookieNames.session_cookies))
     MatchingOutcomeResponse.new(response || {}).tap(&:validate).outcome
+  end
+
+  def response_for_rp(cookies)
+    response = @api_client.get(RESPONSE_FOR_RP_PATH, cookies: select_cookies(cookies, CookieNames.session_cookies))
+    ResponseForRp.new(response || {}).tap(&:validate)
   end
 
 private

app/views/redirect_to_service/signing_in.html.erb
@@ -1,0 +1,23 @@
+<% content_for :page_title, 'Please wait. Signing you in' %>
+
+<script>
+  function init(){document.forms[0].submit();}
+  window.onload=init;
+</script>
+<div class="content-inner">
+  <div class="grid-row">
+    <div class="column-two-thirds">
+      <h1 class="heading-xlarge">Continue to next step</h1>
+      <div class="application-notice info-notice">
+        <p>Because Javascript is not enabled on your browser, you must press the continue button</p>
+      </div>
+      <%= form_tag(@response_for_rp.location) do %>
+          <%= hidden_field_tag 'SAMLResponse', @response_for_rp.saml_message %>
+          <% if @response_for_rp.relay_state %>
+            <%= hidden_field_tag 'RelayState', @response_for_rp.relay_state %>
+          <% end %>
+          <%= submit_tag 'Continue', id: 'continue-button' %>
+      <% end %>
+    </div>
+  </div>
+</div>

spec/api_test_helper.rb
@@ -78,3 +78,12 @@ end
 def stub_matching_outcome(outcome = MatchingOutcomeResponse::WAIT)
   stub_request(:get, api_uri('session/matching-outcome')).to_return(body: { 'outcome' => outcome }.to_json)
 end
+
+def stub_response_for_rp
+  response_body = {
+    'postEndpoint' => '/test-rp',
+    'samlMessage' => 'a saml message',
+    'relayState' => 'a relay state'
+  }
+  stub_request(:get, api_uri('session/response-for-rp')).to_return(body: response_body.to_json)
+end

spec/features/user_submits_start_page_form_spec.rb
@@ -2,7 +2,8 @@ require 'feature_helper'
 require 'models/cookie_names'
 
 def given_api_returns_federation_info
-  body = { 'idps' => [{ 'simpleId' => 'stub-idp-one', 'entityId' => 'http://idcorp.com' }], 'transactionSimpleId' => 'test-rp', 'transactionEntityId' => 'some-id' }
+  body = { 'idps' => [{ 'simpleId' => 'stub-idp-one', 'entityId' => 'http://idcorp.com' }],
+           'transactionSimpleId' => 'test-rp', 'transactionEntityId' => 'some-id' }
   stub_request(:get, api_uri('session/federation')).to_return(body: body.to_json)
 end
 

commit f7907c5c32350d8e545f8d22371f6b97ff9e70aa

3322: Fix localisation of response-processing
This was accidentally broken in a rebase in 39081e2.

Authors: @richardtowers @yolinas

config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
   post 'SAML2/SSO/Response/POST' => 'authn_response#idp_response'
   get 'redirect-to-idp' => 'redirect_to_idp#index', as: :redirect_to_idp
   get 'redirect-to-idp-cy' => 'redirect_to_idp#index'
-  get 'response-processing' => 'response_processing#index', as: :response_processing
-  get 'response-processing-cy' => 'response_processing#index'
   get 'redirect-to-service/signing-in' => 'redirect_to_service#signing_in', as: :redirect_to_service_signing_in
   get 'redirect-to-service/signing-in-cy' => 'redirect_to_service#signing_in'
   get 'redirect-to-service/start-again' => 'redirect_to_service#start_again', as: :redirect_to_service_start_again

spec/features/user_visits_response_processing_page_spec.rb
@@ -81,4 +81,10 @@ RSpec.describe 'When the user visits the response processing page' do
     expect(page).to have_title('Something went wrong - GOV.UK Verify - GOV.UK')
     expect(stubbed_report_request).to have_been_made.once
   end
+
+  it 'displays the content in Welsh' do
+    stub_matching_outcome
+    visit '/response-processing-cy'
+    expect(page).to have_css 'html[lang=cy]'
+  end
 end