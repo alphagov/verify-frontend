cfc6c92c892aabd2837c1869dd6fb8bf79fdc49e
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-06-13 17:37:35 +0100

    3346: Strip down markup in JS spec
    It's nice when the markup in specs contains only what is needed for the
    code being executed to work correctly.
    
    Authors: @richardtowers @yolinas

--- spec/javascripts/feedback_spec.js
+++ spec/javascripts/feedback_spec.js
@@ -5,65 +5,34 @@
 
 describe("Feedback Form", function () {
 
-  var formWithNoErrors = '<form class="feedback-form" id="feedback" novalidate="novalidate" action="/feedback" accept-charset="UTF-8" method="post">' +
-                           '<fieldset>' +
-                             '<div class="form-group">' +
-                               '<div class="form-group">' +
-                                 '<label class="form-label" for="feedback_form_what">What were you trying to do?</label>' +
-                                 '<textarea rows="10" cols="80" class="form-control" required="required" data-msg="Please provide details" name="feedback_form[what]" id="feedback_form_what" aria-required="true"></textarea>' +
-                               '</div>' +
-                             '</div>' +
-                             '<div class="form-group">' +
-                               '<div class="form-group">' +
-                                 '<label class="form-label" for="feedback_form_details">Please provide details of your question, problem or feedback</label>' +
-                                 '<textarea rows="20" cols="80" class="form-control" required="required" data-msg="Please provide details" name="feedback_form[details]" id="feedback_form_details" aria-required="true"></textarea>' +
-                               '</div>' +
-                             '</div>' +
-                             '<div class="form-section">' +
-                               '<fieldset class="inline">' +
-                                 '<legend class="heading-medium">Do you want a reply?</legend>' +
-                                 '<label class="block-label" onclick="" for="feedback_form_reply_true">' +
-                                   '<input value="true" name="feedback_form[reply]" id="feedback_form_reply_true" type="radio">' +
-                                   '<span>' +
-                                     '<span class="inner"></span>' +
-                                   '</span>' +
-                                   'Yes' +
-                                 '</label>' +
-                                 '<label class="block-label" onclick="" for="feedback_form_reply_false">' +
-                                   '<input value="false" name="feedback_form[reply]" id="feedback_form_reply_false" type="radio">' +
-                                   '<span>' +
-                                     '<span class="inner"></span>' +
-                                   '</span>' +
-                                   'No' +
-                                 '</label>' +
-                               '</fieldset>' +
-                               '<div class="panel panel-border-narrow reply-fields js-hidden">' +
-                                 '<p>If you’d like GOV.UK Verify to get back to you, please leave your details below.</p>' +
-                                 '<div class="form-group">' +
-                                   '<div class="form-group">' +
-                                     '<label class="form-label" for="feedback_form_name">Name</label>' +
-                                     '<input class="form-control form-control-3-4" data-msg-required="Please enter a name" name="feedback_form[name]" id="feedback_form_name" type="text">' +
-                                   '</div>' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<div class="form-group">' +
-                                     '<label class="form-label" for="feedback_form_email">' +
-                                       'Email address' +
-                                       '<span class="form-hint">We’ll only use this to reply to your message.</span>' +
-                                     '</label>' +
-                                     '<input class="form-control form-control-3-4" data-msg="Please enter a valid email address" name="feedback_form[email]" id="feedback_form_email" type="email">' +
-                                   '</div>' +
-                                 '</div>' +
-                               '</div>' +
-                             '</div>' +
-                             '<input name="feedback_form[referer]" id="feedback_form_referer" type="hidden">' +
-                             '<input name="feedback_form[user_agent]" id="feedback_form_user_agent" type="hidden">' +
-                             '<input value="true" name="feedback_form[js_disabled]" id="feedback_form_js_disabled" type="hidden">' +
-                             '<div class="actions">' +
-                               '<input name="commit" value="Send message" class="button" type="submit">' +
-                             '</div>' +
-                           '</fieldset>' +
-                         '</form>';
+  var formWithNoErrors =
+    '<form id="feedback" novalidate="novalidate" action="/feedback" method="post">' +
+      '<label for="feedback_form_what">What were you trying to do?</label>' +
+      '<textarea required="required" data-msg="Please provide details" name="feedback_form[what]" id="feedback_form_what"></textarea>' +
+      '<label for="feedback_form_details">Please provide details of your question, problem or feedback</label>' +
+      '<textarea required="required" data-msg="Please provide details" name="feedback_form[details]" id="feedback_form_details"></textarea>' +
+      '<legend>Do you want a reply?</legend>' +
+      '<label for="feedback_form_reply_true">' +
+        '<input value="true" name="feedback_form[reply]" id="feedback_form_reply_true" type="radio">' +
+        'Yes' +
+      '</label>' +
+      '<label for="feedback_form_reply_false">' +
+        '<input value="false" name="feedback_form[reply]" id="feedback_form_reply_false" type="radio">' +
+        'No' +
+      '</label>' +
+      '<div class="reply-fields js-hidden">' +
+        '<label for="feedback_form_name">Name</label>' +
+        '<input data-msg-required="Please enter a name" name="feedback_form[name]" id="feedback_form_name" type="text">' +
+        '<label for="feedback_form_email">' +
+          'Email address' +
+        '</label>' +
+        '<input data-msg="Please enter a valid email address" name="feedback_form[email]" id="feedback_form_email" type="email">' +
+      '</div>' +
+      '<input name="feedback_form[referer]" id="feedback_form_referer" type="hidden">' +
+      '<input name="feedback_form[user_agent]" id="feedback_form_user_agent" type="hidden">' +
+      '<input value="true" name="feedback_form[js_disabled]" id="feedback_form_js_disabled" type="hidden">' +
+      '<input name="commit" value="Send message" type="submit">' +
+    '</form>';
 
 
   var feedbackForm;
