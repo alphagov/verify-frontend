1628b8c3723e0d09215a1f8cd52f5a8cb5efaf1c
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-06-20 17:15:05 +0100

    3346: support old Feedback page params during deployment
    For ZDD we need to support the old feedback page params for a short period of
    time. This commit adds support.
    
    Authors: @chrisholmes @yolinas

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,12 +1,13 @@
 class FeedbackController < ApplicationController
   skip_before_action :validate_cookies
+  skip_before_action :verify_authenticity_token, only: :submit
 
   def index
     @form = FeedbackForm.new(referer: request.referer, user_agent: request.user_agent)
   end
 
   def submit
-    @form = FeedbackForm.new(params['feedback_form'] || {})
+    @form = FeedbackForm.new(params['feedback_form'] || old_feedback_form_params || {})
     if @form.valid?
       session_id = cookies[CookieNames::SESSION_ID_COOKIE_NAME]
       if FEEDBACK_SERVICE.submit!(session_id, @form)
@@ -20,5 +21,20 @@ class FeedbackController < ApplicationController
       flash.now[:errors] = @form.errors[:base].first
       render :index
     end
+  end
+
+private
+
+  def old_feedback_form_params
+    {
+      what: params["what"],
+      details: params["details"],
+      reply: params["selection"],
+      name: params["name"],
+      email: params["email"],
+      referer: params["referer"],
+      user_agent: params["user-agent"],
+      js_disabled: params["js-disabled"]
+    }
   end
 end
