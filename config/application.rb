2e5645680f67a8021cd5e573af0819d8a7037b26
Robin Whittleton <robin.whittleton@digital.cabinet-office.gov.uk>
2016-07-12 14:13:43 +0100

    3795: convert the custom radio buttons from a partial to a helper
    Author: @robinwhittleton

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
+require "./lib/action_view/helpers/verify_form_builder"
+
 Bundler.require(*Rails.groups)
 
 
@@ -41,5 +44,8 @@ module VerifyFrontend
     # by default rails wraps invalid inputs with <div class="field_with_errors">
     # we have our own way of styling errors, so we don't need this behaviour:
     config.action_view.field_error_proc = Proc.new { |html_tag| html_tag }
+
+    # add in our custom form builder
+    ActionView::Base.default_form_builder = VerifyFormBuilder::FormBuilder
   end
 end
