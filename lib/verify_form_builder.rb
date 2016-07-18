ada1bba9edd2953ce12bab317a25e3017805003e
Robin Whittleton <robin@kyan.com>
2016-07-18 08:49:01 +0100

    3795: form helper for custom check boxes
    Repeat the work done for custom radios for custom check boxes. The standard check_box helper has a different argument order to radio_button; I’ve mirrored that but could be persuaded to match radio_button’s argument order.
    
    Author: @robinwhittleton

--- lib/verify_form_builder.rb
+++ lib/verify_form_builder.rb
@@ -1,11 +1,18 @@
 class VerifyFormBuilder < ActionView::Helpers::FormBuilder
-  # Our custom radio button implementation. Workarounds:
+  # Our custom radio / checkbox implementation. Workarounds:
   # 1. empty onclick attribute for iOS5 support
   # 2. blank space in the inner span to let JAWS-on-IE read the following text
-  def block_label key, value, text, attributes = {}
+  def custom_radio_button key, value, text, attributes = {}
     label "#{key}_#{value.to_s.parameterize}", class: 'block-label', onclick: '' do
-      radio = radio_button key, value, attributes
-      "#{radio} <span><span class=\"inner\">&nbsp;</span></span> #{text}".html_safe
+      input = radio_button key, value, attributes
+      "#{input} <span><span class=\"inner\">&nbsp;</span></span> #{text}".html_safe
+    end
+  end
+
+  def custom_check_box key, attributes, true_value, false_value, text
+    label key, class: 'block-label', onclick: '' do
+      input = check_box key, attributes, true_value, false_value
+      "#{input} <span><span class=\"inner\">&nbsp;</span></span> #{text}".html_safe
     end
   end
 end
