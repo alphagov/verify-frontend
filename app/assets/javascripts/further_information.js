3d1709fd0364012864b5a8e2e16552f8f86d9426
Thomas Lee <thomas.lee@digital.cabinet-office.gov.uk>
2016-07-19 13:56:04 +0100

    Revert "3332 Toggle js validation when nullable attribute"
    Reverting due to updated design which does not require js changes that
    were made.
    
    Authors: @chrishomles @thomasalanlee @yolinas

--- app/assets/javascripts/further_information.js
+++ app/assets/javascripts/further_information.js
@@ -5,23 +5,10 @@
     $ = root.jQuery;
   if(typeof root.GOVUK === 'undefined') { root.GOVUK = {}; }
 
-  var furtherInformation = {
-        toggleValidation: function () {
-            var $sumbitButton = furtherInformation.$form.find('input#cycle_three_submit[type=submit]');
-            if (!furtherInformation.$form.find('#cycle_three_form_null_attribute').prop('checked')) {
-                $sumbitButton.removeAttr("formnovalidate")
-            } else {
-                $sumbitButton.attr("formnovalidate", "formnovalidate")
-            }
-        },
-        init: function () {
-            furtherInformation.$form = $('#further-information');
-            if(furtherInformation.$form.length === 1) {
-                furtherInformation.$form.find('#cycle_three_form_null_attribute').on('click', furtherInformation.toggleValidation);
-                furtherInformation.$form.validate();
-            }
-        }
-    };
-
-  root.GOVUK.furtherInformation = furtherInformation;
+  root.GOVUK.furtherInformation = {
+    init: function () {
+      this.$form = $('#further-information');
+      this.$form.validate();
+    }
+  };
 }).call(this);
--- spec/javascripts/further_information_spec.js
+++ spec/javascripts/further_information_spec.js
@@ -14,26 +14,7 @@ describe("Further Information Form", function () {
         '</label>' +
         '<input required="required" pattern="^pear$" type="text" id="cycle_three_form_cycle_three_data" data-msg="validation message">' +
       '</div>' +
-    '<input type="submit" value="Continue" id="cycle_three_submit" class="button">' +
     '</form>';
-
-  var formWithCheckbox =
-      '<form id="further-information" novalidate="novalidate">' +
-        '<div class="form-group">' +
-            '<label for="cycle_three_form_cycle_three_data">' +
-                'Enter your National Insurance number' +
-            '</label>' +
-            '<input required="required" pattern="^pear$" type="text" id="cycle_three_form_cycle_three_data" data-msg="validation message">' +
-        '</div>' +
-        '<div class="form-group">' +
-            '<label for="cycle_three_form_null_attribute">' +
-                'I don\'t have a National Insurance number' +
-            '</label>' +
-            '<input type="checkbox" id="cycle_three_form_null_attribute">' +
-        '</div>' +
-        '<input type="submit" value="Continue" id="cycle_three_submit" class="button">' +
-      '</form>';
-
 
   var furtherInformationForm;
   var $dom;
@@ -58,15 +39,6 @@ describe("Further Information Form", function () {
     $dom.remove();
   });
 
-  function furtherInformationFormWithCheckbox() {
-    $dom.remove();
-    $dom = $('<div>' + formWithCheckbox + '</div>');
-    $(document.body).append($dom);
-    GOVUK.validation.init();
-    GOVUK.furtherInformation.init();
-    return GOVUK.furtherInformation.$form;
-  }
-
   it("should have no errors on initialising the form.", function () {
     expect(furtherInformationForm.find('.error').length).toBe(0);
   });
@@ -86,20 +58,5 @@ describe("Further Information Form", function () {
     furtherInformationForm.find('input').val('pear');
     submitForm();
     expectNoError();
-  });
-
-  it("should have no errors when nullable attribute field is checked", function () {
-    furtherInformationForm =  furtherInformationFormWithCheckbox();
-    furtherInformationForm.find('input[type=checkbox]').trigger('click');
-    expect(furtherInformationForm.find('input[formnovalidate=formnovalidate]').size()).toBe(1);
-  });
-
-  it("validation should trigger when checkbox toggled", function () {
-    furtherInformationForm =  furtherInformationFormWithCheckbox();
-    furtherInformationForm.find('input[type=checkbox]').trigger('click');
-    furtherInformationForm.find('input[type=checkbox]').trigger('click');
-    expect(furtherInformationForm.find('input[formnovalidate=formnovalidate]').size()).toBe(0);
-    submitForm();
-    expect(furtherInformationForm.find('.error-message').eq(0).text()).toBe('validation message');
   });
 });
