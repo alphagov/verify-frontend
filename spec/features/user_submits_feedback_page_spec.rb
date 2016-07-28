dd4f9d03ddc03b43eeb9ecd108c2ea98b9a6a94a
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-07-28 13:08:36 +0100

    3704: Update text on the feedback sent page
    To match the text provided by Ant.
    
    Authors: @richardtowers @DataMinerUK

--- spec/features/user_submits_feedback_page_spec.rb
+++ spec/features/user_submits_feedback_page_spec.rb
@@ -1,6 +1,10 @@
 require 'feature_helper'
 
 RSpec.feature 'When the user submits the feedback page' do
+  let(:session_not_valid_link) {
+    I18n.t('hub.feedback_sent.session_not_valid', link: I18n.t('hub.feedback_sent.session_not_valid_link'))
+  }
+
   it 'should tell the user that their message has been sent with email' do
     visit feedback_path
 
@@ -11,10 +15,11 @@ RSpec.feature 'When the user submits the feedback page' do
     fill_in 'feedback_form_email', with: 'bob@smith.com'
 
     click_button I18n.t('hub.feedback.send_message')
+    expect(page).to have_title(I18n.t('hub.feedback_sent.title'))
     expect(page).to have_current_path(feedback_sent_path, only_path: true)
     expect(page).to have_content(I18n.t('hub.feedback_sent.message_email'))
-    expect(page).to_not have_content(I18n.t('hub.feedback_sent.message_no_email'))
-    expect(page).to have_content(I18n.t('hub.feedback_sent.session_timeout'))
+    expect(page).to have_content(session_not_valid_link)
+    expect(page).to have_link I18n.t('hub.feedback_sent.session_not_valid_link')
   end
 
   it 'should tell the user that their message has been sent without email' do
@@ -24,12 +29,11 @@ RSpec.feature 'When the user submits the feedback page' do
     fill_in 'feedback_form_details', with: 'Some details'
     choose 'feedback_form_reply_false'
 
-
     click_button I18n.t('hub.feedback.send_message')
     expect(page).to have_current_path(feedback_sent_path, only_path: true)
     expect(page).to_not have_content(I18n.t('hub.feedback_sent.message_email'))
-    expect(page).to have_content(I18n.t('hub.feedback_sent.message_no_email'))
-    expect(page).to have_content(I18n.t('hub.feedback_sent.session_timeout'))
+    expect(page).to have_content(session_not_valid_link)
+    expect(page).to have_link I18n.t('hub.feedback_sent.session_not_valid_link')
   end
 
   it 'should tell the user that their message has been sent and link them back to the start page' do
@@ -40,12 +44,10 @@ RSpec.feature 'When the user submits the feedback page' do
     fill_in 'feedback_form_details', with: 'Some details'
     choose 'feedback_form_reply_false'
 
-
     click_button I18n.t('hub.feedback.send_message')
     expect(page).to have_current_path(feedback_sent_path, only_path: true)
     expect(page).to_not have_content(I18n.t('hub.feedback_sent.message_email'))
-    expect(page).to have_content(I18n.t('hub.feedback_sent.message_no_email'))
-    expect(page).to_not have_content(I18n.t('hub.feedback_sent.session_timeout'))
-    expect(page).to have_css('a', text: I18n.t('hub.feedback_sent.start_again_link'))
+    expect(page).to_not have_content(session_not_valid_link)
+    expect(page).to have_link I18n.t('hub.feedback_sent.session_valid_link')
   end
 end
