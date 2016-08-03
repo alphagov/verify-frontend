f220833b6cbbf2c64e51efd60334fea60603c28a
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2016-08-03 14:56:38 +0100

    3659: Serve correct content type and cache correct headers
    @Authors: @andrienricketts @richardtowers @mahmudhussain

--- app/controllers/metadata_controller.rb
+++ app/controllers/metadata_controller.rb
@@ -1,11 +1,20 @@
 class MetadataController < ApplicationController
   skip_before_action :validate_cookies
 
+  METADATA_CONTENT_TYPE = 'application/samlmetadata+xml'.freeze
+
   def service_providers
-    render xml: METADATA_CLIENT.idp_metadata
+    do_not_cache
+    render xml: METADATA_CLIENT.idp_metadata, content_type: METADATA_CONTENT_TYPE
   end
 
   def identity_providers
-    render xml: METADATA_CLIENT.sp_metadata
+    do_not_cache
+    render xml: METADATA_CLIENT.sp_metadata, content_type: METADATA_CONTENT_TYPE
+  end
+
+  private
+  def do_not_cache
+    response.headers['Cache-Control'] = 'no-cache, no-store, no-transform'
   end
 end
