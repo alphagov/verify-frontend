51fc6592035bd07d68444bfeef010f4f5ddbfe16
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-08-05 08:56:58 +0100

    bau: use heroku buildpack directly

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -eu
 
-bundle
 export HEADLESS=true
 export DISPLAY=:0
-./pre-commit.sh
-RAILS_ENV=production dotenv bundle exec rake assets:precompile
-RAILS_ENV=production dotenv bundle exec rake tmp:clear
 
-cp -r public/new-assets public/assets
-
-pkgr package . --version="${BUILD_NUMBER}" --iteration=1 --name=front
+pkgr package . --version="${BUILD_NUMBER}" --iteration=1 --name=front --buildpack https://github.com/heroku/heroku-buildpack-ruby --env STACK=cedar-14
