150b0934ba7982ae92d7a65e9b703972e3251c87
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-08-09 13:33:42 +0100

    BAU: Run stub-api on a different port to the real API
    This avoids problems where the real API tries to start on a port that's
    already been grabbed by the stub API.
    
    Authors: @richardtowers @lloydnye

--- startup.sh
+++ startup.sh
@@ -3,9 +3,9 @@
 
 if [ "$1" == '--stub-api' ]
 then
-  echo "Starting stub-api server on port 50191"
-  export API_HOST=http://localhost:50191
-  rackup --daemonize --port 50191 --pid tmp/stub_api.pid stub/stub_api_conf.ru
+  echo "Starting stub-api server on port 50199"
+  export API_HOST=http://localhost:50199
+  rackup --daemonize --port 50199 --pid tmp/stub_api.pid stub/stub_api_conf.ru
 fi
 
 bundle exec puma -e development -d -p 50300
