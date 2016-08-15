f81f92af44ef5eea5a05debc376f37aea2388c36
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-08-15 11:10:51 +0100

    bau: add session id to raven user context
    When we send an error to sentry it would be helpful if we include the session id
    as a parameter so that we can correlate it easily against other logs/errors.
    
    Usually, we wouldn't have to do this ourselves with Raven as it usually includes
    cookies with the error, but we have disabled this so that we don't leak the
    user's secure cookie. To work around this we can set a user context containing
    the session id.

--- lib/store_session_id.rb
+++ lib/store_session_id.rb
@@ -6,7 +6,9 @@ class StoreSessionId
 
   def call(env)
     request = ActionDispatch::Request.new env
-    RequestStore.store[:session_id] = request.cookies[CookieNames::SESSION_ID_COOKIE_NAME]
+    session_id = request.cookies[CookieNames::SESSION_ID_COOKIE_NAME]
+    RequestStore.store[:session_id] = session_id
+    Raven.user_context(session_id: session_id)
     @app.call(env)
   end
 end
