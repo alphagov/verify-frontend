bec428b407b7878dea80e5123d850a3e0586f33a
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-08-15 14:36:05 +0100

    5025: Do not copy new-assets to assets
    Now the assetsPath setting points to /assets anyway, so this copy is no
    longer required.
    
    Authors: @richardtowers @datamineruk @adityapahuja

--- jenkins.sh
+++ jenkins.sh
@@ -7,6 +7,4 @@ export DISPLAY=:0
 RAILS_ENV=production dotenv bundle exec rake assets:precompile
 RAILS_ENV=production dotenv bundle exec rake tmp:clear
 
-cp -r public/new-assets public/assets
-
 pkgr package . --version="${BUILD_NUMBER}" --iteration=1 --name=front
