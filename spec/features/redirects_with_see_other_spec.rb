815d81dbd1d64a70b689c58e8b796e14d0782678
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-08-16 12:48:32 +0100

    bau: use new params format for request specs

--- spec/features/redirects_with_see_other_spec.rb
+++ spec/features/redirects_with_see_other_spec.rb
@@ -4,7 +4,7 @@ describe 'pages redirect with see other', type: :request do
   it 'sets a see other for redirects' do
     stub_federation
     stub_api_saml_endpoint
-    post '/SAML2/SSO', 'SAMLRequest' => 'my-saml-request', 'RelayState' => 'my-relay-state'
+    post '/SAML2/SSO', params: { 'SAMLRequest' => 'my-saml-request', 'RelayState' => 'my-relay-state' }
     expect(response.status).to eql 303
   end
 end
