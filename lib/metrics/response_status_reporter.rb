22740f525abb99019e4ad5235be2f56acc89a5c6
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2016-08-18 15:41:42 +0100

    5072: report metrics around HTTP status code counts
    We would like to see the occurrences of different status codes in our responses
    to users so that we can provide a visualisation relating to the health of our
    system (count of 2xxs vs 3xxs vs 4xxs vs 5xxs).
    
    I have used the statsd/notifications metric reporter system to provide these metrics
    
    /service-status is currenty hard coded as testing with service_status_path or
    dependency injecting it doesn't appear to be possible.

---
+++ lib/metrics/response_status_reporter.rb
@@ -0,0 +1,19 @@
+module Metrics
+  class ResponseStatusReporter
+    def initialize(statsd_client, logger = Rails.logger)
+      @statsd_client = statsd_client
+      @logger = logger
+    end
+
+    def report(_name, _start, _finish, _id, payload)
+      if payload[:path] != '/service-status'
+        begin
+          status_code = Integer(payload[:status])
+          @statsd_client.increment("#{status_code / 100}xx_responses")
+        rescue TypeError #status code is not a number
+          @logger.warn('unable to read status code from response')
+        end
+      end
+    end
+  end
+end
