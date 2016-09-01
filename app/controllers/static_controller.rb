7aeaa7f9fb0b7c22ffe6dd00b9845536cf1a1e20
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2016-09-01 15:53:31 +0100

    bau: fix humans.txt
    Pair: WillPa

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,3 +1,7 @@
 class StaticController < ApplicationController
   skip_before_action :validate_session
+
+  def humanstxt
+    render plain: 'GOV.UK Verify is built by a team at the Government Digital Service in London. If you\'d like to join us, see https://identityassurance.blog.gov.uk/work-with-us/'
+  end
 end
--- spec/features/user_visits_humanstxt_spec.rb
+++ spec/features/user_visits_humanstxt_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe 'user visits humans.txt' do
   it 'will tell humans how to work at GOV.UK Verify' do
     visit('/humans.txt')
 
-    expect(page).to have_content 'GOV.UK Verify is built by a team at the Government Digital Service in London. If you\'d like to join us, see https://identityassurance.blog.gov.uk/work-with-us/'
+    expect(page.body).to eql 'GOV.UK Verify is built by a team at the Government Digital Service in London. If you\'d like to join us, see https://identityassurance.blog.gov.uk/work-with-us/'
   end
 end
