1986ea3e76fee4ddb2510b4d64ce1b5eae2f6b7d
Anshul Sirur <vixus0@gmail.com>
2016-09-21 17:59:01 +0100

    BAU: Split middleware initialisers
    - The middlewares aren't really related and it would be better if they
      were initialised independently.
    
    Author: @vixus0

--- config/initializers/middleware.rb
+++
@@ -1,7 +0,0 @@
-# Middleware for providing service status
-require 'service_status_filter'
-Rails.application.config.middleware.use ServiceStatusFilter
-
-# Middleware for storing the session ID
-require 'store_session_id'
-Rails.application.config.middleware.insert_before Rails::Rack::Logger, StoreSessionId
---
+++ config/initializers/middleware_store_session_id.rb
@@ -0,0 +1,2 @@
+require 'service_status_filter'
+Rails.application.config.middleware.use ServiceStatusFilter
