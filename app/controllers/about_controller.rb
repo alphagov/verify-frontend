2530c259d0af6d2e56b2c140bf03db9f60975f84
Nicola Hughes <nihughes@thoughtworks.com>
2017-01-23 13:55:47 +0000

    TT-88: Concluding right company AB test
    Variant B with more info implemented in view
    
    Authors: @DataMinerUK @phss

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -13,15 +13,4 @@ class AboutController < ApplicationController
   def certified_companies
     @identity_providers = IDENTITY_PROVIDER_DISPLAY_DECORATOR.decorate_collection(current_identity_providers)
   end
-
-  def choosing_a_company
-    AbTest.report('right_company', ab_test('right_company'), current_transaction_simple_id, request)
-    @is_in_b_group = is_in_b_group?
-  end
-
-private
-
-  def is_in_b_group?
-    ab_test('right_company') == 'right_company_more_info'
-  end
 end
