0e41e55d7fc5f1e964b2187ee744505e4bc0b71e
Anshul Sirur <vixus0@gmail.com>
2017-02-28 16:04:14 +0000

    TT-199: Store requested LOA in session cookie
    - Obtain list of LOAs from ida-frontend (fed config).
    - Assign the first LOA as the requested LOA for the session.
    
    Authors: @nicolahughes @simongreensmith @mahmudhussain @anshulsirur

--- app/models/session_response.rb
+++ app/models/session_response.rb
@@ -1,10 +1,11 @@
 class SessionResponse < Api::Response
-  attr_reader :session_id, :transaction_simple_id, :idps
-  validates :session_id, :transaction_simple_id, :idps, presence: true
+  attr_reader :session_id, :transaction_simple_id, :idps, :levels_of_assurance
+  validates :session_id, :transaction_simple_id, :idps, :levels_of_assurance, presence: true
 
   def initialize(hash)
     @session_id = hash['sessionId']
     @transaction_simple_id = hash['transactionSimpleId']
     @idps = hash['idps']
+    @levels_of_assurance = hash['levelsOfAssurance']
   end
 end
