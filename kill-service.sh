ba96b8dd5cfe265ef6881a5b14e1464fa0d2040e
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2017-03-02 13:43:43 +0000

    bau: Nicer way of killing processes
    Thanks to Russell Howe for pointing out the bashism.
    
    Authors: @richardtowers

--- kill-service.sh
+++ kill-service.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+
 if [ -a './tmp/stub_api.pid' ]
 then
-  xargs kill < ./tmp/stub_api.pid
+  kill "$(< ./tmp/stub_api.pid)"
   rm ./tmp/stub_api.pid
 fi
 
 if [ -a './tmp/puma.pid' ]
 then
-  xargs kill < ./tmp/puma.pid
+  kill "$(< ./tmp/puma.pid)"
   rm ./tmp/puma.pid
 fi
 
