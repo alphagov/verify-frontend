16af60c2c3ea7efa805c2f3975acc3479f4defc7
nashraf <nashraf@thoughtworks.com>
2017-05-16 22:01:29 +0100

    TT-355: Completed confirmation page for LOA1 journey
    Finished controller and feature tests for confirmation journey
    
    Authors: @naseerashraf @cobainc0

--- app/controllers/confirmation_controller.rb
+++ app/controllers/confirmation_controller.rb
@@ -6,6 +6,7 @@ class ConfirmationController < ApplicationController
     selected_idp = session.fetch(:selected_idp)
     @idp_name = IDENTITY_PROVIDER_DISPLAY_DECORATOR.decorate(IdentityProvider.new(selected_idp)).display_name
     @transaction_name = current_transaction.name
+
     if is_loa1?
       render :confirmation_LOA1
     else
--- spec/controllers/confirmation_spec.rb
+++ spec/controllers/confirmation_spec.rb
@@ -1,50 +1,36 @@
 require 'rails_helper'
 require 'controller_helper'
+require 'spec_helper'
 
 describe ConfirmationController do
   let(:identity_provider_display_decorator) { double(:IdentityProviderDisplayDecorator) }
   let(:repository) { double(:repository) }
   let(:display_data) { double(:display_data) }
+  entity_id = 'http://idcorp.com'
+  simple_id = 'stub-idp-loa'
+  levels_of_assurance = %w(LEVEL_1 LEVEL_2)
+  transaction_simple_id = 'test-rp'
 
   subject { get :index, params: { locale: 'en' } }
 
+  before(:each) do
+    create_session_cookie('selected_idp', 'entity_id' => entity_id, 'simple_id' => simple_id, 'levels_of_assurance' => levels_of_assurance)
+    create_session_cookie('transaction_simple_id', transaction_simple_id)
+
+    stub_const('IDENTITY_PROVIDER_DISPLAY_DECORATOR', stub_identity_provider_display_decorator(identity_provider_display_decorator, simple_id, entity_id, levels_of_assurance))
+    stub_const('RP_DISPLAY_REPOSITORY', stub_rp_display_repository(transaction_simple_id))
+  end
+
   it 'renders the confirmation LOA1 template when LEVEL_1 is the requested LOA' do
-    stub_session
     set_session_and_cookies_with_loa('LEVEL_1')
-    loa_identity_provider = IdentityProvider.new('simple_id' => 'stub-idp-loa1',
-                                                 'entity_id' => 'http://idcorp.com',
-                                                 'levels_of_assurance' => %w(LEVEL_1 LEVEL_2))
 
-    LoaRecommended = Struct.new(
-        :identity_provider,
-        :display_data,
-        :logo_path,
-        :white_logo_path
-    ) do
-      delegate :entity_id, to: :identity_provider
-      delegate :simple_id, to: :identity_provider
-      delegate :model_name, to: :identity_provider
-      delegate :to_key, to: :identity_provider
-      delegate :display_name, :about_content, :requirements, :special_no_docs_instructions, :no_docs_requirement, :contact_details, :interstitial_question, :tagline, to: :display_data
-    end
-
-    loa_recommended = LoaRecommended.new(loa_identity_provider, display_data, 'idp-logos/barclays.png', 'idp-logos-white/barclays.png')
-
-    stub_const('IDENTITY_PROVIDER_DISPLAY_DECORATOR', identity_provider_display_decorator)
-    expect(identity_provider_display_decorator).to receive(:decorate) { |idps|
-      expect(idps.first).to have_attributes(simple_id: loa_identity_provider.simple_id,
-                                            entity_id: loa_identity_provider.entity_id,
-                                            levels_of_assurance: loa_identity_provider.levels_of_assurance)
-    }.and_return(loa_recommended)
-    expect(loa_recommended).to receive(:display_name).and_return('idp-display-name')
-    stub_const('RP_DISPLAY_REPOSITORY', repository)
-    expect(repository).to receive(:fetch).with(instance_of(String)).and_return('current_transaction_simple_id')
     expect(subject).to render_template(:confirmation_LOA1)
     expect(subject).to_not render_template(:confirmation_LOA2)
   end
 
   it 'renders the confirmation LOA2 template when LEVEL_2 is the requested LOA' do
     set_session_and_cookies_with_loa('LEVEL_2')
+
     expect(subject).to render_template(:confirmation_LOA2)
     expect(subject).to_not render_template(:confirmation_LOA1)
   end
