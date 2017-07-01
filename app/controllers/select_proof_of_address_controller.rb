e2b8b19d668d052594fa3167c1fe914898c0b9ef
nashraf <nashraf@thoughtworks.com>
2017-07-01 14:44:52 +0100

    TT-681: Add proof of address details to form
    - add bank account, debit card, credit card check boxes to form
    - add post route
    - add controller test
    
    Author: @NasAshrafThoughworks

--- app/controllers/select_proof_of_address_controller.rb
+++ app/controllers/select_proof_of_address_controller.rb
@@ -1,8 +1,12 @@
-class SelectProofOfAddressController < ConfigurableJourneyController
-
+class SelectProofOfAddressController < ApplicationController
   def index
     @form = SelectProofOfAddressForm.new({})
 
     render :select_proof_of_address
+  end
+
+  def select_proof
+    @form = SelectProofOfAddressForm.new(params['select_proof_of_address_form'] || {})
+    selected_answer_store.store_selected_answers('address_proof', @form.selected_answers)
   end
 end
