2b2d20616e76d969e70c77e713366e2dcf428f85
Kevin Garwood <kevingarwood@gds0262.local>
2017-07-04 10:55:02 +0100

    TT-665: Removing feature tests now covered by controller tests
    Feature tests that click various combinations of buttons to test routing are now managed as controller tests.  Removed feature tests that check if text displays in Welsh.  Taking advantage of utility method stub_piwik_request and putting it in a before block.
    
    Authors: @kgarwood, @cobainc0, @vixus0

--- spec/features/user_visits_will_it_work_for_me_page_spec.rb
+++ spec/features/user_visits_will_it_work_for_me_page_spec.rb
@@ -11,76 +11,6 @@ RSpec.describe 'When the user visits the will it work for me page' do
     expect_feedback_source_to_be(page, 'WILL_IT_WORK_FOR_ME_PAGE', '/will-it-work-for-me')
   end
 
-  it 'displays the page in Welsh' do
-    visit "/#{I18n.t('routes.will_it_work_for_me', locale: 'cy')}"
-    expect(page).to have_title 'Allai i gael fy nilysu? - GOV.UK Verify - GOV.UK'
-    expect(page).to have_css 'html[lang=cy]'
-  end
-
-  it 'redirects to the select document page when user is over 20 and is a uk resident' do
-    visit '/will-it-work-for-me'
-
-    choose 'will_it_work_for_me_form_above_age_threshold_true', allow_label_click: true
-    choose 'will_it_work_for_me_form_resident_last_12_months_true', allow_label_click: true
-    click_button 'Continue'
-
-    expect(page).to have_current_path(select_documents_path)
-  end
-
-  it 'redirects to the why-might-this-not-work-for-me page when user is over 20 and has moved to the uk in the last 12 months' do
-    visit '/will-it-work-for-me'
-    choose 'will_it_work_for_me_form_above_age_threshold_true', allow_label_click: true
-    choose 'will_it_work_for_me_form_resident_last_12_months_false', allow_label_click: true
-    choose 'will_it_work_for_me_form_not_resident_reason_movedrecently', allow_label_click: true
-
-    click_button 'Continue'
-
-    expect(page).to have_current_path(why_might_this_not_work_for_me_path)
-  end
-
-  it 'redirects to the select-documents page when user is over 20, has lived in the uk for more than 12 months and doesnt have a UK address' do
-    visit '/will-it-work-for-me'
-    choose 'will_it_work_for_me_form_above_age_threshold_true', allow_label_click: true
-    choose 'will_it_work_for_me_form_resident_last_12_months_true', allow_label_click: true
-    choose 'will_it_work_for_me_form_not_resident_reason_noaddress', allow_label_click: true
-
-    click_button 'Continue'
-
-    expect(page).to have_current_path(select_documents_path)
-  end
-
-  it 'redirects to the why-might-this-not-work-for-me page when user is under 20' do
-    visit '/will-it-work-for-me'
-    choose 'will_it_work_for_me_form_above_age_threshold_false', allow_label_click: true
-    choose 'will_it_work_for_me_form_resident_last_12_months_true', allow_label_click: true
-
-    click_button 'Continue'
-
-    expect(page).to have_current_path(why_might_this_not_work_for_me_path)
-  end
-
-  it 'redirects to the may-not-work-if-you-live-overseas page when user is over 20 and has address in the uk but not a resident' do
-    visit '/will-it-work-for-me'
-    choose 'will_it_work_for_me_form_above_age_threshold_true', allow_label_click: true
-    choose 'will_it_work_for_me_form_resident_last_12_months_false', allow_label_click: true
-    choose 'will_it_work_for_me_form_not_resident_reason_addressbutnotresident', allow_label_click: true
-
-    click_button 'Continue'
-
-    expect(page).to have_current_path(may_not_work_if_you_live_overseas_path)
-  end
-
-  it 'redirects to the will-not-work-without-uk-address page when user is over 20 and is not resident and has no address in the uk' do
-    visit '/will-it-work-for-me'
-    choose 'will_it_work_for_me_form_above_age_threshold_true', allow_label_click: true
-    choose 'will_it_work_for_me_form_resident_last_12_months_false', allow_label_click: true
-    choose 'will_it_work_for_me_form_not_resident_reason_noaddress', allow_label_click: true
-
-    click_button 'Continue'
-
-    expect(page).to have_current_path(will_not_work_without_uk_address_path)
-  end
-
   context 'shows a validation message when form is invalid' do
     it 'when js is off' do
       visit '/will-it-work-for-me'
@@ -102,28 +32,5 @@ RSpec.describe 'When the user visits the will it work for me page' do
       expect(page).to have_current_path(will_it_work_for_me_path)
       expect(page).to have_css '#validation-error-message-js', text: 'Please answer all the questions'
     end
-  end
-
-  it 'reports to Piwik when the form is valid' do
-    stub_request(:get, INTERNAL_PIWIK.url).with(query: hash_including({}))
-    piwik_request = { 'action_name' => 'Can I be Verified Next' }
-
-    visit '/will-it-work-for-me'
-    choose 'will_it_work_for_me_form_above_age_threshold_true', allow_label_click: true
-    choose 'will_it_work_for_me_form_resident_last_12_months_true', allow_label_click: true
-
-    click_button 'Continue'
-
-    expect(a_request(:get, INTERNAL_PIWIK.url).with(query: hash_including(piwik_request))).to have_been_made.once
-  end
-
-  it 'does not report to Piwik when the form is invalid' do
-    stub_request(:get, INTERNAL_PIWIK.url).with(query: hash_including({}))
-    piwik_request = { 'action_name' => 'Can I be Verified Next' }
-
-    visit '/will-it-work-for-me'
-    click_button 'Continue'
-
-    expect(a_request(:get, INTERNAL_PIWIK.url).with(query: hash_including(piwik_request))).to_not have_been_made
   end
 end
