2725e532ad91b786f20b1bc0d6804bc6b7c2fc61
Nicola Hughes <nihughes@thoughtworks.com>
2017-07-10 14:23:47 +0100

    TT-681: Changing financial questions to use radio buttons and adding validation
    Rails validation for when JavaScript is disabled
    
    Authors: @DataMinerUK @nasashrafthoughtworks

--- app/controllers/select_proof_of_address_controller.rb
+++ app/controllers/select_proof_of_address_controller.rb
@@ -1,13 +1,17 @@
 class SelectProofOfAddressController < ApplicationController
   def index
     @form = SelectProofOfAddressForm.new({})
-
     render :select_proof_of_address
   end
 
   def select_proof
     @form = SelectProofOfAddressForm.new(params['select_proof_of_address_form'] || {})
-    selected_answer_store.store_selected_answers('address_proof', @form.selected_answers)
-    redirect_to select_phone_path
+    if @form.valid?
+      selected_answer_store.store_selected_answers('address_proof', @form.selected_answers)
+      redirect_to select_phone_path
+    else
+      flash.now[:errors] = @form.errors.full_messages.join(', ')
+      render :select_proof_of_address
+    end
   end
 end
--- app/models/select_proof_of_address_form.rb
+++ app/models/select_proof_of_address_form.rb
@@ -2,6 +2,7 @@ class SelectProofOfAddressForm
   include ActiveModel::Model
 
   attr_reader :uk_bank_account_details, :debit_card, :credit_card
+  validate :answered_all_questions
 
   def initialize(params)
     @uk_bank_account_details = params[:uk_bank_account_details]
@@ -17,5 +18,17 @@ class SelectProofOfAddressForm
         .select(&filter_invalid_attributes)
         .map(&attribute_to_value)
         .to_h
+  end
+
+  def answered_all_questions
+    if uk_bank_account_details.nil? || debit_card.nil? || credit_card.nil?
+      add_no_selection_error
+    end
+  end
+
+private
+
+  def add_no_selection_error
+    errors.add(:base, I18n.t('hub.select_phone.errors.no_selection'))
   end
 end
