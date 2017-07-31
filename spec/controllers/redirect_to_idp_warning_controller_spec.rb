d334c1f2c967ac1c1891bd1e5e0bffe789e7fbfb
Kevin Garwood <kevingarwood@gds0262.local>
2017-07-31 14:16:25 +0100

    TT-805: Adding additional controller tests to cover index page variations
    Adding new test cases to make sure that when user journey lands on the control
    or variants, the correct pages will be rendered and that they will be rendered correctly.
    
    Authors: @kgarwood

--- spec/controllers/redirect_to_idp_warning_controller_spec.rb
+++ spec/controllers/redirect_to_idp_warning_controller_spec.rb
@@ -11,7 +11,7 @@ describe RedirectToIdpWarningController do
     session[:selected_idp_was_recommended] = [true, false].sample
   end
 
-  context 'renders idp logos' do
+  context 'index page for the control of TT-805 AB Test' do
     subject { get :index, params: { locale: 'en' } }
 
     it 'warning page when idp selected' do
@@ -20,6 +20,42 @@ describe RedirectToIdpWarningController do
                                  'levels_of_assurance' => %w(LEVEL_1 LEVEL_2) }
 
       expect(subject).to render_template(:index)
+    end
+
+    it 'error page when no idp selected' do
+      session[:selected_idp] = {}
+
+      expect(subject).to render_template('errors/something_went_wrong')
+    end
+  end
+
+  context 'index page for the variant_heading_account variant of TT-805 AB Test' do
+    subject { get :index_variant_heading_account, params: { locale: 'en' } }
+
+    it 'warning page when idp selected' do
+      session[:selected_idp] = { 'simple_id' => 'stub-idp-two',
+                                 'entity_id' => 'http://idcorp.com',
+                                 'levels_of_assurance' => %w(LEVEL_1 LEVEL_2) }
+
+      expect(subject).to render_template(:index_variant_heading_account)
+    end
+
+    it 'error page when no idp selected' do
+      session[:selected_idp] = {}
+
+      expect(subject).to render_template('errors/something_went_wrong')
+    end
+  end
+
+  context 'index page for the variant_heading_website variant of TT-805 AB Test' do
+    subject { get :index_variant_heading_website, params: { locale: 'en' } }
+
+    it 'warning page when idp selected' do
+      session[:selected_idp] = { 'simple_id' => 'stub-idp-two',
+                                 'entity_id' => 'http://idcorp.com',
+                                 'levels_of_assurance' => %w(LEVEL_1 LEVEL_2) }
+
+      expect(subject).to render_template(:index_variant_heading_website)
     end
 
     it 'error page when no idp selected' do
