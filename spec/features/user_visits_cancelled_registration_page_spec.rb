51c1871403a16cf541831082ef2be0ed9d1089aa
nashraf <nashraf@thoughtworks.com>
2017-08-03 11:52:14 +0100

    TT-372: Added new feature test for find other ways to erify
    Author: @NasAshrafThoughtworks

--- spec/features/user_visits_cancelled_registration_page_spec.rb
+++ spec/features/user_visits_cancelled_registration_page_spec.rb
@@ -10,10 +10,20 @@ RSpec.describe 'When user visits cancelled registration page' do
   it 'will render itself' do
     page.set_rack_session(
       selected_idp: { entity_id: 'http://idcorp.com', simple_id: 'stub-idp-one' },
-      transaction_simple_id: 'test-rp'
     )
     visit('/cancelled-registration')
 
     expect(page).to have_title I18n.t('hub.cancelled_registration.title')
+  end
+
+  it 'will render other ways page when user clicks other ways link' do
+    page.set_rack_session(
+        selected_idp: { entity_id: 'http://idcorp.com', simple_id: 'stub-idp-one' },
+        transaction_simple_id: 'test-rp'
+    )
+    visit('/cancelled-registration')
+
+    click_link 'Find out the other ways to register for an identity profile'
+    expect(page).to have_current_path(other_ways_to_access_service_path)
   end
 end
