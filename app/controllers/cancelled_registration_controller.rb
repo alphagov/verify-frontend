b1048fb2453e6f46308cb8a76294d35f8dfd5b24
nashraf <nashraf@thoughtworks.com>
2017-08-04 19:00:52 +0100

    TT-372: Add option links to cancel registration page
    - User who's registration is cancelled for whatever reason now has four
    options to continue
    
    Author: @NasAshrafThougtworks

--- app/controllers/cancelled_registration_controller.rb
+++ app/controllers/cancelled_registration_controller.rb
@@ -1,6 +1,8 @@
 class CancelledRegistrationController < ApplicationController
   def index
     @idp = IDENTITY_PROVIDER_DISPLAY_DECORATOR.decorate(selected_identity_provider)
+    @service_name = current_transaction.name
+
     render :index
   end
 end
--- spec/features/user_visits_cancelled_registration_page_spec.rb
+++ spec/features/user_visits_cancelled_registration_page_spec.rb
@@ -5,25 +5,45 @@ require 'piwik_test_helper'
 RSpec.describe 'When user visits cancelled registration page' do
   before :each do
     set_session_and_session_cookies!
+    page.set_rack_session(
+      selected_idp: { entity_id: 'http://idcorp.com', simple_id: 'stub-idp-one' },
+      transaction_simple_id: 'test-rp'
+    )
   end
 
   it 'will render itself' do
-    page.set_rack_session(
-      selected_idp: { entity_id: 'http://idcorp.com', simple_id: 'stub-idp-one' },
-    )
     visit('/cancelled-registration')
 
     expect(page).to have_title I18n.t('hub.cancelled_registration.title')
   end
 
   it 'will render other ways page when user clicks other ways link' do
-    page.set_rack_session(
-        selected_idp: { entity_id: 'http://idcorp.com', simple_id: 'stub-idp-one' },
-        transaction_simple_id: 'test-rp'
-    )
     visit('/cancelled-registration')
 
     click_link 'Find out the other ways to register for an identity profile'
     expect(page).to have_current_path(other_ways_to_access_service_path)
+  end
+
+  it 'will render idp picker page when user clicks verify with another company' do
+    stub_api_idp_list
+
+    visit('/cancelled-registration')
+
+    click_link 'Verify with another certified company'
+    expect(page).to have_current_path(choose_a_certified_company_path)
+  end
+
+  it 'will render photo identity documents page when user clicks verify using other documents link' do
+    visit('/cancelled-registration')
+
+    click_link 'Verify using other documents'
+    expect(page).to have_current_path(select_documents_path)
+  end
+
+  it 'will render feedback page when user clicks contact gov uk verify' do
+    visit('/cancelled-registration')
+
+    click_link 'Contact GOV.UK Verify'
+    expect(page).to have_current_path(feedback_path)
   end
 end
