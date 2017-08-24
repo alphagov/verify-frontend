ad1f49490b934e842e68ee0e530f0c85ff358b25
hugh-emerson <hugh.emerson@digital.cabinet-office.gov.uk>
2017-08-24 17:17:58 +0100

    TT-963: fix tests to use transaction entity id for the new config endpoint
    Re-enabling change after it was reverted.
    
    Author: @hugh-emerson

--- app/models/config_endpoints.rb
+++ app/models/config_endpoints.rb
@@ -4,6 +4,7 @@ module ConfigEndpoints
   IDP_LIST_SUFFIX = 'idps/idp-list'.freeze
 
   def idp_list_endpoint(transaction_id)
-    PATH_PREFIX.join(IDP_LIST_SUFFIX).to_s + "?transactionEntityId=#{transaction_id}"
+    transaction_id_query_parameter = { transactionEntityId: transaction_id }.to_query
+    PATH_PREFIX.join(IDP_LIST_SUFFIX).to_s + "?#{transaction_id_query_parameter}"
   end
 end
