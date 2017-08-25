f6b6a116144e01a993ddceac35d1958b81ab4684
hugh-emerson <hugh.emerson@digital.cabinet-office.gov.uk>
2017-08-25 17:11:56 +0100

    TT-963: save new session variable but don't use it
    To ensure zero downtime, save the new transaction_entity_id session variable but don't use it.
    
    Authors: @DataMinerUK, @vixus0, @hugh-emerson

--- app/models/session_response.rb
+++ app/models/session_response.rb
@@ -1,5 +1,5 @@
 class SessionResponse < Api::Response
-  attr_reader :session_id, :transaction_simple_id, :levels_of_assurance, :transaction_supports_eidas
+  attr_reader :session_id, :transaction_simple_id, :levels_of_assurance, :transaction_supports_eidas, :transaction_entity_id
   validates :session_id, :transaction_simple_id, :levels_of_assurance, presence: true
   validates :transaction_supports_eidas, inclusion: { in: [true, false] }
 
@@ -7,6 +7,7 @@ class SessionResponse < Api::Response
   def initialize(hash)
     @session_id = hash['sessionId']
     @transaction_simple_id = hash['transactionSimpleId']
+    @transaction_entity_id = hash['transactionEntityId']
     @levels_of_assurance = hash['levelsOfAssurance']
     @transaction_supports_eidas = hash['transactionSupportsEidas']
   end
