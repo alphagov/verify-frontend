718f036c6a9f4fec2a24a9b7eeba8c0a60576fed
Michael Walker <michael.walker@softwire.com>
2017-10-18 16:39:23 +0100

    TT-1394: Clear up stubs for old feature tests.
    Found these extra stubs referencing the old AB Test, so removed them too.
    
    Authors: @rachelthecodesmith, @michaelwalker

--- spec/features/user_submits_start_page_form_spec.rb
+++ spec/features/user_submits_start_page_form_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe 'when user submits start page form' do
   end
 
   it 'will display sign in with IDP page when user chooses sign in' do
-    set_session_and_ab_session_cookies!('loa1_shortened_journey_v2' => 'loa1_shortened_journey_v2_control')
     visit '/start'
     choose('start_form_selection_false')
     click_button('next-button')
