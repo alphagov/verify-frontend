7bece8e464f4629430df8533a0627cab2825f006
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2017-10-25 10:45:41 +0100

    TT-1398: Report IDP selection in a single place (partial)
    Initial commit for moving reporting to logic to a common place.
    
    Pair: @callumknights @jakubmiarka

--- app/controllers/redirect_to_idp_controller.rb
+++ app/controllers/redirect_to_idp_controller.rb
@@ -1,5 +1,10 @@
 class RedirectToIdpController < ApplicationController
   def index
+    Rails.logger.error(request)
+    FEDERATION_REPORTER.report_idp_registration(current_transaction, request, idp_name, selected_idp_names, selected_answer_store.selected_evidence, recommended)
+    
+    FEDERATION_REPORTER.report_sign_in_idp_selection(current_transaction, request, idp_name)
+    
     saml_message = SESSION_PROXY.idp_authn_request(session[:verify_session_id])
     @request = IdentityProviderRequest.new(
       saml_message,
