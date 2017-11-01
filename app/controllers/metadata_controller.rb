d1636d4c816bc30b198459aad88422b6eab48c1b
hugh-emerson <hugh.emerson@digital.cabinet-office.gov.uk>
2017-11-01 10:53:52 +0000

    TT-1462: metadata endpoints no longer return html on errors
    XML endpoints shouldn't start returning HTML when they throw an error
    
    Solo: @hugh-emerson

--- app/controllers/metadata_controller.rb
+++ app/controllers/metadata_controller.rb
@@ -3,6 +3,10 @@ class MetadataController < ApplicationController
   skip_before_action :set_piwik_custom_variables
   skip_after_action :store_locale_in_cookie
 
+  rescue_from StandardError do |exception|
+    render xml: exception, status: 500
+  end
+
   METADATA_CONTENT_TYPE = 'application/samlmetadata+xml'.freeze
 
   def service_providers
@@ -15,13 +19,13 @@ class MetadataController < ApplicationController
     render xml: metadata_client.sp_metadata, content_type: METADATA_CONTENT_TYPE
   end
 
-  def metadata_client
-    METADATA_CLIENT
-  end
-
 private
 
   def do_not_cache
     response.headers['Cache-Control'] = 'no-cache, no-store, no-transform'
+  end
+
+  def metadata_client
+    METADATA_CLIENT
   end
 end
