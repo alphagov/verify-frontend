8a9b49ceb814058173e1b82003b2ac164e0ba107
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2017-11-30 10:18:04 +0000

    TT-1606: Simplified the check for existing phone evidence
    Added feature test, pending confirmation of the final content/behaviour.
    
    Author: @jakubmiarka

--- app/controllers/clever_questions/confirming_it_is_you_controller.rb
+++ app/controllers/clever_questions/confirming_it_is_you_controller.rb
@@ -7,11 +7,8 @@ class CleverQuestions::ConfirmingItIsYouController < ApplicationController
   def select_answer
     @form = CleverQuestions::ConfirmingItIsYouForm.new(params['confirming_it_is_you_form'] || {})
     current_answers = selected_answer_store.selected_answers['phone'] || {}
-    if current_answers.length
-      selected_answer_store.store_selected_answers('phone', current_answers.merge(@form.selected_answers))
-    else
-      selected_answer_store.store_selected_answers('phone', @form.selected_answers)
-    end
+    selected_answer_store.store_selected_answers('phone', current_answers.merge(@form.selected_answers))
+    report_to_analytics('Smart Phone Next')
     redirect_to proof_of_address_path
   end
 end
--- app/controllers/clever_questions/select_phone_controller.rb
+++ app/controllers/clever_questions/select_phone_controller.rb
@@ -8,11 +8,7 @@ class CleverQuestions::SelectPhoneController < ApplicationController
     if @form.valid?
       report_to_analytics('Phone Next')
       current_answers = selected_answer_store.selected_answers['phone'] || {}
-      if current_answers.length
-        selected_answer_store.store_selected_answers('phone', current_answers.merge(@form.selected_answers))
-      else
-        selected_answer_store.store_selected_answers('phone', @form.selected_answers)
-      end
+      selected_answer_store.store_selected_answers('phone', current_answers.merge(@form.selected_answers))
       idps_available = IDP_ELIGIBILITY_CHECKER.any?(selected_evidence, current_identity_providers)
       redirect_to idps_available ? choose_a_certified_company_path : no_mobile_phone_path
     else
