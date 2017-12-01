1898a3de90e46c8d43571f88cd8b7f163f9367de
Michael Walker <michael.walker@softwire.com>
2017-12-01 10:37:46 +0000

    TT-1715: add micro survey to measure demand for F2F supported verification
    Pair: @michaelWalker, @hugh-emerson

---
+++ app/assets/javascripts/supported_verification_survey.js
@@ -0,0 +1,28 @@
+(function(global) {
+    "use strict";
+    var GOVUK = global.GOVUK || {};
+    var $ = global.jQuery;
+
+    GOVUK.supportedVerificationSurvey = {
+        init: function() {
+            var supportedVerificationSurvey = $('#supported-verification-survey');
+
+            function recordResponse(response) {
+                global._paq.push(['trackEvent', 'Micro Survey', response, 'F2F Support']);
+                supportedVerificationSurvey.find('.question').hide();
+                supportedVerificationSurvey.find('.thank-you-message').show();
+            }
+
+            if (supportedVerificationSurvey.length) {
+                $('#answer-yes').click(function() {
+                    recordResponse('Yes');
+                    return false;
+                });
+                $('#answer-no').click(function() {
+                    recordResponse('No');
+                    return false;
+                });
+            }
+        }
+    };
+})(window);
