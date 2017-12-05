fce1b87b0ffa8c25925017b72e6b1a3b689d25b3
Rachel Smith <rachelcodesmith@outlook.com>
2017-12-05 13:43:43 +0000

    tt-1609: Refactor to sting interpolation Pair: @rachelthecodesmith @CharlesIC

--- app/models/config_endpoints.rb
+++ app/models/config_endpoints.rb
@@ -1,17 +1,19 @@
 module ConfigEndpoints
   PATH = '/config'.freeze
   PATH_PREFIX = Pathname(PATH)
-  IDP_LIST_SUFFIX = 'idps/idp-list/%s/%s'.freeze
-  IDP_LIST_SIGN_IN_SUFFIX = 'idps/idp-list-for-sign-in/%s'.freeze
-  DISPLAY_DATA_SUFFIX = 'transactions/%s/display-data'.freeze
+  IDP_LIST_SUFFIX = 'idps/idp-list/%<transaction_name>s/%<loa>s'.freeze
+  IDP_LIST_SIGN_IN_SUFFIX = 'idps/idp-list-for-sign-in/%<transaction_name>s'.freeze
+  DISPLAY_DATA_SUFFIX = 'transactions/%<transaction_entity_id>s/display-data'.freeze
   TRANSACTIONS_SUFFIX = 'transactions/enabled'.freeze
 
   def idp_list_for_loa_endpoint(transaction_id, loa)
-    PATH_PREFIX.join(IDP_LIST_SUFFIX % [CGI.escape(transaction_id), CGI.escape(loa)]).to_s
+    # PATH_PREFIX.join(IDP_LIST_SUFFIX % [CGI.escape(transaction_id), CGI.escape(loa)]).to_s
+    PATH_PREFIX.join(IDP_LIST_SUFFIX % { transaction_name: CGI.escape(transaction_id), loa: CGI.escape(loa) }).to_s
   end
 
   def idp_list_for_sign_in_endpoint(transaction_id)
-    PATH_PREFIX.join(IDP_LIST_SIGN_IN_SUFFIX % [CGI.escape(transaction_id)]).to_s
+    # PATH_PREFIX.join(IDP_LIST_SIGN_IN_SUFFIX % [CGI.escape(transaction_id)]).to_s
+    PATH_PREFIX.join(IDP_LIST_SIGN_IN_SUFFIX % { transaction_name: CGI.escape(transaction_id) }).to_s
   end
 
   def transactions_endpoint
@@ -19,6 +21,6 @@ module ConfigEndpoints
   end
 
   def transaction_display_data_endpoint(transaction_entity_id)
-    PATH_PREFIX.join(DISPLAY_DATA_SUFFIX % CGI.escape(transaction_entity_id)).to_s
+    PATH_PREFIX.join(DISPLAY_DATA_SUFFIX % { transaction_entity_id: CGI.escape(transaction_entity_id) }).to_s
   end
 end
