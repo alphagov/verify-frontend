5e7c4665dfdb9a981a49625241ef8a0e346f834f
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2017-12-28 11:52:31 +0000

    TT-1766: Add IDP logos to "about certified companies" page in LOA1
    Authors: @CharlesIC, @hugh-emerson

--- spec/features/user_visits_about_certified_companies_page_spec.rb
+++ spec/features/user_visits_about_certified_companies_page_spec.rb
@@ -4,44 +4,89 @@ require 'cookie_names'
 
 RSpec.describe 'When the user visits the about certified companies page' do
   let(:simple_id) { 'stub-idp-one' }
+  let(:simple_id_loa1) { 'stub-idp-loa1' }
   let(:idp_entity_id) { 'http://idcorp.com' }
 
-  before(:each) do
-    stub_transactions_list
-    stub_api_idp_list_for_loa
-    set_session_and_session_cookies!
+  context 'loa2' do
+    before(:each) do
+      stub_transactions_list
+      stub_api_idp_list_for_loa(default_idps, 'LEVEL_2')
+      set_session_and_session_cookies!
+    end
+
+    it 'includes the appropriate feedback source' do
+      visit '/about-certified-companies'
+
+      expect_feedback_source_to_be(page, 'ABOUT_CERTIFIED_COMPANIES_PAGE', '/about-certified-companies')
+    end
+
+    it 'displays content in Welsh' do
+      visit '/am-gwmniau-ardystiedig'
+
+      expect(page).to have_content 'Sut y gall cwmnïau wirio hunaniaeth'
+      expect(page).to have_content 'Gall y cwmnïau hyn ddefnyddio eu data eu hunain'
+    end
+
+    it 'displays IdPs that are enabled' do
+      visit '/about-certified-companies'
+
+      expect(page).to have_css("img[src*='/white/#{simple_id}']")
+    end
+
+    it 'will show "How companies can verify identities" section' do
+      visit '/about-certified-companies'
+
+      expect(page).to have_content 'How companies can verify identities'
+      expect(page).to have_content 'These companies can use their own data'
+    end
+
+    it 'will go to about identity accounts page when next is clicked' do
+      visit '/about-certified-companies'
+      click_link('Next')
+
+      expect(page).to have_current_path('/about-identity-accounts')
+    end
   end
 
-  it 'includes the appropriate feedback source' do
-    visit '/about-certified-companies'
+  context 'loa1' do
+    before(:each) do
+      stub_transactions_list
+      stub_api_idp_list_for_loa(default_idps, 'LEVEL_1')
+      set_session_and_session_cookies!
+      set_loa_in_session('LEVEL_1')
+    end
 
-    expect_feedback_source_to_be(page, 'ABOUT_CERTIFIED_COMPANIES_PAGE', '/about-certified-companies')
-  end
+    it 'includes the appropriate feedback source' do
+      visit '/about-certified-companies'
 
-  it 'displays content in Welsh' do
-    visit '/am-gwmniau-ardystiedig'
+      expect_feedback_source_to_be(page, 'ABOUT_CERTIFIED_COMPANIES_PAGE', '/about-certified-companies')
+    end
 
-    expect(page).to have_content 'Sut y gall cwmnïau wirio hunaniaeth'
-    expect(page).to have_content 'Gall y cwmnïau hyn ddefnyddio eu data eu hunain'
-  end
+    it 'displays content in Welsh' do
+      visit '/am-gwmniau-ardystiedig'
 
-  it 'displays IdPs that are enabled' do
-    visit '/about-certified-companies'
+      expect(page).to have_content 'Sut y gall cwmnïau wirio hunaniaeth'
+      expect(page).to have_content 'Gall y cwmnïau hyn ddefnyddio eu data eu hunain'
+    end
 
-    expect(page).to have_css("img[src*='/white/#{simple_id}']")
-  end
+    it 'displays IdPs that are enabled' do
+      visit '/about-certified-companies'
 
-  it 'will show "How companies can verify identities" section' do
-    visit '/about-certified-companies'
+      expect(page).to have_css("img[src*='/white/#{simple_id_loa1}']")
+    end
 
-    expect(page).to have_content 'How companies can verify identities'
-    expect(page).to have_content 'These companies can use their own data'
-  end
+    it 'will show "How companies can verify identities" section' do
+      visit '/about-certified-companies'
 
-  it 'will go to about identity accounts page when next is clicked' do
-    visit '/about-certified-companies'
-    click_link('Next')
+      expect(page).to have_content 'How companies can verify identities'
+      expect(page).to have_content 'These companies can use their own data'
+    end
 
-    expect(page).to have_current_path('/about-identity-accounts')
+    it 'will go to about identity accounts page when next is clicked' do
+      visit '/about-certified-companies'
+      click_link('Next')
+
+      expect(page).to have_current_path('/about-identity-accounts')
+    end
   end
 end
