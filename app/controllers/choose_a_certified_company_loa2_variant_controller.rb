d71d0e394a7289e4d8d0dacdd54212897785c672
Rachel Smith <rachelcodesmith@outlook.com>
2018-03-06 11:20:27 +0000

    HUB-71: AB test to remove the questions section from the LoA2 hub journey.
    Co-authored-by: Simon Wilson <simon.wilson@digital.cabinet-office.gov.uk>

---
+++ app/controllers/choose_a_certified_company_loa2_variant_controller.rb
@@ -0,0 +1,23 @@
+require 'partials/viewable_idp_partial_controller'
+
+class ChooseACertifiedCompanyLoa2VariantController < ApplicationController
+  include ChooseACertifiedCompanyAbout
+  include ViewableIdpPartialController
+
+  def index
+    @identity_providers = IDENTITY_PROVIDER_DISPLAY_DECORATOR.decorate_collection(current_identity_providers_for_loa)
+
+    render 'choose_a_certified_company/choose_a_certified_company_LOA2_variant'
+  end
+
+  def select_idp
+    select_viewable_idp_for_loa(params.fetch('entity_id'))
+    redirect_to warning_page
+  end
+
+private
+
+  def warning_page
+    redirect_to_idp_warning_path
+  end
+end
