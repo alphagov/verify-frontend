7f9458ba811526e988cf94d87efecfae33284a0a
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2018-03-12 14:13:36 +0000

    HUB-64: Make changes for AB Test 7 (Questions "Light")
    Co-authored-by: Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>

--- config/main_routes.rb
+++ config/main_routes.rb
@@ -2,23 +2,15 @@ def add_routes(routes_name)
   instance_eval(File.read(Rails.root.join("config/#{routes_name}.rb")))
 end
 
+QUESTIONS_LIGHT = "questions_light".freeze
+
+questions_light_control_piwik = SelectRoute.new(QUESTIONS_LIGHT, 'control', is_start_of_test: true, experiment_loa: 'LEVEL_2')
+questions_light_control = SelectRoute.new(QUESTIONS_LIGHT, 'control', is_start_of_test: false, experiment_loa: 'LEVEL_2')
+questions_light_variant_piwik = SelectRoute.new(QUESTIONS_LIGHT, 'variant', is_start_of_test: true, experiment_loa: 'LEVEL_2')
+questions_light_variant = SelectRoute.new(QUESTIONS_LIGHT, 'variant', is_start_of_test: false, experiment_loa: 'LEVEL_2')
+
 get 'sign_in', to: 'sign_in#index', as: :sign_in
 post 'sign_in', to: 'sign_in#select_idp', as: :sign_in_submit
-
-get 'select_documents', to: 'select_documents#index', as: :select_documents
-get 'select_documents_none', to: 'select_documents#no_documents', as: :select_documents_no_documents
-post 'select_documents', to: 'select_documents#select_documents', as: :select_documents_submit
-get 'unlikely_to_verify', to: 'select_documents#unlikely_to_verify', as: :unlikely_to_verify
-get 'other_identity_documents', to: 'other_identity_documents#index', as: :other_identity_documents
-post 'other_identity_documents', to: 'other_identity_documents#select_other_documents', as: :other_identity_documents_submit
-get 'select_phone', to: 'select_phone#index', as: :select_phone
-post 'select_phone', to: 'select_phone#select_phone', as: :select_phone_submit
-get 'no_mobile_phone', to: 'select_phone#no_mobile_phone', as: :no_mobile_phone
-get 'will_it_work_for_me', to: 'will_it_work_for_me#index', as: :will_it_work_for_me
-post 'will_it_work_for_me', to: 'will_it_work_for_me#will_it_work_for_me', as: :will_it_work_for_me_submit
-get 'why_might_this_not_work_for_me', to: 'will_it_work_for_me#why_might_this_not_work_for_me', as: :why_might_this_not_work_for_me
-get 'may_not_work_if_you_live_overseas', to: 'will_it_work_for_me#may_not_work_if_you_live_overseas', as: :may_not_work_if_you_live_overseas
-get 'will_not_work_without_uk_address', to: 'will_it_work_for_me#will_not_work_without_uk_address', as: :will_not_work_without_uk_address
 
 constraints IsLoa1 do
   get 'start', to: 'start#index', as: :start
@@ -44,6 +36,24 @@ constraints IsLoa2 do
   get 'start', to: 'start#index', as: :start
   post 'start', to: 'start#request_post', as: :start
   get 'begin_registration', to: 'start#register', as: :begin_registration
+  get 'about', to: 'about_loa2#index', as: :about
+  get 'about_certified_companies', to: 'about_loa2#certified_companies', as: :about_certified_companies
+  get 'about_identity_accounts', to: 'about_loa2#identity_accounts', as: :about_identity_accounts
+  # HUB-64: Questions "Light" AB test - add commented out routes to tear down
+  # get 'about_choosing_a_company', to: 'about_loa2#choosing_a_company', as: :about_choosing_a_company
+  # get 'will_it_work_for_me', to: 'will_it_work_for_me#index', as: :will_it_work_for_me
+  # post 'will_it_work_for_me', to: 'will_it_work_for_me#will_it_work_for_me', as: :will_it_work_for_me_submit
+  # get 'why_might_this_not_work_for_me', to: 'will_it_work_for_me#why_might_this_not_work_for_me', as: :why_might_this_not_work_for_me
+  # get 'may_not_work_if_you_live_overseas', to: 'will_it_work_for_me#may_not_work_if_you_live_overseas', as: :may_not_work_if_you_live_overseas
+  # get 'will_not_work_without_uk_address', to: 'will_it_work_for_me#will_not_work_without_uk_address', as: :will_not_work_without_uk_address
+  # get 'select_documents', to: 'select_documents#index', as: :select_documents
+  # get 'select_documents_none', to: 'select_documents#no_documents', as: :select_documents_no_documents
+  # post 'select_documents', to: 'select_documents#select_documents', as: :select_documents_submit
+  # get 'other_identity_documents', to: 'other_identity_documents#index', as: :other_identity_documents
+  # post 'other_identity_documents', to: 'other_identity_documents#select_other_documents', as: :other_identity_documents_submit
+  get 'select_phone', to: 'select_phone#index', as: :select_phone
+  post 'select_phone', to: 'select_phone#select_phone', as: :select_phone_submit
+  get 'no_mobile_phone', to: 'select_phone#no_mobile_phone', as: :no_mobile_phone
   get 'choose_a_certified_company', to: 'choose_a_certified_company_loa2#index', as: :choose_a_certified_company
   post 'choose_a_certified_company', to: 'choose_a_certified_company_loa2#select_idp', as: :choose_a_certified_company_submit
   get 'choose_a_certified_company/:company', to: 'choose_a_certified_company_loa2#about', as: :choose_a_certified_company_about
@@ -54,10 +64,7 @@ constraints IsLoa2 do
   post 'redirect_to_idp_question', to: 'redirect_to_idp_question_loa2#continue', as: :redirect_to_idp_question_submit
   get 'idp_wont_work_for_you_one_doc', to: 'redirect_to_idp_question_loa2#idp_wont_work_for_you', as: :idp_wont_work_for_you_one_doc
   get 'confirmation', to: 'confirmation_loa2#index', as: :confirmation
-  get 'about', to: 'about_loa2#index', as: :about
-  get 'about_certified_companies', to: 'about_loa2#certified_companies', as: :about_certified_companies
-  get 'about_identity_accounts', to: 'about_loa2#identity_accounts', as: :about_identity_accounts
-  get 'about_choosing_a_company', to: 'about_loa2#choosing_a_company', as: :about_choosing_a_company
+  get 'unlikely_to_verify', to: 'select_documents#unlikely_to_verify', as: :unlikely_to_verify
 end
 
 post 'redirect_to_idp_warning', to: 'redirect_to_idp_warning#continue', as: :redirect_to_idp_warning_submit
@@ -93,3 +100,38 @@ post 'further_information_null_attribute', to: 'further_information#submit_null_
 get 'no_idps_available', to: 'no_idps_available#index', as: :no_idps_available
 get 'cancelled_registration', to: 'cancelled_registration#index', as: :cancelled_registration
 get 'paused_registration', to: 'paused_registration#index', as: :paused_registration
+
+# HUB-64: Questions "Light" AB test - remove below constraints to tear down
+constraints questions_light_control_piwik do
+  get 'about_choosing_a_company', to: 'about_loa2#choosing_a_company', as: :about_choosing_a_company
+end
+
+constraints questions_light_variant_piwik do
+  get 'about_choosing_a_company', to: 'about_loa2_variant#choosing_a_company', as: :about_choosing_a_company
+end
+
+constraints questions_light_control do
+  get 'will_it_work_for_me', to: 'will_it_work_for_me#index', as: :will_it_work_for_me
+  post 'will_it_work_for_me', to: 'will_it_work_for_me#will_it_work_for_me', as: :will_it_work_for_me_submit
+  get 'why_might_this_not_work_for_me', to: 'will_it_work_for_me#why_might_this_not_work_for_me', as: :why_might_this_not_work_for_me
+  get 'may_not_work_if_you_live_overseas', to: 'will_it_work_for_me#may_not_work_if_you_live_overseas', as: :may_not_work_if_you_live_overseas
+  get 'will_not_work_without_uk_address', to: 'will_it_work_for_me#will_not_work_without_uk_address', as: :will_not_work_without_uk_address
+
+  get 'select_documents', to: 'select_documents#index', as: :select_documents
+  get 'select_documents_none', to: 'select_documents#no_documents', as: :select_documents_no_documents
+  post 'select_documents', to: 'select_documents#select_documents', as: :select_documents_submit
+  get 'unlikely_to_verify', to: 'select_documents#unlikely_to_verify', as: :unlikely_to_verify
+
+  get 'other_identity_documents', to: 'other_identity_documents#index', as: :other_identity_documents
+  post 'other_identity_documents', to: 'other_identity_documents#select_other_documents', as: :other_identity_documents_submit
+end
+
+constraints questions_light_variant do
+  get 'select_documents', to: 'select_documents_variant#index', as: :select_documents
+  get 'select_documents_none', to: 'select_documents_variant#no_documents', as: :select_documents_no_documents
+  post 'select_documents', to: 'select_documents_variant#select_documents', as: :select_documents_submit
+  get 'unlikely_to_verify', to: 'select_documents_variant#unlikely_to_verify', as: :unlikely_to_verify
+
+  get 'other_identity_documents', to: 'other_identity_documents_variant#index', as: :other_identity_documents
+  post 'other_identity_documents', to: 'other_identity_documents_variant#select_other_documents', as: :other_identity_documents_submit
+end
