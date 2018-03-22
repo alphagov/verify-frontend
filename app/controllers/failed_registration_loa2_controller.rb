5e6dd1a51e56b732fc9c6798a778603f521d358d
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2018-03-22 17:39:53 +0000

    HUB-87: Custom failed registration page
    Added a feature to show a custom failed registration page for a specific RP. This triggered by the
    presence of a yml flag 'custom_fail_heading' in the RP yml file. This then renders a partial view
    with the custom content, with the custom content itself specified in the RP yml file.
    
    Also added a feature test that tests the custom failed registration page shows when it should do.

--- app/controllers/failed_registration_loa2_controller.rb
+++ app/controllers/failed_registration_loa2_controller.rb
@@ -2,7 +2,7 @@ class FailedRegistrationLoa2Controller < ApplicationController
   def index
     @idp = IDENTITY_PROVIDER_DISPLAY_DECORATOR.decorate(selected_identity_provider)
     @transaction = current_transaction
-
+    @custom_fail = !current_transaction.custom_fail_heading.nil?
     if CONTINUE_ON_FAILED_REGISTRATION_RPS.include?(current_transaction_simple_id)
       render 'failed_registration/index_continue_on_failed_registration_LOA2'
     else
--- app/models/display/rp_display_data.rb
+++ app/models/display/rp_display_data.rb
@@ -7,5 +7,11 @@ module Display
     content :other_ways_text
     content :analytics_description
     content :tailored_text
+    content :custom_fail_heading, default: nil
+    content :custom_fail_what_next_content, default: nil
+    content :custom_fail_other_options, default: nil
+    content :custom_fail_try_another_summary, default: nil
+    content :custom_fail_try_another_text, default: nil
+    content :custom_fail_contact_details_intro, default: nil
   end
 end
