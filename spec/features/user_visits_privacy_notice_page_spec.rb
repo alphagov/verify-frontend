1d5072a8fbdd5c66efed320701e86041bd9d917c
Rachel Smith <rachelcodesmith@outlook.com>
2018-05-11 15:04:59 +0100

    HUB-141: Text change for privacy notice
    Copy change for static privacy notice page. English only for the time being while we await Welsh translation.
    
    Solo: @rachelthecodesmith

--- spec/features/user_visits_privacy_notice_page_spec.rb
+++ spec/features/user_visits_privacy_notice_page_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe 'When the user visits the privacy notice page' do
   it 'displays the page in English' do
     visit '/privacy-notice'
     expect(page).to have_title t('hub.privacy_notice.title')
-    expect(page).to have_link 'Summary', href: '#summary'
-    expect(page).to have_content 'GOV.UK Verify is provided by the Government Digital Service (GDS). We use your personal data to help you to transact securely with government services.'
-    expect(page).to have_link 'Privacy policies of certified companies and government departments', href: '#privacy-policies-of-certified-companies-and-government-departments'
-    expect(page).to have_content 'The certified companies have their own privacy policies that apply to their handling of your personal data as part of the identity verification process. Likewise, the government services have their own privacy policies that apply to their handling of your personal data when you use their services.'
+    expect(page).to have_content 'To verify your identity, we will pass the personal data that you give to your chosen certified company to the government service that you want to access.'
+    expect(page).to have_link 'DPO@cabinetoffice.gov.uk', href: 'DPO@cabinetoffice.gov.uk'
+    expect(page).to have_content 'The data controller for your personal data is the Cabinet Office – a data controller determines how and why personal data is processed.'
   end
 
   it 'displays the page in Welsh' do
