ffdf720f3e3c481d800dbe647d8eca890bd50640
Rachel Smith <rachelcodesmith@outlook.com>
2018-05-14 14:22:34 +0100

    HUB-141: Fix incorrect tag type and add mailto to email links
    Fix incorrect tag type and add mailto to email links
    
    Co-Authored-by: Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
    Co-Authored-by: Callum Knights <callum.knights@digital.cabinet-office.gov.uk>

--- spec/features/user_visits_privacy_notice_page_spec.rb
+++ spec/features/user_visits_privacy_notice_page_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'When the user visits the privacy notice page' do
     visit '/privacy-notice'
     expect(page).to have_title t('hub.privacy_notice.title')
     expect(page).to have_content 'To verify your identity, we will pass the personal data that you give to your chosen certified company to the government service that you want to access.'
-    expect(page).to have_link 'DPO@cabinetoffice.gov.uk', href: 'DPO@cabinetoffice.gov.uk'
+    expect(page).to have_link 'DPO@cabinetoffice.gov.uk', href: 'mailto:DPO@cabinetoffice.gov.uk'
     expect(page).to have_content 'The data controller for your personal data is the Cabinet Office – a data controller determines how and why personal data is processed.'
   end
 
