6bca4570f6f11a59fa110df5c367445afa883790
Rylan Gooch <rylan.gooch@digital.cabinet-office.gov.uk>
2018-05-21 16:43:14 +0100

    Hub-135 Spike: recognise existing GOV.UK users on interstitial page
    Adds endpoint allowing us to test whether we can retrieve data from verify-frontend using a request from GOV.UK
    
    Co-Authored-By: Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>

---
+++ app/controllers/hint_controller.rb
@@ -0,0 +1,20 @@
+require 'partials/journey_hinting_partial_controller'
+
+class HintController < ApplicationController
+  include JourneyHintingPartialController
+  skip_before_action :validate_session
+  skip_before_action :set_piwik_custom_variables
+
+  def index
+    set_headers
+
+    entity_id = entity_id_of_journey_hint_for('SUCCESS')
+
+    render json: { 'status': 'OK', 'value': !entity_id.nil? }
+  end
+
+  def set_headers
+    response.headers['Access-Control-Allow-Origin'] = 'https://www.gov.uk'
+    response.headers['Access-Control-Request-Method'] = 'GET'
+  end
+end
