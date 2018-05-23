f2cee3c04cfa95a1194f0324b27789c291244f06
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2018-05-23 14:38:23 +0100

    HUB-157: Updates to privacy policy wording
    Co-Authored-By: Rachel Smith <rachel.smith@digital.cabinet-office.gov.uk>

--- spec/features/user_visits_privacy_notice_page_spec.rb
+++ spec/features/user_visits_privacy_notice_page_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'When the user visits the privacy notice page' do
   it 'displays the page in English' do
     visit '/privacy-notice'
     expect(page).to have_title t('hub.privacy_notice.title')
-    expect(page).to have_content 'To verify your identity, we will pass the personal data that you give to your chosen certified company to the government service that you want to access.'
+    expect(page).to have_content 'We process some personal data to help to prevent fraud in government services.'
     expect(page).to have_link 'DPO@cabinetoffice.gov.uk', href: 'mailto:DPO@cabinetoffice.gov.uk'
     expect(page).to have_content 'The data controller for your personal data is the Cabinet Office – a data controller determines how and why personal data is processed.'
   end
