3e242b4b644a0d64facfa40829c4d628ac2c8a94
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2018-05-23 16:46:02 +0100

    CTV-201 Create stubs for translations from config service
    This commit adds stubs for the endpoints in the config service which will serve translations to the frontend. It also adds in some infrastructure that is necessary to get translations from an external source rather than from local YAML files.
    
    Solo: @karlbaker02

--- app/models/config_proxy.rb
+++ app/models/config_proxy.rb
@@ -10,6 +10,21 @@ class ConfigProxy
     TransactionResponse.validated_response(response)
   end
 
+  def get_transaction_translations(transaction_entity_id, locale)
+    response = @api_client.get(transaction_translation_data_endpoint(transaction_entity_id, locale))
+    translations_for_locale = TransactionTranslationResponse.validated_response(response)
+
+    {
+      name: translations_for_locale.name,
+      rp_name: translations_for_locale.rp_name,
+      analytics_description: translations_for_locale.analytics_description,
+      other_ways_text: translations_for_locale.other_ways_text,
+      other_ways_description: translations_for_locale.other_ways_description,
+      tailored_text: translations_for_locale.tailored_text,
+      taxon_name: translations_for_locale.taxon_name
+    }
+  end
+
   def transactions
     @api_client.get(transactions_endpoint)
   end
---
+++ app/services/rp_translation_service.rb
@@ -0,0 +1,43 @@
+class RpTranslationService
+  def initialize(translator)
+    @translator = translator
+    @locales = %w(en cy)
+
+    update_rp_translations
+  end
+
+  def get_transactions
+    CONFIG_PROXY.transactions.map do |transaction|
+      transaction['simpleId']
+    end
+  end
+
+  def update_rp_translations
+    # Get transactions from external endpoint
+    # This could use all translations to get a list of transactions,
+    # depending on how the returned data is structured.
+    transactions = get_transactions
+
+    transactions.each do |transaction|
+      @locales.each do |locale|
+        translations = get_translations(transaction, locale)
+
+        I18n.backend.store_translations(locale, {
+          rps: Hash[transaction, translations]
+        })
+      end
+    end
+
+    if defined? RP_DISPLAY_REPOSITORY
+      RP_DISPLAY_REPOSITORY.merge!({
+        transaction => Display::RpDisplayData.new(transaction, @translator)
+      })
+    end
+  end
+
+  private
+
+  def get_translations(transaction, locale)
+    CONFIG_PROXY.get_transaction_translations(transaction, locale)
+  end
+end
