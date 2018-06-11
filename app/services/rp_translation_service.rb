1658099eb430e9a6f0f1dbaf743792904029da1a
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2018-06-11 10:20:54 +0100

    Use correct config service endpoint for translations

--- app/services/rp_translation_service.rb
+++ app/services/rp_translation_service.rb
@@ -14,28 +14,28 @@ class RpTranslationService
     # This could use all translations to get a list of transactions,
     # depending on how the returned data is structured.
     transactions = get_transactions
+    locales = %w(en cy)
 
     transactions.each do |transaction|
-      translations = get_translations(transaction)
+      locales.each do |locale|
+        translations = get_translations(transaction, locale)
 
-      # This is making the assumption that the returned data will be keyed by locale.
-      translations.map do |locale, translations_for_locale|
         I18n.backend.store_translations(locale, {
-          rps: Hash[transaction, translations_for_locale]
+            rps: Hash[transaction, translations]
         })
       end
+    end
 
-      if defined? RP_DISPLAY_REPOSITORY
-        RP_DISPLAY_REPOSITORY.merge!({
-          transaction => Display::RpDisplayData.new(transaction, @translator)
-        })
-      end
+    if defined? RP_DISPLAY_REPOSITORY
+      RP_DISPLAY_REPOSITORY.merge!({
+        transaction => Display::RpDisplayData.new(transaction, @translator)
+      })
     end
   end
 
   private
 
-  def get_translations(transaction)
-    CONFIG_PROXY.get_transaction_translations(transaction)
+  def get_translations(transaction, locale)
+    CONFIG_PROXY.get_transaction_translations(transaction, locale)
   end
 end
