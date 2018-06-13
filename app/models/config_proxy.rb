d09ae6dd22c650387d4084534ec240858f4a85bb
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2018-06-13 14:31:44 +0100

    Add missing fields to translation response

--- app/models/config_proxy.rb
+++ app/models/config_proxy.rb
@@ -14,15 +14,7 @@ class ConfigProxy
     response = @api_client.get(transaction_translation_data_endpoint(transaction_entity_id, locale))
     translations_for_locale = TransactionTranslationResponse.validated_response(response)
 
-    {
-      name: translations_for_locale.name,
-      rp_name: translations_for_locale.rp_name,
-      analytics_description: translations_for_locale.analytics_description,
-      other_ways_text: translations_for_locale.other_ways_text,
-      other_ways_description: translations_for_locale.other_ways_description,
-      tailored_text: translations_for_locale.tailored_text,
-      taxon_name: translations_for_locale.taxon_name
-    }
+    translations_for_locale.to_h
   end
 
   def transactions
