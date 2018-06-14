914e1019d8fbd5486c0596d91d5e26ed423dff7a
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2018-06-14 10:54:52 +0100

    HUB-167: Updated styling as per Codacy
    And fixed the undefined element variable.

--- public/get_hint.js
+++ public/get_hint.js
@@ -5,28 +5,26 @@ $(document).ready(function() {
         dataType: 'jsonp'
       }).then(function(data){
         if(data.value){
-            var verifyRadio = $('input[value=sign-in-with-gov-uk-verify]')
-            var verifyContainer = verifyRadio.parent();
-            verifyContainer.prev().insertAfter(verifyContainer);
-            GOVUK.analytics.trackEvent('verify-hint', 'shown', { transport: 'beacon' })
+          var verifyRadio = $('input[value=sign-in-with-gov-uk-verify]');
+          var verifyContainer = verifyRadio.parent();
+          verifyContainer.prev().insertAfter(verifyContainer);
+          GOVUK.analytics.trackEvent('verify-hint', 'shown', { transport: 'beacon' })
 
-            element.on('submit', function (event) {
-                var options = { transport: 'beacon' }
-        
-                var $submittedForm = $(event.target)
-        
-                var $checkedOption = $submittedForm.find('input:checked')
-        
-                var checkedValue = $checkedOption.val()
-        
-                if (typeof checkedValue === 'undefined') {
-                  checkedValue = 'submitted-without-choosing'
-                }
-        
-                GOVUK.analytics.trackEvent('Radio button chosen', checkedValue + '-with-hint', options)
-              })
-
-
+          $('*[data-module="track-radio-group"]').on('submit', function (event) {
+            var options = { transport: 'beacon' };
+    
+            var $submittedForm = $(event.target);
+    
+            var $checkedOption = $submittedForm.find('input:checked');
+    
+            var checkedValue = $checkedOption.val();
+    
+            if (typeof checkedValue === 'undefined') {
+              checkedValue = 'submitted-without-choosing';
+            }
+    
+            GOVUK.analytics.trackEvent('Radio button chosen', checkedValue + '-with-hint', options);
+          });
         }
       });
 });
