5b891f676590533ad8008f3723fbbe5f98bb33fa
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2018-06-19 16:36:10 +0100

    Remove cache warm-up on app start-up

---
+++ app/models/display/rp_translation_repository.rb
@@ -0,0 +1,18 @@
+module Display
+  class RpTranslationRepository
+    def initialize(translator)
+      @translator = translator
+      @display_data = {}
+    end
+
+    def get_translations(transaction_simple_id)
+      unless @display_data.has_key?(transaction_simple_id)
+        display_data = Display::RpDisplayData.new(transaction_simple_id, @translator)
+        display_data.validate_content!
+        @display_data[transaction_simple_id] = display_data
+      end
+
+      @display_data.fetch(transaction_simple_id)
+    end
+  end
+end
