743ec41d06100f9f78ff71529f42ebf934cbd133
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2018-06-21 14:33:50 +0100

    Refactor rp repo

--- app/models/display/rp_display_repository.rb
+++ app/models/display/rp_display_repository.rb
@@ -16,9 +16,7 @@ module Display
 
     def get_translations(transaction_simple_id)
       unless @display_data.has_key?(transaction_simple_id)
-        display_data = Display::RpDisplayData.new(transaction_simple_id, @translator)
-        display_data.validate_content!
-        @display_data[transaction_simple_id] = display_data
+        update_display_data(transaction_simple_id)
       end
 
       @display_data.fetch(transaction_simple_id)
