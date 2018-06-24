2bb420926e44b4c632dbae4d3171be708bea3c3f
Rachel Smith <rachelcodesmith@outlook.com>
2018-06-24 18:16:00 +0100

    HUB-164: Re-run Questions Light A/B test
    The previous Questions Light A/B test was a success, we're now re-running
    it to confirm and validate the results
    
    solo: @rachelthecodesmith
    solo: @jakubmiarka

--- config/main_routes.rb
+++ config/main_routes.rb
@@ -11,6 +11,14 @@ LOA1_PERF_MANAGEMENT_V2 = "loa1_perf_management_v2".freeze
 loa1_perf_management_v2_control_piwik = SelectRoute.new(LOA1_PERF_MANAGEMENT_V2, 'control', is_start_of_test: true, experiment_loa: 'LEVEL_1')
 loa1_perf_management_v2_variant_piwik = SelectRoute.new(LOA1_PERF_MANAGEMENT_V2, 'variant', is_start_of_test: true, experiment_loa: 'LEVEL_1')
 
+
+QUESTIONS_LIGHT_V2 = "questions_light_v2".freeze
+
+questions_light_v2_control_piwik = SelectRoute.new(QUESTIONS_LIGHT_V2, 'control', is_start_of_test: true, experiment_loa: 'LEVEL_2')
+questions_light_v2_control = SelectRoute.new(QUESTIONS_LIGHT_V2, 'control', is_start_of_test: false, experiment_loa: 'LEVEL_2')
+questions_light_v2_variant_piwik = SelectRoute.new(QUESTIONS_LIGHT_V2, 'variant', is_start_of_test: true, experiment_loa: 'LEVEL_2')
+questions_light_v2_variant = SelectRoute.new(QUESTIONS_LIGHT_V2, 'variant', is_start_of_test: false, experiment_loa: 'LEVEL_2')
+
 constraints IsLoa1 do
   get 'start', to: 'start#index', as: :start
   post 'start', to: 'start#request_post', as: :start
@@ -46,26 +54,26 @@ constraints IsLoa2 do
   get 'start', to: 'start#index', as: :start
   post 'start', to: 'start#request_post', as: :start
   get 'begin_registration', to: 'start#register', as: :begin_registration
-  get 'about', to: 'about_loa2#index', as: :about
-  get 'about_certified_companies', to: 'about_loa2#certified_companies', as: :about_certified_companies
-  get 'about_identity_accounts', to: 'about_loa2#identity_accounts', as: :about_identity_accounts
-  get 'about_choosing_a_company', to: 'about_loa2#choosing_a_company', as: :about_choosing_a_company
-  get 'will_it_work_for_me', to: 'will_it_work_for_me#index', as: :will_it_work_for_me
-  post 'will_it_work_for_me', to: 'will_it_work_for_me#will_it_work_for_me', as: :will_it_work_for_me_submit
+  # get 'about', to: 'about_loa2#index', as: :about
+  # get 'about_certified_companies', to: 'about_loa2#certified_companies', as: :about_certified_companies
+  # get 'about_identity_accounts', to: 'about_loa2#identity_accounts', as: :about_identity_accounts
+  # get 'about_choosing_a_company', to: 'about_loa2#choosing_a_company', as: :about_choosing_a_company
+  # get 'will_it_work_for_me', to: 'will_it_work_for_me#index', as: :will_it_work_for_me
+  # post 'will_it_work_for_me', to: 'will_it_work_for_me#will_it_work_for_me', as: :will_it_work_for_me_submit
   get 'why_might_this_not_work_for_me', to: 'will_it_work_for_me#why_might_this_not_work_for_me', as: :why_might_this_not_work_for_me
-  get 'may_not_work_if_you_live_overseas', to: 'will_it_work_for_me#may_not_work_if_you_live_overseas', as: :may_not_work_if_you_live_overseas
-  get 'will_not_work_without_uk_address', to: 'will_it_work_for_me#will_not_work_without_uk_address', as: :will_not_work_without_uk_address
+  # get 'may_not_work_if_you_live_overseas', to: 'will_it_work_for_me#may_not_work_if_you_live_overseas', as: :may_not_work_if_you_live_overseas
+  # get 'will_not_work_without_uk_address', to: 'will_it_work_for_me#will_not_work_without_uk_address', as: :will_not_work_without_uk_address
   get 'select_documents', to: 'select_documents#index', as: :select_documents
-  get 'select_documents_none', to: 'select_documents#no_documents', as: :select_documents_no_documents
-  post 'select_documents', to: 'select_documents#select_documents', as: :select_documents_submit
-  get 'other_identity_documents', to: 'other_identity_documents#index', as: :other_identity_documents
-  post 'other_identity_documents', to: 'other_identity_documents#select_other_documents', as: :other_identity_documents_submit
-  get 'select_phone', to: 'select_phone#index', as: :select_phone
-  post 'select_phone', to: 'select_phone#select_phone', as: :select_phone_submit
-  get 'no_mobile_phone', to: 'select_phone#no_mobile_phone', as: :no_mobile_phone
-  get 'choose_a_certified_company', to: 'choose_a_certified_company_loa2#index', as: :choose_a_certified_company
-  post 'choose_a_certified_company', to: 'choose_a_certified_company_loa2#select_idp', as: :choose_a_certified_company_submit
-  get 'choose_a_certified_company/:company', to: 'choose_a_certified_company_loa2#about', as: :choose_a_certified_company_about
+  # get 'select_documents_none', to: 'select_documents#no_documents', as: :select_documents_no_documents
+  # post 'select_documents', to: 'select_documents#select_documents', as: :select_documents_submit
+  # get 'other_identity_documents', to: 'other_identity_documents#index', as: :other_identity_documents
+  # post 'other_identity_documents', to: 'other_identity_documents#select_other_documents', as: :other_identity_documents_submit
+  # get 'select_phone', to: 'select_phone#index', as: :select_phone
+  # post 'select_phone', to: 'select_phone#select_phone', as: :select_phone_submit
+  # get 'no_mobile_phone', to: 'select_phone#no_mobile_phone', as: :no_mobile_phone
+  # get 'choose_a_certified_company', to: 'choose_a_certified_company_loa2#index', as: :choose_a_certified_company
+  # post 'choose_a_certified_company', to: 'choose_a_certified_company_loa2#select_idp', as: :choose_a_certified_company_submit
+  # get 'choose_a_certified_company/:company', to: 'choose_a_certified_company_loa2#about', as: :choose_a_certified_company_about
   get 'why_companies', to: 'why_companies_loa2#index', as: :why_companies
   get 'failed_registration', to: 'failed_registration_loa2#index', as: :failed_registration
   get 'cancelled_registration', to: 'cancelled_registration_loa2#index', as: :cancelled_registration
@@ -105,3 +113,52 @@ post 'further_information_null_attribute', to: 'further_information#submit_null_
 get 'no_idps_available', to: 'no_idps_available#index', as: :no_idps_available
 get 'cancelled_registration', to: 'cancelled_registration#index', as: :cancelled_registration
 get 'paused_registration', to: 'paused_registration#index', as: :paused_registration
+
+
+
+constraints questions_light_v2_control_piwik do
+  get 'about', to: 'about_loa2#index', as: :about
+end
+
+constraints questions_light_v2_control do
+  get 'about_certified_companies', to: 'about_loa2#certified_companies', as: :about_certified_companies
+  get 'about_identity_accounts', to: 'about_loa2#identity_accounts', as: :about_identity_accounts
+  get 'about_choosing_a_company', to: 'about_loa2#choosing_a_company', as: :about_choosing_a_company
+  get 'will_it_work_for_me', to: 'will_it_work_for_me#index', as: :will_it_work_for_me
+  post 'will_it_work_for_me', to: 'will_it_work_for_me#will_it_work_for_me', as: :will_it_work_for_me_submit
+  get 'may_not_work_if_you_live_overseas', to: 'will_it_work_for_me#may_not_work_if_you_live_overseas', as: :may_not_work_if_you_live_overseas
+  get 'will_not_work_without_uk_address', to: 'will_it_work_for_me#will_not_work_without_uk_address', as: :will_not_work_without_uk_address
+
+  get 'select_documents_none', to: 'select_documents#no_documents', as: :select_documents_no_documents
+  post 'select_documents', to: 'select_documents#select_documents', as: :select_documents_submit
+
+  get 'other_identity_documents', to: 'other_identity_documents#index', as: :other_identity_documents
+  post 'other_identity_documents', to: 'other_identity_documents#select_other_documents', as: :other_identity_documents_submit
+  get 'select_phone', to: 'select_phone#index', as: :select_phone
+  post 'select_phone', to: 'select_phone#select_phone', as: :select_phone_submit
+  get 'no_mobile_phone', to: 'select_phone#no_mobile_phone', as: :no_mobile_phone
+
+  get 'choose_a_certified_company', to: 'choose_a_certified_company_loa2#index', as: :choose_a_certified_company
+  post 'choose_a_certified_company', to: 'choose_a_certified_company_loa2#select_idp', as: :choose_a_certified_company_submit
+  get 'choose_a_certified_company/:company', to: 'choose_a_certified_company_loa2#about', as: :choose_a_certified_company_about
+end
+
+constraints questions_light_v2_variant_piwik do
+  get 'about', to: 'about_loa2_variant#index', as: :about
+end
+
+constraints questions_light_v2_variant do
+  get 'about_certified_companies', to: 'about_loa2_variant#certified_companies', as: :about_certified_companies
+  get 'about_identity_accounts', to: 'about_loa2_variant#identity_accounts', as: :about_identity_accounts
+  get 'about_choosing_a_company', to: 'about_loa2_variant#choosing_a_company', as: :about_choosing_a_company
+
+  get 'select_documents_none', to: 'select_documents_variant#no_documents', as: :select_documents_no_documents
+  post 'select_documents', to: 'select_documents_variant#select_documents', as: :select_documents_submit
+
+  get 'other_identity_documents', to: 'other_identity_documents_variant#index', as: :other_identity_documents
+  post 'other_identity_documents', to: 'other_identity_documents_variant#select_other_documents', as: :other_identity_documents_submit
+
+  get 'choose_a_certified_company', to: 'choose_a_certified_company_loa2_variant#index', as: :choose_a_certified_company
+  post 'choose_a_certified_company', to: 'choose_a_certified_company_loa2_variant#select_idp', as: :choose_a_certified_company_submit
+  get 'choose_a_certified_company/:company', to: 'choose_a_certified_company_loa2_variant#about', as: :choose_a_certified_company_about
+end
