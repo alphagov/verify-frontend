9abbffe942424f090d8900f72d40ead1c9a18885
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2018-06-27 15:29:49 +0100

    Skip stub transactions and translations for stub_api_spec

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,11 @@
 #!/usr/bin/env puma
 
-environment 'development'
+environment 'production'
 
 pidfile 'tmp/puma.pid'
 state_path 'tmp/puma.state'
 stdout_redirect 'log/puma.stdout', 'log/puma.stderr', true
 
 bind 'unix://tmp/puma.sock'
-bind 'tcp://127.0.0.1:50300'
 
 workers 2 unless Gem.win_platform?
