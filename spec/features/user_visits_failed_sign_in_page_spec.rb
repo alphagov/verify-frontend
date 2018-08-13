db7cc367ca2b4355b8e5f90a2dded40b317c91bc
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2018-08-13 15:40:53 +0100

    EID-795: Revise error pages - AuthFailure response
    Redirect users to the Verify/eIDAS picker page when we receive an AuthFailure response from the selected country. Regular Verify users will be redirected to the start page on AuthFailure response from the IDP
    
    Author: @CharlesIC

--- spec/features/user_visits_failed_sign_in_page_spec.rb
+++ spec/features/user_visits_failed_sign_in_page_spec.rb
@@ -28,4 +28,35 @@ RSpec.describe 'When the user visits the failed sign in page' do
       expect(page).to have_css 'html[lang=cy]'
     end
   end
+
+  context '#country' do
+    before(:each) do
+      stub_countries_list
+      page.set_rack_session selected_country: { entity_id: 'http://stub-country.uk', simple_id: 'YY', enabled: true }
+    end
+
+    it 'includes expected content' do
+      visit '/failed-country-sign-in'
+
+      expect_feedback_source_to_be(page, 'FAILED_COUNTRY_SIGN_IN_PAGE', '/failed-country-sign-in')
+      expect(page).to have_title t('hub.failed_country_sign_in.title')
+      expect(page).to have_content t('hub.failed_country_sign_in.heading', country_name: 'Stub Country')
+      expect(page.body).to have_content t('hub.failed_country_sign_in.online')
+      expect(page).to have_link t('hub.failed_country_sign_in.online_link'), href: prove_identity_path
+      expect(page.body).to have_content t('hub.failed_country_sign_in.offline')
+    end
+
+    it 'should redirect to prove-identity page on matching error for an eIDAS journey' do
+      visit '/failed-country-sign-in'
+      click_on t('hub.failed_country_sign_in.online_link')
+
+      expect(page).to have_current_path('/prove-identity')
+    end
+
+    it 'displays the content in Welsh' do
+      visit '/methu-mewngofnodi-gwlad'
+
+      expect(page).to have_css 'html[lang=cy]'
+    end
+  end
 end
