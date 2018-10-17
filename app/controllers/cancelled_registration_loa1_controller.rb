96df1b21311647ccd50f5643c4d6fbcb5ea9050f
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2018-10-17 17:58:52 +0100

    HUB-298: Update cancel page content
    Changed all necessary content, as well as some controller changes to facilitate the new content in
    a clearer fashion, some remaining issues with capitalization of custom RP config though.

--- app/controllers/cancelled_registration_loa1_controller.rb
+++ app/controllers/cancelled_registration_loa1_controller.rb
@@ -1,8 +1,10 @@
 class CancelledRegistrationLoa1Controller < ApplicationController
   def index
     @idp = IDENTITY_PROVIDER_DISPLAY_DECORATOR.decorate(selected_identity_provider)
-    @service_name = current_transaction.name
+    @transaction = current_transaction
+    @other_ways_decorated = @transaction.other_ways_text
+    @other_ways_decorated[0] = @other_ways_decorated[0].capitalize
 
-    render :cancelled_registration_LOA1
+    render :cancelled_registration_LOA1, locals: { transaction: @transaction }
   end
 end
--- app/controllers/cancelled_registration_loa2_controller.rb
+++ app/controllers/cancelled_registration_loa2_controller.rb
@@ -1,8 +1,8 @@
 class CancelledRegistrationLoa2Controller < ApplicationController
   def index
     @idp = IDENTITY_PROVIDER_DISPLAY_DECORATOR.decorate(selected_identity_provider)
-    @service_name = current_transaction.name
+    @transaction = current_transaction
 
-    render :cancelled_registration_LOA2
+    render :cancelled_registration_LOA2, locals: { transaction: @transaction }
   end
 end
