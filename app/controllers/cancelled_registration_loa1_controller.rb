d5a3038399f624d73ebed09c06be4e845fe331d0
stevebutler <steve.butler@digital.cabinet-office.gov.uk>
2018-10-22 15:09:05 +0100

    HUB-298: Fix other ways text on new cancel page
    Needed a slightly different bit of the dislpay config.

--- app/controllers/cancelled_registration_loa1_controller.rb
+++ app/controllers/cancelled_registration_loa1_controller.rb
@@ -2,7 +2,7 @@ class CancelledRegistrationLoa1Controller < ApplicationController
   def index
     @idp = IDENTITY_PROVIDER_DISPLAY_DECORATOR.decorate(selected_identity_provider)
     @transaction = current_transaction
-    @other_ways_decorated = @transaction.other_ways_text
+    @other_ways_decorated = @transaction.other_ways_description
     @other_ways_decorated[0] = @other_ways_decorated[0].capitalize
 
     render :cancelled_registration_LOA1, locals: { transaction: @transaction }
--- app/controllers/cancelled_registration_loa2_controller.rb
+++ app/controllers/cancelled_registration_loa2_controller.rb
@@ -2,6 +2,8 @@ class CancelledRegistrationLoa2Controller < ApplicationController
   def index
     @idp = IDENTITY_PROVIDER_DISPLAY_DECORATOR.decorate(selected_identity_provider)
     @transaction = current_transaction
+    @other_ways_decorated = @transaction.other_ways_description
+    @other_ways_decorated[0] = @other_ways_decorated[0].capitalize
 
     render :cancelled_registration_LOA2, locals: { transaction: @transaction }
   end
