87adff23adaafc58205aab41e9bb528a93a9dfc1
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2018-10-30 07:17:42 +0000

    BAU: Remove 'polling_wait_time' configuration
    This was never used.
    
    The POLLING_WAIT_TIME env var is, by lib/tasks/zdd.rake

--- config/initializers/00_configuration.rb
+++ config/initializers/00_configuration.rb
@@ -15,7 +15,6 @@ CONFIG = Configuration.load! do
   option_string 'policy_host', 'POLICY_HOST'
   option_string 'logo_directory', 'LOGO_DIRECTORY', default: '/idp-logos'
   option_string 'zdd_file', 'ZDD_LATCH'
-  option_string 'polling_wait_time', 'POLLING_WAIT_TIME', default: 6
   option_bool 'metrics_enabled', 'METRICS_ENABLED', default: true
   if metrics_enabled
     option_string 'statsd_host', 'STATSD_HOST', default: '127.0.0.1'
