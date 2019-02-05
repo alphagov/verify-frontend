8709c8aa9a2962ca92c9cf76297f7cccb8913818
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2019-02-05 17:56:52 +0000

    HUB-42: Stop excessive repetition of delegation

--- app/models/display/viewable_identity_provider.rb
+++ app/models/display/viewable_identity_provider.rb
@@ -5,13 +5,25 @@ module Display
     :logo_path,
     :white_logo_path,
   ) do
-    delegate :entity_id, to: :identity_provider
-    delegate :simple_id, to: :identity_provider
-    delegate :model_name, to: :identity_provider
-    delegate :to_key, to: :identity_provider
-    delegate :authentication_enabled, to: :identity_provider
-    delegate :display_name, :about_content, :requirements, :special_no_docs_instructions, :no_docs_requirement, :contact_details, :interstitial_question, :interstitial_explanation, :mobile_app_installation, :tagline, to: :display_data
-    delegate :temporarily_unavailable, to: :identity_provider
+    delegate :entity_id,
+             :simple_id,
+             :model_name,
+             :to_key,
+             :authentication_enabled,
+             :temporarily_unavailable,
+             to: :identity_provider
+
+    delegate :display_name,
+             :about_content,
+             :requirements,
+             :special_no_docs_instructions,
+             :no_docs_requirement,
+             :contact_details,
+             :interstitial_question,
+             :interstitial_explanation,
+             :mobile_app_installation,
+             :tagline,
+             to: :display_data
 
     def viewable?
       true
