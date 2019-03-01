e2aabe2f8ab4c5fb7f6d63331b28aee3a61b8ef7
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2019-03-01 17:52:39 +0000

    BAU: Commonise nil handling
    Create a new method to abstract away nil checking. Also remove a bunch of duplicate calls to journey_hint_value

--- app/controllers/partials/journey_hinting_partial_controller.rb
+++ app/controllers/partials/journey_hinting_partial_controller.rb
@@ -8,47 +8,46 @@ module JourneyHintingPartialController
     nil
   end
 
+  def with(value)
+    unless value.nil?
+      yield value
+    end
+  end
+
   def attempted_entity_id
-    journey_hint_value.nil? ? nil : journey_hint_value['ATTEMPT']
+    with(journey_hint_value) { |h| h['ATTEMPT'] }
   end
 
   def success_entity_id
-    journey_hint_value.nil? ? nil : journey_hint_value['SUCCESS']
+    with(journey_hint_value) { |h| h['SUCCESS'] }
   end
 
   def resume_link?
-    journey_hint = journey_hint_value
-    !(journey_hint.nil? || journey_hint.fetch('RESUMELINK', nil).nil?)
+    with(journey_hint_value) { |h| h['RESUMELINK'] }
   end
 
   def resume_link_idp
-    journey_hint = journey_hint_value
-    journey_hint.nil? ? nil : journey_hint.dig('RESUMELINK', 'IDP')
+    with(journey_hint_value) { |h| h.dig('RESUMELINK', 'IDP') }
   end
 
   def last_status
-    journey_hint = journey_hint_value
-    journey_hint.nil? ? nil : journey_hint['STATE']
+    with(journey_hint_value) { |h| h['STATE'] }
   end
 
   def is_last_status?(status)
-    last_status_value = last_status
-    !last_status_value.nil? && last_status_value['STATUS'] == status
+    with(last_status) { |last_status_value| last_status_value['STATUS'] == status }
   end
 
   def last_idp
-    last_status_value = last_status
-    last_status_value.nil? ? nil : last_status_value.fetch('IDP', nil)
+    with(last_status) { |last_status_value| last_status_value['IDP'] }
   end
 
   def last_rp
-    last_status_value = last_status
-    last_status_value.nil? ? nil : last_status_value.fetch('RP', nil)
+    with(last_status) { |last_status_value| last_status_value['RP'] }
   end
 
   def user_followed_journey_hint(entity_id_followed_by_user)
-    hinted_id = success_entity_id
-    !hinted_id.nil? && hinted_id == entity_id_followed_by_user
+    with(success_entity_id) { |hinted_id| hinted_id == entity_id_followed_by_user }
   end
 
   def retrieve_decorated_singleton_idp_array_by_entity_id(providers, entity_id)
