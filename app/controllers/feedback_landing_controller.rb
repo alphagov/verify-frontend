7adc56780c062f369aa6437dd282d5de833f8edb
John Watts <john.watts@digital.cabinet-office.gov.uk>
2019-04-29 15:46:34 +0100

    Codacy is opinionated
    Use a return statement (aka guard clause) instead of a conditional block.

--- app/controllers/feedback_landing_controller.rb
+++ app/controllers/feedback_landing_controller.rb
@@ -8,11 +8,11 @@ class FeedbackLandingController < ApplicationController
     flash['feedback_source'] = params['feedback-source'].nil? ? flash['feedback_source'] : params['feedback-source']
     @feedback_landing_heading = t('hub.feedback_landing.basic_heading')
 
-    unless current_transaction_simple_id.nil?
-      @other_ways_heading = t('hub.feedback_landing.services.heading')
-      @other_ways_text = current_transaction.other_ways_text
-      @service_name = current_transaction.name
-      @feedback_landing_heading = t('hub.feedback_landing.heading', service_name: @service_name)
-    end
+    return if current_transaction_simple_id.nil?
+
+    @other_ways_heading = t('hub.feedback_landing.services.heading')
+    @other_ways_text = current_transaction.other_ways_text
+    @service_name = current_transaction.name
+    @feedback_landing_heading = t('hub.feedback_landing.heading', service_name: @service_name)
   end
 end
