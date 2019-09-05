537f3a4c6cb2dca18b707738a2e1bff677fec343
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2019-09-05 17:33:38 +0100

    TfEzFC4W: Implement cross domain Google Analytics tracking for Verify
    Fix indentation in `analytics.js`

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,68 +1,68 @@
-(function(global) {
-    'use strict';
-    var GOVUK = global.GOVUK || {};
-    var $ = global.jQuery;
+(function (global) {
+  'use strict';
+  var GOVUK = global.GOVUK || {};
+  var $ = global.jQuery;
 
-    function generateEightHexadecimalDigits() {
-        return Math.random().toString(16).substr(2, 8);
+  function generateEightHexadecimalDigits() {
+    return Math.random().toString(16).substr(2, 8);
+  }
+
+  function getPiwikVisitorIdCookie() {
+    var userId = GOVUK.getCookie('PIWIK_USER_ID');
+    if (!userId) {
+      userId = generateEightHexadecimalDigits() + generateEightHexadecimalDigits();
+      GOVUK.setCookie('PIWIK_USER_ID', userId);
+    }
+    return userId;
+  }
+
+  var trackerUrl = $('#piwik-url').text();
+
+  if (trackerUrl) {
+    var siteId = $('#piwik-site-id').text();
+    var customUrl = $('#piwik-custom-url').text();
+    var enTitle = $('meta[name="verify|title"]').attr("content");
+    var newVisit = $('#piwik-new-visit').length ? 1 : 0;
+
+    var piwikAnalyticsQueue = [
+      ['appendToTrackingUrl', 'new_visit=' + newVisit],
+      ['setUserId', getPiwikVisitorIdCookie()],
+      ['setDocumentTitle', enTitle],
+      ['trackPageView'],
+      ['enableLinkTracking'],
+      ['setTrackerUrl', trackerUrl],
+      ['setSiteId', siteId]
+    ];
+
+    var customVariablesString = $('#piwik-custom-variables').text();
+    if (customVariablesString) {
+      var customVariables = JSON.parse(customVariablesString);
+      $.each(customVariables, function (index) {
+        piwikAnalyticsQueue.unshift([
+          'setCustomVariable',
+          customVariables[index].index,
+          customVariables[index].name,
+          customVariables[index].value,
+          customVariables[index].scope
+        ])
+      });
     }
 
-    function getPiwikVisitorIdCookie() {
-        var userId = GOVUK.getCookie('PIWIK_USER_ID');
-        if (!userId) {
-            userId = generateEightHexadecimalDigits() + generateEightHexadecimalDigits();
-            GOVUK.setCookie('PIWIK_USER_ID', userId);
-        }
-        return userId;
+    if (customUrl) {
+      // customUrl needs to go at the beginning of the piwik array
+      piwikAnalyticsQueue.unshift(['setCustomUrl', customUrl]);
     }
 
-    var trackerUrl = $('#piwik-url').text();
+    global._paq = piwikAnalyticsQueue;
+  }
 
-    if(trackerUrl) {
-        var siteId = $('#piwik-site-id').text();
-        var customUrl = $('#piwik-custom-url').text();
-        var enTitle = $('meta[name="verify|title"]').attr("content");
-        var newVisit = $('#piwik-new-visit').length ? 1 : 0;
-
-        var piwikAnalyticsQueue = [
-            ['appendToTrackingUrl', 'new_visit=' + newVisit],
-            ['setUserId', getPiwikVisitorIdCookie()],
-            ['setDocumentTitle', enTitle ],
-            ['trackPageView'],
-            ['enableLinkTracking'],
-            ['setTrackerUrl', trackerUrl],
-            ['setSiteId', siteId]
-        ];
-
-        var customVariablesString = $('#piwik-custom-variables').text();
-        if (customVariablesString) {
-            var customVariables = JSON.parse(customVariablesString);
-            $.each(customVariables, function(index) {
-                piwikAnalyticsQueue.unshift([
-                    'setCustomVariable',
-                    customVariables[index].index,
-                    customVariables[index].name,
-                    customVariables[index].value,
-                    customVariables[index].scope
-                ])
-            });
-        }
-
-        if (customUrl) {
-            // customUrl needs to go at the beginning of the piwik array
-            piwikAnalyticsQueue.unshift(['setCustomUrl', customUrl]);
-        }
-
-        global._paq = piwikAnalyticsQueue;
-    }
-
-    var cross_gov_ga_tracker_id = $("#cross-gov-ga-tracker-id").text();
-    if (cross_gov_ga_tracker_id) {
-        var domain_list = JSON.parse($("#cross-gov-ga-domain-list").text());
-        ga("create", cross_gov_ga_tracker_id, "auto", "govuk_shared", {"allowLinker": true});
-        ga("govuk_shared.require", "linker");
-        ga("govuk_shared.linker.set", "anonymizeIp", true);
-        ga("govuk_shared.linker:autoLink", domain_list, false, false);
-        ga("govuk_shared.send", "pageview");
-    }
+  var cross_gov_ga_tracker_id = $("#cross-gov-ga-tracker-id").text();
+  if (cross_gov_ga_tracker_id) {
+    var domain_list = JSON.parse($("#cross-gov-ga-domain-list").text());
+    ga("create", cross_gov_ga_tracker_id, "auto", "govuk_shared", {"allowLinker": true});
+    ga("govuk_shared.require", "linker");
+    ga("govuk_shared.linker.set", "anonymizeIp", true);
+    ga("govuk_shared.linker:autoLink", domain_list, false, false);
+    ga("govuk_shared.send", "pageview");
+  }
 })(window);
