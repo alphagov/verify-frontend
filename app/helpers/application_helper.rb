0758841bf31406fefd8df9f96a1f33bec356fdc2
Jamie Maynard <james.maynard@digital.cabinet-office.gov.uk>
2019-11-07 18:02:17 +0000

    WIP first bits of the DS3 upgrade

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -68,4 +68,11 @@ module ApplicationHelper
     options[:role] = 'button'
     link_to text, path, options
   end
+
+  def display_page_title
+    title = content_for :page_title
+    raise NotImplementedError.new('Missing page title') if Rails.env.test? && (title == PAGE_TITLE_SUFFIX || title.nil?)
+
+    title
+  end
 end
