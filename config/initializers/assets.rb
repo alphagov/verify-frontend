d563e125fff1a291adacc78230bd9c232cbd1021
Jamie Maynard <james.maynard@digital.cabinet-office.gov.uk>
2019-11-28 10:06:28 +0000

    Final push to address all views.
    All thats left is some minor bugs and the letter counts on the
    feedback form.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("lib/node_modules")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(style.css style-ie8.css gov.uk_logotype_crown.svg piwik_idp_picker_tracking.js jasmine-jquery.js)
+Rails.application.config.assets.precompile += %w(style.css style-ie8.css gov.uk_logotype_crown.svg piwik_idp_picker_tracking.js jasmine-jquery.js *.ico *.svg *.png)
