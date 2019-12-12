44b603f92a63523c9c1e8a329224343fd71399b7
John Watts <john.watts@digital.cabinet-office.gov.uk>
2019-12-12 17:40:28 +0000

    WIP HUH-234

--- spec/features/user_visits_select_documents_variant_c_page_spec.rb
+++ spec/features/user_visits_select_documents_variant_c_page_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature 'When user visits document selection page' do
   before(:each) do
     experiment = { "short_hub_2019_q3-preview" => "short_hub_2019_q3-preview_variant_c_2_idp_short_hub" }
     set_session_and_ab_session_cookies!(experiment)
+    stub_api_idp_list_for_loa
     visit '/select-documents'
   end
 
@@ -21,16 +22,24 @@ RSpec.feature 'When user visits document selection page' do
     expect(page).to have_content('Which of these do you have available right now?')
   end
 
-  context 'with javascript enabled', js: true do
-    it 'redirects to the idp picker page when selects 3 documents' do
-      visit '/select-documents'
+  it 'redirects to the idp picker page when selects 3 documents' do
+    visit '/select-documents'
 
-      check 'Your current driving licence, full or provisional, with your photo on it'
-      check 'Your valid passport'
-      check 'Your credit or debit card'
-      click_button t('navigation.continue')
+    check 'Your current driving licence, full or provisional, with your photo on it', allow_label_click: true
+    check 'Your valid passport', allow_label_click: true
+    check 'Your credit or debit card', allow_label_click: true
+    click_button t('navigation.continue')
 
-      expect(page).to have_current_path(choose_a_certified_company_path)
-    end
+    expect(page).to have_current_path(choose_a_certified_company_path)
+  end
+
+  it 'redirects to the select advice page when selects 2 documents' do
+    visit '/select-documents'
+
+    check 'Your current driving licence, full or provisional, with your photo on it', allow_label_click: true
+    check 'Your credit or debit card', allow_label_click: true
+    click_button t('navigation.continue')
+
+    expect(page).to have_current_path(select_documents_advice_path)
   end
 end
