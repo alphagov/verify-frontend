d93fe71009dcb99122aec75d1e1f8494e3d49c19
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2019-12-12 17:06:08 +0000

    HUH-74 Add last_successful_idp endpoint to HintController
    We want to run a test where users are shown a direct link to log in with
    the supplier they were last successful with. This is derived from their
    cookie. Some JS is being written that will call this endpoint to find
    out which IDP that was, and return some information which will be
    useful.

--- spec/controllers/hint_controller_spec.rb
+++ spec/controllers/hint_controller_spec.rb
@@ -5,37 +5,105 @@ require 'api_test_helper'
 
 describe HintController do
   subject { get :ajax_request, params: { locale: 'en' } }
+  let(:successful_idp) { get :last_successful_idp, params: { locale: 'en' } }
 
-  context 'user has a journey hint present' do
-    it 'json object should return true' do
-      cookies.encrypted[CookieNames::VERIFY_FRONT_JOURNEY_HINT] = {
-        'ATTEMPT' => 'http://idcorp.com',
-      }.to_json
+  context '#hint' do
+    context 'user has a journey hint present' do
+      it 'json object should return true' do
+        cookies.encrypted[CookieNames::VERIFY_FRONT_JOURNEY_HINT] = {
+          'ATTEMPT' => 'http://idcorp.com',
+        }.to_json
 
-      body = JSON.parse(subject.body)
-      expect(body["value"]).to eq(true)
-      expect(subject.content_type).to eq("application/json")
-      expect(subject).to have_http_status(200)
+        body = JSON.parse(subject.body)
+        expect(body["value"]).to eq(true)
+        expect(subject.content_type).to eq("application/json")
+        expect(subject).to have_http_status(200)
+      end
+
+      it 'json object should return true even if the value is not set' do
+        cookies.encrypted[CookieNames::VERIFY_FRONT_JOURNEY_HINT] = {
+          'ATTEMPT' => '',
+        }.to_json
+
+        body = JSON.parse(subject.body)
+        expect(body["value"]).to eq(true)
+        expect(subject.content_type).to eq("application/json")
+        expect(subject).to have_http_status(200)
+      end
     end
 
-    it 'json object should return true even if the value is not set' do
-      cookies.encrypted[CookieNames::VERIFY_FRONT_JOURNEY_HINT] = {
-        'ATTEMPT' => '',
-      }.to_json
-
-      body = JSON.parse(subject.body)
-      expect(body["value"]).to eq(true)
-      expect(subject.content_type).to eq("application/json")
-      expect(subject).to have_http_status(200)
+    context 'user does not have a journey hint present' do
+      it 'json object should return false' do
+        body = JSON.parse(subject.body)
+        expect(body["value"]).to eq(false)
+        expect(subject.content_type).to eq("application/json")
+        expect(subject).to have_http_status(200)
+      end
     end
   end
 
-  context 'user does not have a journey hint present' do
-    it 'json object should return false' do
-      body = JSON.parse(subject.body)
-      expect(body["value"]).to eq(false)
-      expect(subject.content_type).to eq("application/json")
-      expect(subject).to have_http_status(200)
+  context '#last_successful_idp' do
+    before(:each) do
+      stub_api_idp_list_for_sign_in([{ 'simpleId' => 'stub-idp-one',
+                                       'entityId' => 'http://idcorp.com',
+                                       'levelsOfAssurance' => %w(LEVEL_1) },
+                                     { 'simpleId' => 'stub-idp-two',
+                                       'entityId' => 'http://idcorp-two.com',
+                                       'levelsOfAssurance' => %w(LEVEL_1) },
+                                     { 'simpleId' => 'stub-idp-broken',
+                                       'entityId' => 'http://idcorp-broken.com',
+                                       'levelsOfAssurance' => %w(LEVEL_1),
+                                       'temporarilyUnavailable' => true }])
+      set_session_and_cookies_with_loa('LEVEL_1')
+    end
+
+    context 'user has previously succesfully signed in' do
+      it 'json object should include simpleId and displayName' do
+        cookies.encrypted[CookieNames::VERIFY_FRONT_JOURNEY_HINT] = {
+          'SUCCESS' => 'http://idcorp-two.com',
+        }.to_json
+
+        body = JSON.parse(successful_idp.body)
+
+        expect(body['found']).to eq('true')
+        expect(body['simpleId']).to eq('stub-idp-two')
+        expect(body['displayName']).to eq('Bob’s Identity Service')
+        expect(successful_idp.content_type).to eq("application/json")
+        expect(successful_idp).to have_http_status(200)
+      end
+    end
+
+    context 'user has not previously successfully signed in' do
+      it 'json object should contain empty strings for simpleId and displayName' do
+        cookies.encrypted[CookieNames::VERIFY_FRONT_JOURNEY_HINT] = {
+          'ATTEMPT' => 'http://idcorp-two.com',
+        }.to_json
+
+        body = JSON.parse(successful_idp.body)
+
+        expect(body['found']).to eq('false')
+        expect(body.keys).not_to include('simpleId')
+        expect(body.keys).not_to include('displayName')
+        expect(successful_idp.content_type).to eq("application/json")
+        expect(successful_idp).to have_http_status(200)
+      end
+    end
+
+    context 'list of available identity providers is empty' do
+      it 'json object should contain empty strings for simpleId and displayName' do
+        stub_api_idp_list_for_sign_in([])
+        cookies.encrypted[CookieNames::VERIFY_FRONT_JOURNEY_HINT] = {
+          'SUCCESS' => 'http://idcorp-two.com',
+        }.to_json
+
+        body = JSON.parse(successful_idp.body)
+
+        expect(body['found']).to eq('false')
+        expect(body.keys).not_to include('simpleId')
+        expect(body.keys).not_to include('displayName')
+        expect(successful_idp.content_type).to eq("application/json")
+        expect(successful_idp).to have_http_status(200)
+      end
     end
   end
 end
