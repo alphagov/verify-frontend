e484b617928b02e3edbc2cf1629094dec5fc58e7
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2020-03-25 18:31:57 +0000

    Fix test with selected_idp
    The page needs the selected IDP in the session

--- spec/controllers/what_happens_next_controller_spec.rb
+++ spec/controllers/what_happens_next_controller_spec.rb
@@ -6,6 +6,11 @@ require 'piwik_test_helper'
 describe WhatHappensNextController do
   before(:each) do
     set_session_and_cookies_with_loa('LEVEL_2')
+    set_selected_idp(
+      'simple_id' => 'stub-idp-one',
+      'entity_id' => 'http://idcorp.com',
+      'levels_of_assurance' => %w(LEVEL_1 LEVEL_2)
+    )
   end
 
   context 'GET what_happens_next#index' do
--- spec/features/user_visits_what_happens_next_page_spec.rb
+++ spec/features/user_visits_what_happens_next_page_spec.rb
@@ -7,12 +7,10 @@ RSpec.describe 'When the user visits the what happens next page' do
   before(:each) do
     set_session_and_session_cookies!
     stub_api_idp_list_for_registration
+    set_selected_idp_in_session(entity_id: 'http://idcorp.com', simple_id: 'stub-idp-one')
   end
 
   context 'session cookie contains transaction id' do
-    before(:each) do
-      page.set_rack_session(transaction_simple_id: 'test-rp')
-    end
     it "will display the page and report the user's selection to piwik" do
       visit '/what-happens-next'
 
