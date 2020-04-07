975c4e9790b99daede5ce4453b66cae4488cbdcb
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2020-04-07 14:15:45 +0100

    BUGFIX: Add GOV.UK fonts to the asset manifest
    With latest uprade to Rails, we need to explicitly link the directories

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 //= link_tree ../../../lib/node_modules/govuk-frontend/govuk/assets/images
+//= link_tree ../../../lib/node_modules/govuk-frontend/govuk/assets/fonts
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link application.css
