a0ad8e1601a70f30841a9053f5fb286a4381bc3f
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2020-04-14 09:45:23 +0100

    Linter fixes
    Since this PR drifted from master as it's been open for a while.

--- spec/features/user_encounters_error_page_spec.rb
+++ spec/features/user_encounters_error_page_spec.rb
@@ -94,19 +94,19 @@ RSpec.describe "user encounters error page" do
       end
 
 
-      it 'will present a session timeout error page when the API returns session timeout' do
+      it "will present a session timeout error page when the API returns session timeout" do
         stub_saml_proxy_authn_request_endpoint
         stub_policy_sign_in_process_details
         stub_transaction_details
 
-        visit('/test-saml')
-        click_button 'saml-post'
-        visit('/test-saml')
-        click_button 'saml-post-trigger-session-expiry'
+        visit("/test-saml")
+        click_button "saml-post"
+        visit("/test-saml")
+        click_button "saml-post-trigger-session-expiry"
 
-        expect(page).to have_content t('errors.session_timeout.try_again', other_ways_description: t('rps.test-rp.other_ways_description'))
-        expect(page.body).to include t('errors.session_timeout.return_to_service_html')
-        expect(page).to have_link t('errors.session_timeout.start_again'), href: "http://www.test-rp.gov.uk/"
+        expect(page).to have_content t("errors.session_timeout.try_again", other_ways_description: t("rps.test-rp.other_ways_description"))
+        expect(page.body).to include t("errors.session_timeout.return_to_service_html")
+        expect(page).to have_link t("errors.session_timeout.start_again"), href: "http://www.test-rp.gov.uk/"
         expect(page).to have_css "#piwik-custom-url", text: "errors/timeout-error"
         expect(page).to have_css "a[href*=EXPIRED_ERROR_PAGE]"
         expect(page.status_code).to eq(403)
