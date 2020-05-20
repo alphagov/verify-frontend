73f2bdbb568262783b113b10c3ac39ab73cbc91c
Lewis Westbury <lewis.westbury@digital.cabinet-office.gov.uk>
2020-05-20 16:06:15 +0100

    Update AnalyticsCookiePartialController to use the cookie name directly.

--- app/controllers/partials/analytics_cookie_partial_controller.rb
+++ app/controllers/partials/analytics_cookie_partial_controller.rb
@@ -3,19 +3,15 @@ require "securerandom"
 # analytics_session_id is disconnected from matomo and generated if not present
 module AnalyticsCookiePartialController
   def analytics_session_id
-    cookie_value = cookies.fetch(persistent_cookie_name, nil)
+    cookie_value = cookies.fetch(CookieNames::PERSISTENT_SESSION_ID_COOKIE_NAME, nil)
     if cookie_value.nil?
       cookie_value = SecureRandom.uuid
-      cookies[persistent_cookie_name] = { value: cookie_value, expires: 13.months.from_now }
+      cookies[CookieNames::PERSISTENT_SESSION_ID_COOKIE_NAME] = { value: cookie_value, expires: 13.months.from_now }
     end
     cookie_value
   end
 
 private
-
-  def persistent_cookie_name
-    CookieNames::PERSISTENT_SESSION_ID_COOKIE_NAME
-  end
 
   def analytics_cookie_name
     cookies.each { |name, _value| return name if name.starts_with? CookieNames::ANALYTICS_SESSION_COOKIE_PREFIX }
