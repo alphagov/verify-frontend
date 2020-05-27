8fd4dd1430a0f0c3d60d845045a43c283c414e7f
Lewis Westbury <lewis.westbury@digital.cabinet-office.gov.uk>
2020-05-27 11:48:02 +0100

    Draw id into persistent session cookie from matomo if available, otherwise generate. Extend cookie duration to 13 months from each use.

--- app/controllers/partials/analytics_cookie_partial_controller.rb
+++ app/controllers/partials/analytics_cookie_partial_controller.rb
@@ -1,13 +1,18 @@
 require "securerandom"
 
-# analytics_session_id is disconnected from matomo and generated if not present
+# persistent session id is taken from matomo if available, and generated if not
 module AnalyticsCookiePartialController
   def analytics_session_id
     cookie_value = cookies.fetch(CookieNames::PERSISTENT_SESSION_ID_COOKIE_NAME, nil)
     if cookie_value.nil?
-      cookie_value = SecureRandom.uuid
-      cookies[CookieNames::PERSISTENT_SESSION_ID_COOKIE_NAME] = { value: cookie_value, expires: 13.months.from_now }
+      matomo_value = cookies.fetch(analytics_cookie_name, nil)
+      cookie_value = matomo_value.split(".").first unless matomo_value.nil?
+      if cookie_value.nil?
+        cookie_value = SecureRandom.uuid
+      end
     end
+    # analytics session id lasts for 13 months from use - so always refresh the cookie
+    cookies[CookieNames::PERSISTENT_SESSION_ID_COOKIE_NAME] = { value: cookie_value, expires: 13.months.from_now }
     cookie_value
   end
 
