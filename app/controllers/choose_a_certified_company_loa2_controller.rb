664c9fc9613affc6bb85178ff957d2317822688b
Karol Gancarz <karol.sinx@gmail.com>
2021-08-25 18:17:06 +0100

    HUB-971: Refactor IDP picker controllers
      - Remove the `current_available_identity_providers_for_registration_loa2` method and use the base version instead
      - Refactor idp_disconnecting and idp_already_tried logic into separate methods

--- app/controllers/choose_a_certified_company_loa2_controller.rb
+++ app/controllers/choose_a_certified_company_loa2_controller.rb
@@ -1,4 +1,5 @@
 require "partials/viewable_idp_partial_controller"
+
 class ChooseACertifiedCompanyLoa2Controller < ChooseACertifiedCompanyRedirectController
   include ChooseACertifiedCompanyAbout
   include ViewableIdpPartialController
@@ -7,7 +8,7 @@ class ChooseACertifiedCompanyLoa2Controller < ChooseACertifiedCompanyRedirectCon
 
   def index
     session[:selected_answers]&.delete("interstitial")
-    idps = current_available_identity_providers_for_registration_loa2(current_transaction_entity_id)
+    idps = current_available_identity_providers_for_registration
     suggestions = IDP_RECOMMENDATION_ENGINE.get_suggested_idps_for_registration(idps, selected_evidence, current_transaction_simple_id, idps_tried)
     @recommended_idps = order_with_unavailable_last(IDENTITY_PROVIDER_DISPLAY_DECORATOR.decorate_collection(suggestions[:recommended]))
     @non_recommended_idps = order_with_unavailable_last(IDENTITY_PROVIDER_DISPLAY_DECORATOR.decorate_collection(suggestions[:unlikely]))
@@ -27,7 +28,14 @@ class ChooseACertifiedCompanyLoa2Controller < ChooseACertifiedCompanyRedirectCon
     return render "errors/something_went_wrong", status: 400 unless params[:entity_id].present?
 
     select_viewable_idp_for_registration(params.fetch("entity_id")) do |decorated_idp|
-      session[:selected_idp_was_recommended] = IDP_RECOMMENDATION_ENGINE.recommended?(decorated_idp.identity_provider, current_available_identity_providers_for_sign_in, selected_evidence, current_transaction_simple_id, idps_tried)
+      session[:selected_idp_was_recommended] = IDP_RECOMMENDATION_ENGINE.recommended?(
+        decorated_idp.identity_provider,
+        current_available_identity_providers_for_registration,
+        selected_evidence,
+        current_transaction_simple_id,
+        idps_tried,
+      )
+
       # TODO - do the spinny thing page
       do_redirect(decorated_idp)
     end
