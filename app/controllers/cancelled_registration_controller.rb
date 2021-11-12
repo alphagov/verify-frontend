67e261fbceedaca1e69b965ea56922d8b07854a4
Jamie Maynard <jamie.maynard@digital.cabinet-office.gov.uk>
2021-11-12 13:34:27 +0000

    HUB-1118 Last page update for the single idp world

--- app/controllers/cancelled_registration_controller.rb
+++ app/controllers/cancelled_registration_controller.rb
@@ -1,4 +1,7 @@
+require "partials/viewable_idp_partial_controller"
+
 class CancelledRegistrationController < ApplicationController
+  include ViewableIdpPartialController
   before_action { @hide_feedback_link = true }
 
   def index
@@ -6,6 +9,7 @@ class CancelledRegistrationController < ApplicationController
     @transaction = current_transaction
     @other_ways_decorated = @transaction.other_ways_description
     @other_ways_decorated[0] = @other_ways_decorated[0].capitalize
+    @identity_providers = IDENTITY_PROVIDER_DISPLAY_DECORATOR.decorate_collection(identity_providers_available_for_registration)
 
     render :cancelled_registration
   end
--- spec/controllers/cancelled_registration_controller_spec.rb
+++ spec/controllers/cancelled_registration_controller_spec.rb
@@ -3,20 +3,24 @@ require "controller_helper"
 require "api_test_helper"
 
 describe CancelledRegistrationController do
+  let(:identity_provider_display_decorator) { double(:IdentityProviderDisplayDecorator) }
   subject { get :index, params: { locale: "en" } }
 
   before :each do
+    stub_request(:get, CONFIG.config_api_host + "/config/transactions/enabled")
     set_selected_idp("entity_id" => "http://idcorp.com", "simple_id" => "stub-idp-one", "levels_of_assurance" => %w(LEVEL_1 LEVEL_2))
   end
 
   it "renders the cancelled registration LOA1 template when LEVEL_1 is the requested LOA" do
     set_session_and_cookies_with_loa("LEVEL_1")
+    stub_api_idp_list_for_registration(loa: "LEVEL_1")
 
     expect(subject).to render_template(:cancelled_registration)
   end
 
   it "renders the cancelled registration LOA2 template when LEVEL_2 is the requested LOA" do
     set_session_and_cookies_with_loa("LEVEL_2")
+    stub_api_idp_list_for_registration(loa: "LEVEL_2")
 
     expect(subject).to render_template(:cancelled_registration)
   end
